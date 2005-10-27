2659e874b485797f9ca1286b67b83d00fe30d872
Ryan Thrash <ryan@modx.com>
2005-10-27 21:45:53 +0000

    Stupid typo caused mucho shenanigans... (space in front of opening <?php...)

--- manager/includes/version.inc.php
+++ manager/includes/version.inc.php
@@ -1,6 +1,6 @@
-    <?php
-$version	= '0.9.0';	// Current version.
-$code_name	= 'x9';		// Current codename 
+<?php
+$version = '0.9.0';	// Current version.
+$code_name = 'x9';		// Current codename 
 $full_appname = 'MODx'.' '.$version.($code_name ? " ($code_name)":"");
 $small_version = '0';
 $patch_level = '0';
