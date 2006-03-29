778fb96380a7a31b5b4434f17e35e81bc2d37504
Ryan Thrash <ryan@modx.com>
2006-03-29 22:46:18 +0000

    Update version/changelog for test build 6 (or is it 7???)

--- manager/includes/version.inc.php
+++ manager/includes/version.inc.php
@@ -1,6 +1,6 @@
 <?php
 $version = '0.9.5-pre';	// Current version.
-$code_name = 'rev 905';		// Current codename [OpenGeek:changed to SVN rev num for quick reference to repository]
+$code_name = 'rev 910';		// Current codename [OpenGeek:changed to SVN rev num for quick reference to repository]
 $full_appname = 'MODx'.' '.$version.($code_name ? " ($code_name)":"");
 $small_version = '0';
 $patch_level = '0';
