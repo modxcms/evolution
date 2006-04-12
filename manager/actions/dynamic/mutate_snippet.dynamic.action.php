d54fcc99947b90ceb369f3f19f90667254d0ac7d
Ryan Thrash <ryan@modx.com>
2006-04-12 18:29:06 +0000

    Add the opening and closing PHP tags on render to enable syntax highlighting.

--- manager/actions/dynamic/mutate_snippet.dynamic.action.php
+++ manager/actions/dynamic/mutate_snippet.dynamic.action.php
@@ -335,7 +335,7 @@ function decode(s){
 		    	<span style="float:left;color:#707070;font-weight:bold; padding:3px">&nbsp;<?php echo $_lang['snippet_code']; ?></span>
 		    	<span style="float:right;color:#707070;"><?php echo $_lang['wrap_lines']; ?><input name="wrap" type="checkbox" <?php echo $content['wrap']== 1 ? "checked='checked'" : "" ;?> class="inputBox" onclick="setTextWrap(document.mutate.post,this.checked)" /></span>
 		   	</div>
-			<textarea name="post" style="width:100%; height:370px;" wrap="<?php echo $content['wrap']== 1 ? "soft" : "off" ;?>" onchange="documentDirty=true;"><?php echo htmlspecialchars($content['snippet']); ?></textarea>
+			<textarea name="post" style="width:100%; height:370px;" wrap="<?php echo $content['wrap']== 1 ? "soft" : "off" ;?>" onchange="documentDirty=true;"><?php echo "<?php"."\n".trim(htmlspecialchars($content['snippet']))."\n"."?>"; ?></textarea>
 		</div>
 		<!-- PHP text editor end -->
 	</div>
