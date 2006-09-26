d35226f540458345dcbd360d6c65378adadf5a55
Ryan Thrash <ryan@modx.com>
2006-09-26 14:14:06 +0000

    Updating MODx light styles. Probably requires updates to MODx Green styles to accommodate new topbar.php structure. (see MODxLight/style.php and .css for reference)

--- manager/frames/topbar.php
+++ manager/frames/topbar.php
@@ -11,42 +11,33 @@ if(IN_MANAGER_MODE!="true") die("<b>INCLUDE_ORDERING_ERROR</b><br /><br />Please
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-
     <title>Top bar</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=<?php echo $modx_charset; ?>" />
+    <meta http-equiv="Content-Type" content="text/html; charset=<?php echo $etomite_charset; ?>" />
     <link rel="stylesheet" type="text/css" href="media/style/<?php echo $manager_theme ? "$manager_theme/":""; ?>style.css" />
     <script type="text/javascript">var MODX_MEDIA_PATH = "<?php echo IN_MANAGER_MODE ? "media":"manager/media"; ?>";</script>
-
 </head>
-<body>
+<body id="topbar">
 
-<div id="topbar"><table width="100%"  border="0" cellspacing="0" cellpadding="0" style="height:20px;">
-  <tr>
-    <td width="10">&nbsp;</td>
-    <td valign="middle">
-        <span id="tocText"> </span>
-        <span id="buildText">&nbsp;&nbsp;<img src='media/style/<?php echo $manager_theme ? "$manager_theme/":""; ?>images/icons/b02.gif' width='16' height='16' />&nbsp;<b><?php echo $_lang['loading_doc_tree']; ?></b></span>
-        <span id="workText">&nbsp;<img src='media/style/<?php echo $manager_theme ? "$manager_theme/":""; ?>images/icons/delete.gif' width='16' height='16' />&nbsp;<b><?php echo $_lang['working']; ?></b></span>
-    </td>
-    <td>&nbsp;</td>
-    <td align='right' nowrap="nowrap">
-        <b><?php echo $site_name ;?></b> - <b><?php echo $full_appname; ?> | <img src='media/style/<?php echo $manager_theme ? "$manager_theme/":""; ?>images/icons/user.gif' width='16' height='16'>
+<div id="topbar-container">
+    <span id="tocText"> </span>
+    <span id="buildText"><img src="<?php echo $_style['icons_loading_doc_tree']; ?>" /><?php echo $_lang['loading_doc_tree']; ?></span>
+    <span id="workText"><img src="<?php echo $_style['icons_working']; ?>" /><?php echo $_lang['working']; ?></span>
+
+    <div id="supplementalNav">
+        <?php echo $site_name ;?> - <?php echo $full_appname; ?> | <img src="<?php echo $_style['icons_user_current']; ?>" />
         <?php 
             echo ($modx->hasPermission('change_password'))? '<a onclick="this.blur();" href="index.php?a=28" target="main">'.$modx->getLoginUserName().'</a>': $modx->getLoginUserName();
-            echo '</b>';
         ?>
         <?php if($modx->hasPermission('messages')) { ?>
-            <b>|</b> <span id="newMail" style="display:none;font-size:11px;"><a href="index.php?a=10" title="<?php echo $_lang["you_got_mail"]; ?>" target="main"><img src="media/style/<?php echo $manager_theme ? "$manager_theme/":""; ?>images/icons/mailalert.gif" border="0" width='16' height='16' /></a></span>
-            <a onclick="this.blur();" href="index.php?a=10" target="main"><?php echo $_lang["messages"]; ?> <span id="msgCounter">(? / ? )</span></a>
+            | <span id="newMail"><a href="index.php?a=10" title="<?php echo $_lang["you_got_mail"]; ?>" target="main"><img src="<?php echo $_style['icons_mail']; ?>" /></a></span>
+            <a onclick="this.blur();" href="index.php?a=10" target="main"><?php echo $_lang["messages"]; ?> <span id="msgCounter">( ? / ? )</span></a>
         <?php } ?> 
         <?php if($modx->hasPermission('help')) { ?>
             &nbsp;|&nbsp;<a href="index.php?a=9" target="main"><?php echo $_lang["help"]; ?></a>
         <?php } ?>
             &nbsp;|&nbsp;<a href="index.php?a=8" target="_top"><?php echo $_lang["logout"]; ?></a>
-    </td>
-    <td width="20">&nbsp;</td>
-  </tr>
-</table>
+    </div>
+    <!-- close #supplementalNav -->
 </div>
 
 </body>
