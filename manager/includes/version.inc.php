a06526b2ab1169a1bf1d7909333695736f80c2c8
Jason Coward <jason@modx.com>
2006-10-19 17:15:11 +0000

    * Removed duplicate english language entry manage_metatags_help (exists as metatag_notice) and corrected reference in manage_metatags.dynamic.php. (1743) * Minor changes to login.html. (1738)
    merge -r1737:1738 http://svn1.cvsdude.com/rethrash/tattoo/tattoo/branches/raymond/trunk 
        U  /manager/media/style/MODxLight/login.html
        U  /manager/media/style/MODx/login.html
        U  /manager/media/style/MODxGreen/login.html
        Merge complete.
        ===== File Statistics: =====
        Updated: 3

--- manager/includes/version.inc.php
+++ manager/includes/version.inc.php
@@ -1,6 +1,6 @@
 <?php
 $version = '0.9.5 beta 5';	// Current version.
-$code_name = 'rev 1742';	// SVN version number
+$code_name = 'rev 1743';	// SVN version number
 $full_appname = 'MODx'.' '.$version.($code_name ? " ($code_name)":"");
 $small_version = '0';
 $patch_level = '0';
