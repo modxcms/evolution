d17fc53a6a8afaadd2648aec1bb8c4de7f4f45cb
Jason Coward <jason@modx.com>
2007-05-17 19:43:45 +0000

    * MODx 0.9.6 final commit (just sync'ing changelog and version)

--- manager/includes/version.inc.php
+++ manager/includes/version.inc.php
@@ -1,6 +1,6 @@
 <?php
 $version = '0.9.6';	// Current version
-$code_name = 'rev 2730';	// SVN version number
+$code_name = 'rev 2734';	// SVN version number
 $full_appname = 'MODx'.' '.$version.($code_name ? " ($code_name)":"");
 $small_version = '0';
 $patch_level = '0';
