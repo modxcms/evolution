f30aaabba8d54ce0c5f239ccaf706112a85313bf
Jason Coward <jason@modx.com>
2008-10-22 20:55:59 +0000

    * Updating version references to 0.9.6.3-RC2

--- install/setup.info.php
+++ install/setup.info.php
@@ -3,7 +3,7 @@
 #:::::::::::::::::::::::::::::::::::::::::
 
 	$moduleName = "MODx";
-	$moduleVersion = " 0.9.6.3-RC1";
+	$moduleVersion = " 0.9.6.3-RC2";
 	$moduleSQLBaseFile = "setup.sql";
 	$moduleSQLDataFile = "setup.data.sql";
 	$moduleSQLUpdateFile = "setup.updates.sql";
