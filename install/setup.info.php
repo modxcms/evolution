7ad2a1bdc19be6fa836542ee00b5520e3cb7a392
Jason Coward <jason@modx.com>
2008-12-18 03:28:50 +0000

    * Remove remaining RC references in preparation for 0.9.6.3 release. [merge -r4563:4564 http://svn.modxcms.com/svn/tattoo/tattoo/branches/0.9.6]

--- install/setup.info.php
+++ install/setup.info.php
@@ -3,7 +3,7 @@
 #:::::::::::::::::::::::::::::::::::::::::
 
 	$moduleName = "MODx";
-	$moduleVersion = " 0.9.6.3-RC2";
+	$moduleVersion = " 0.9.6.3";
 	$moduleSQLBaseFile = "setup.sql";
 	$moduleSQLDataFile = "setup.data.sql";
 	$moduleSQLUpdateFile = "setup.updates.sql";
