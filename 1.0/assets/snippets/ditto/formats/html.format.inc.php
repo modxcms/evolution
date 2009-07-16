2e54c2331f57371f81e098e028c0ff3ee728301e
Garry Nutting <garry@collabpad.com>
2009-07-16 14:03:32 +0000

    Oops, didn't really mean this folder to be here!

--- 1.0/assets/snippets/ditto/formats/html.format.inc.php
+++
@@ -1,4 +0,0 @@
-<?php
-$header = "";
-$footer = "";
-?>
--- 1.0/manager/includes/version.inc.php
+++
@@ -1,6 +0,0 @@
-<?php
-$version = '1.0.0';   /* Current version */
-$code_name = 'rev 5053';    /* SVN version number */
-$full_appname = 'MODx'.' '.$version.($code_name ? " ($code_name)":"");
-$small_version = '0';
-$patch_level = '0';
