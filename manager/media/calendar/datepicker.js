449d23f5f59efc267371dfb9d6c38a6e72dbd652
Garry Nutting <garry@collabpad.com>
2009-07-18 16:44:23 +0000

    [#MODX-866] Allow manual editing of the datepicker fields

--- manager/media/calendar/datepicker.js
+++ manager/media/calendar/datepicker.js
@@ -64,7 +64,6 @@ var DatePicker = new Class({
             yearOrder: options.yearOrder || this.yearOrder,
             yearOffset: options.yearOffset || this.yearOffset
         };
-        dp.setProperties({'id':dp.getProperty('name'), 'readonly':true});
         dp.container = false;
         dp.calendar = false;
         dp.interval = null;
