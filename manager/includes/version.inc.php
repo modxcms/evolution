4092132ac23659a2aca57d5f0053ba4815fe04f7
Ryan Thrash <ryan@modx.com>
2009-07-23 22:15:08 +0000

    Updating version/changelog in preparation for Evo 1.0.0-rc2

--- manager/includes/version.inc.php
+++ manager/includes/version.inc.php
@@ -1,6 +1,6 @@
 <?php
 $version = '1.0.0-rc2';   /* Current version */
-$code_name = 'rev 5254';    /* SVN version number */
+$code_name = 'rev 5515';    /* SVN version number */
 $full_appname = 'MODx'.' '.$version.($code_name ? " ($code_name)":"");
 $small_version = '0';
 $patch_level = '0';
