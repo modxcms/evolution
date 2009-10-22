891edb18cb0412ed815c2f28af1aeb457b922300
Ryan Thrash <ryan@modx.com>
2009-10-22 13:29:32 +0000

    Add $Date$ and $Rev$ SVN properties in preparation for 1.0.1 release

--- manager/includes/version.inc.php
+++ manager/includes/version.inc.php
@@ -1,13 +1,13 @@
 <?php
 function getSVNRev() {
-	// SVN property required to be set, e.g. $Rev: 5622 $
-    $svnrev = '$Rev: 5622 $';
+	// SVN property required to be set, e.g. $Rev$
+    $svnrev = '$Rev$';
     $svnrev = substr($svnrev, 6);
     return intval(substr($svnrev, 0, strlen($svnrev) - 2));
 }
 function getSVNDate() {
-	// $Date: 2009-08-05 15:27:23 -0500 (Wed, 05 Aug 2009) $ SVN property required to be set: $Date: 2009-08-05 15:27:23 -0500 (Wed, 05 Aug 2009) $
-	$svndate = '$Date: 2009-08-05 15:27:23 -0500 (Wed, 05 Aug 2009) $';
+	// $Date$ SVN property required to be set: $Date$
+	$svndate = '$Date$';
 	$svndate = substr($svndate, 40);
 	// need to convert this to a timestamp for using MODx native date format function
 	return strval(substr($svndate, 0, strlen($svndate) - 3));
