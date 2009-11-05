ee5a5fc2aa16f9c26c1c223520967a204adfc397
Jason Coward <jason@modx.com>
2009-11-05 04:38:42 +0000

    - [#MODX-1406] Error: Cannot redeclare getsvnrev() - Updating version to 1.0.2 [merge -r6064:6065 http://svn.modxcms.com/svn/tattoo/tattoo/branches/1.0.x]

--- manager/includes/version.inc.php
+++ manager/includes/version.inc.php
@@ -1,19 +1,23 @@
 <?php
-function getSVNRev() {
-	// SVN property required to be set, e.g. $Rev$
-    $svnrev = '$Rev$';
-    $svnrev = substr($svnrev, 6);
-    return intval(substr($svnrev, 0, strlen($svnrev) - 2));
+if (!function_exists('getSVNRev')) {
+    function getSVNRev() {
+        // SVN property required to be set, e.g. $Rev$
+        $svnrev = '$Rev$';
+        $svnrev = substr($svnrev, 6);
+        return intval(substr($svnrev, 0, strlen($svnrev) - 2));
+    }
 }
-function getSVNDate() {
-	// $Date$ SVN property required to be set: $Date$
-	$svndate = '$Date$';
-	$svndate = substr($svndate, 40);
-	// need to convert this to a timestamp for using MODx native date format function
-	return strval(substr($svndate, 0, strlen($svndate) - 3));
+if (!function_exists('getSVNDate')) {
+    function getSVNDate() {
+        // $Date$ SVN property required to be set: $Date$
+        $svndate = '$Date$';
+        $svndate = substr($svndate, 40);
+        // need to convert this to a timestamp for using MODx native date format function
+        return strval(substr($svndate, 0, strlen($svndate) - 3));
+    }
 }
 
-$modx_version = '1.0.1'; // Current version
+$modx_version = '1.0.2'; // Current version
 $modx_branch = 'Evolution';
 $code_name = 'rev '.getSVNRev(); // SVN version number
 $modx_release_date = getSVNDate();
