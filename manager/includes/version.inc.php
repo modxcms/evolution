e553ee05c205bf49745bfd360915d5632ad16ef2
Shaun McCormick <shaun@modx.com>
2010-06-08 19:56:22 +0000

    Merge changes for v1.0.4 release

--- manager/includes/version.inc.php
+++ manager/includes/version.inc.php
@@ -7,9 +7,8 @@ if (!function_exists('getSVNRev')) {
         return intval(substr($svnrev, 0, strlen($svnrev) - 2));
     }
 }
-
-$modx_version = '1.0.3';           // Current version number
-$modx_release_date = '1 Apr 2010'; // Date of release
+$modx_version = '1.0.4';           // Current version number
+$modx_release_date = '8 June 2010'; // Date of release
 $modx_branch = 'Evolution';        // Codebase name
 $code_name = 'rev '.getSVNRev();   // SVN version number (used mult places)
 $modx_full_appname = 'MODx '.$modx_branch.' '.$modx_version.' (Rev: '.getSVNRev().' Date: '.$modx_release_date.')';
