6327516ec9296ea6c63885dd315ca25e0c2c0edc
Mike Schell <mike@webprogramming.ca>
2010-12-03 09:52:54 -0500

    [#508] fix SQL error in module dependencies
    In some configurations, var_export was causing a php array to be injected
    into the SQL that updates the active_users table. var_export is only
    needed when the variable in question is null, so the code has been
    modified to only use var_export when the value is null, otherwise the
    value, which has been verified as an integer, is used directly.

--- manager/includes/accesscontrol.inc.php
+++ manager/includes/accesscontrol.inc.php
@@ -151,7 +151,7 @@ if(!isset($_SESSION['mgrValidated'])){
 			$_SESSION['mgrShortname'],
 			$lasthittime,
 			(string)$action,
-			var_export($itemid, true),
+			$itemid == null ? var_export(null, true) : $itemid,
 			$ip
 		);
 		if(!$rs = mysql_query($sql)) {
