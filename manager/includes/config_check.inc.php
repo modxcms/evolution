6d893bad66179656d1d0fe14989a92bbf45ef53c
Mike Schell <mike@webprogramming.ca>
2010-12-09 10:22:44 -0500

    [#impr-1360] whitespace cleanup on config_check.inc.php

--- manager/includes/config_check.inc.php
+++ manager/includes/config_check.inc.php
@@ -22,52 +22,52 @@ if (ini_get('register_globals')==TRUE) {
 }
 
 if (!extension_loaded('gd') || !extension_loaded('zip')) {
-	$warningspresent = 1;
-	$warnings[] = array($_lang['configcheck_php_gdzip']);
+    $warningspresent = 1;
+    $warnings[] = array($_lang['configcheck_php_gdzip']);
 }
 
 if(isset($_SESSION['mgrPermissions']['settings']) && $_SESSION['mgrPermissions']['settings'] == '1') {
-	if ($modx->db->getValue('SELECT COUNT(setting_value) FROM '.$modx->getFullTableName('system_settings').' WHERE setting_name=\'validate_referer\' AND setting_value=\'0\'')) {
-		$warningspresent = 1;
-	    $warnings[] = array($_lang['configcheck_validate_referer']);
-	}
-	$script = <<<JS
+    if ($modx->db->getValue('SELECT COUNT(setting_value) FROM '.$modx->getFullTableName('system_settings').' WHERE setting_name=\'validate_referer\' AND setting_value=\'0\'')) {
+        $warningspresent = 1;
+        $warnings[] = array($_lang['configcheck_validate_referer']);
+    }
+    $script = <<<JS
 <script type="text/javascript">
 function hideHeaderVerificationWarning(){
-	var myAjax = new Ajax('index.php?a=118', {
-		method: 'post',
-		data: 'action=setsetting&key=validate_referer&value=00'
-	});
-	myAjax.addEvent('onComplete', function(resp){
-		fieldset = $('validate_referer_warning_wrapper').getParent().getParent();
-		var sl = new Fx.Slide(fieldset);
-		sl.slideOut();
-	});
-	myAjax.request();
+    var myAjax = new Ajax('index.php?a=118', {
+        method: 'post',
+        data: 'action=setsetting&key=validate_referer&value=00'
+    });
+    myAjax.addEvent('onComplete', function(resp){
+        fieldset = $('validate_referer_warning_wrapper').getParent().getParent();
+        var sl = new Fx.Slide(fieldset);
+        sl.slideOut();
+    });
+    myAjax.request();
 }
 </script>
 
 JS;
-	$modx->regClientScript($script);
+    $modx->regClientScript($script);
 }
 
 if ($modx->db->getValue('SELECT published FROM '.$modx->getFullTableName('site_content').' WHERE id='.$unauthorized_page) == 0) {
-	$warningspresent = 1;
+    $warningspresent = 1;
     $warnings[] = array($_lang['configcheck_unauthorizedpage_unpublished']);
 }
 
 if ($modx->db->getValue('SELECT published FROM '.$modx->getFullTableName('site_content').' WHERE id='.$error_page) == 0) {
-	$warningspresent = 1;
+    $warningspresent = 1;
     $warnings[] = array($_lang['configcheck_errorpage_unpublished']);
 }
 
 if ($modx->db->getValue('SELECT privateweb FROM '.$modx->getFullTableName('site_content').' WHERE id='.$unauthorized_page) == 1) {
-	$warningspresent = 1;
+    $warningspresent = 1;
     $warnings[] = array($_lang['configcheck_unauthorizedpage_unavailable']);
 }
 
 if ($modx->db->getValue('SELECT privateweb FROM '.$modx->getFullTableName('site_content').' WHERE id='.$error_page) == 1) {
-	$warningspresent = 1;
+    $warningspresent = 1;
     $warnings[] = array($_lang['configcheck_errorpage_unavailable']);
 }
 
@@ -134,8 +134,8 @@ for ($i=0;$i<count($warnings);$i++) {
             $warnings[$i][1] = $_lang['configcheck_register_globals_msg'];
             break;
         case $_lang['configcheck_php_gdzip'] :
-        	$warnings[$i][1] = $_lang['configcheck_php_gdzip_msg'];
-        	break;
+            $warnings[$i][1] = $_lang['configcheck_php_gdzip_msg'];
+            break;
         case $_lang['configcheck_unauthorizedpage_unpublished'] :
             $warnings[$i][1] = $_lang['configcheck_unauthorizedpage_unpublished_msg'];
             break;
@@ -149,8 +149,8 @@ for ($i=0;$i<count($warnings);$i++) {
             $warnings[$i][1] = $_lang['configcheck_errorpage_unavailable_msg'];
             break;
         case $_lang['configcheck_validate_referer'] :
-        	$warnings[$i][1] = "<span id=\"validate_referer_warning_wrapper\">" . $_lang['configcheck_validate_referer_msg'] . "</span>\n";
-        	break;
+            $warnings[$i][1] = "<span id=\"validate_referer_warning_wrapper\">" . $_lang['configcheck_validate_referer_msg'] . "</span>\n";
+            break;
         default :
             $warnings[$i][1] = $_lang['configcheck_default_msg'];
     }
@@ -162,7 +162,7 @@ for ($i=0;$i<count($warnings);$i++) {
             <p style=\"padding-left:1em\"><em>".$_lang['configcheck_what']."</em><br />
             ".$warnings[$i][1]." ".$admin_warning."</p>
             </fieldset>
-    ";
+";
         if ($i!=count($warnings)-1) {
             $config_check_results .= "<br />";
         }
@@ -171,4 +171,3 @@ for ($i=0;$i<count($warnings);$i++) {
 } else {
     $config_check_results = $_lang['configcheck_ok'];
 }
-?>
