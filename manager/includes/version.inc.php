64c51b9f0ea9d196caf62d77ed79ddac009763cd
Mike Schell <mike@webprogramming.ca>
2011-01-10 16:30:06 -0500

    1.0.5-rc1 release preparation
    version numbers and date bumped

--- manager/includes/version.inc.php
+++ manager/includes/version.inc.php
@@ -1,5 +1,5 @@
 <?php
-$modx_version = '1.0.5';           // Current version number
-$modx_release_date = 'dev'; // Date of release
+$modx_version = '1.0.5-rc1';           // Current version number
+$modx_release_date = 'Jan 10, 2011'; // Date of release
 $modx_branch = 'Evolution';        // Codebase name
 $modx_full_appname = 'MODx '.$modx_branch.' '.$modx_version.' ('.$modx_release_date.')';
