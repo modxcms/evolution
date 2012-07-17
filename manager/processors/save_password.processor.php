72ed302dbae62e263667c529dc832c0db662aac6
Tim <tim@dougal.(none)>
2012-07-17 21:23:50 +0100

    Save password now uses new hashing classes

--- manager/processors/save_password.processor.php
+++ manager/processors/save_password.processor.php
@@ -22,12 +22,18 @@ if(strlen($pass1)<6){
 	exit;
 }
 
-$sql = "UPDATE $dbase.`".$table_prefix."manager_users` SET password=md5('".$pass1."') where id=".$modx->getLoginUserID().";";
+require ('hash.inc.php');
+$HashHandler = new HashHandler(CLIPPER_HASH_PREFERRED, $modx);
+$Hash = $HashHandler->generate($pass1);
+
+$sql = 'UPDATE '.$dbase.'.'.$table_prefix.'manager_users SET hashtype='.CLIPPER_HASH_PREFERRED.', salt=\''.$modx->db->escape($Hash->salt).'\', password=\''.$modx->db->escape($Hash->hash).'\' WHERE id='.$modx->getLoginUserID();
 $rs = mysql_query($sql);
 if(!$rs){
 	echo "An error occured while attempting to save the new password.";
 	exit;
 }
+
+$_SESSION['mgrHashtype'] = CLIPPER_HASH_PREFERRED;
 
 $header="Location: index.php?a=7";
 header($header);
