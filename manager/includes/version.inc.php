827bf3e3981bd422126e8a40bc75650337543873
Jason Coward <jason@opengeek.com>
2013-01-07 22:08:06 -0700

    Update changelog and bump version for 1.0.8 release

--- manager/includes/version.inc.php
+++ manager/includes/version.inc.php
@@ -1,5 +1,5 @@
 <?php
-$modx_version = '1.0.7';           // Current version number
-$modx_release_date = 'Nov 26, 2012'; // Date of release
+$modx_version = '1.0.8';           // Current version number
+$modx_release_date = 'Jan 08, 2013'; // Date of release
 $modx_branch = 'Evolution';        // Codebase name
 $modx_full_appname = 'MODx '.$modx_branch.' '.$modx_version.' ('.$modx_release_date.')';
