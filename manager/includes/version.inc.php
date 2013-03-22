97e1e9c77f196a4fa6c80b78a7f3d9fa70e1cabb
Dmi3yy <dmi3yy@gmail.com>
2013-03-22 14:39:48 +0200

    ready 1.0.10RC

--- manager/includes/version.inc.php
+++ manager/includes/version.inc.php
@@ -1,5 +1,5 @@
 <?php
 $modx_version = '1.0.10RC';           // Current version number
-$modx_release_date = 'Mar 21, 2013'; // Date of release
+$modx_release_date = 'Mar 22, 2013'; // Date of release
 $modx_branch = 'Evolution';        // Codebase name
 $modx_full_appname = 'MODx '.$modx_branch.' '.$modx_version.' ('.$modx_release_date.')';
