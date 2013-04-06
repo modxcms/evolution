558386cbd03cd688044cbe10a23cdac90583f3f4
yama <yamamoto@kyms.jp>
2013-04-06 10:47:07 +0900

    Fix (Related https://github.com/dmi3yy/evolution/commit/1785f2d551abbec805817819354f592dfce80873)

--- manager/actions/mutate_settings.dynamic.php
+++ manager/actions/mutate_settings.dynamic.php
@@ -964,6 +964,7 @@ function confirmLangChange(el, lkey, elupd){
       			$dir = dir("media/style/");
       			while ($file = $dir->read()) {
       				if($file!="." && $file!=".." && is_dir("media/style/$file") && substr($file,0,1) != '.') {
+      					if($file==='common') continue;
       					$themename = $file;
       					$selectedtext = $themename==$manager_theme ? "selected='selected'" : "" ;
       	            	echo "<option value='$themename' $selectedtext>".ucwords(str_replace("_", " ", $themename))."</option>";
