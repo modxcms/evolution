b75cd77a970750e6ed925f5446176abe0d5b82e4
yama <yamamoto@kyms.jp>
2013-04-10 12:12:41 +0900

    Improved for manager theme (Small fix)

--- manager/actions/user_management.static.php
+++ manager/actions/user_management.static.php
@@ -144,7 +144,7 @@ echo $cm->render();
 	$grd->columns=$_lang["icon"].",".$_lang["name"].",".$_lang["user_full_name"].",".$_lang['role'].",".$_lang["email"].",".$_lang["user_gender"].",".$_lang["user_block"];
 	$grd->colWidths="34,,,,,40,34";
 	$grd->colAligns="center,,,,,center,center";
-	$grd->colTypes='template:<a class="gridRowIcon" href="#" onclick="return showContentMenu([+id+],event);" title="'.$_lang['click_to_context'].'"><img src="media/style/'.$manager_theme.'/images/icons/user.gif" width="18" height="18" /></a>||template:<a href="index.php?a=12&id=[+id+]" title="'.$_lang['click_to_edit_title'].'">[+value+]</a>';
+	$grd->colTypes='template:<a class="gridRowIcon" href="#" onclick="return showContentMenu([+id+],event);" title="'.$_lang['click_to_context'].'"><img src="media/style/'.$manager_theme.'/images/icons/user.gif" /></a>||template:<a href="index.php?a=12&id=[+id+]" title="'.$_lang['click_to_edit_title'].'">[+value+]</a>';
 	if($listmode=='1') $grd->pageSize=0;
 	if($_REQUEST['op']=='reset') $grd->pageNumber = 1;
 	// render grid
--- manager/frames/menu.php
+++ manager/frames/menu.php
@@ -223,7 +223,7 @@ if($modx->hasPermission('help')) { ?>
 </div>
 
 <form name="menuForm" action="l4mnu.php" class="clear">
-    <input name="sessToken" id="sessTokenInput" value="<?php echo md5(session_id());?>" />
+    <input type="hidden" name="sessToken" id="sessTokenInput" value="<?php echo md5(session_id());?>" />
 <div id="Navcontainer">
 <div id="divNav">
 	<ul id="nav">
