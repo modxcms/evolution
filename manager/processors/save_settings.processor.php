4e75f3ac226c672373118948eaf2972052ff14b3
yama <yamamoto@kyms.jp>
2013-04-17 14:35:29 +0900

    Fix 023bd1c4ddd4ed7e0a65058c225eb03eddb2a24b ( http://tracker.modx.com/issues/9802 )

--- manager/processors/save_settings.processor.php
+++ manager/processors/save_settings.processor.php
@@ -10,7 +10,7 @@ unset($_POST);
 
 if (isset($data) && count($data) > 0) {
 	$savethese = array();
-	$data['sys_files_checksum'] = $modx->manager->getSystemSnapshot();
+	$data['sys_files_checksum'] = $modx->manager->getSystemChecksum($data['check_files_onlogin']);
 	foreach ($data as $k => $v) {
 		switch ($k) {
             case 'settings_version':{
