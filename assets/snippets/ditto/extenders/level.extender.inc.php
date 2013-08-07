6305e308d305d7f857861c82a0bf0f209c52e816
Dmi3yy <dmi3yy@gmail.com>
2013-08-08 00:16:00 +0300

    add level ditto extender

---
+++ assets/snippets/ditto/extenders/level.extender.inc.php
@@ -0,0 +1,18 @@
+﻿<?php
+/* Вывод документов определенного уровня 
+Параметр &level — уровень от корня сайта
+*/
+
+$GLOBALS["level"] = isset($level) ? (int)$level : 1;
+$filters["custom"]["levelFilter"] = array("id","levelFilter"); 
+if (!function_exists("levelFilter")) {
+	function levelFilter($resource) {
+		global $modx,$level;
+		if (count($modx->getParentIds($resource['id'])) == $level) {
+			return 1;
+		} else {
+			return 0;
+		}
+	}
+}
+?>
