fe05e46b921c72fcbcc19523c101fcb08d2d50a4
Dmi3yy <dmi3yy@gmail.com>
2013-08-09 20:23:57 +0300

    add to tinyMCE normal work with iframe

--- assets/plugins/tinymce/js/xconfig.js
+++ assets/plugins/tinymce/js/xconfig.js
@@ -22,6 +22,7 @@ var mce_valid_elements = "@[id|class|style|title|dir<ltr?rtl|lang|xml::lang|oncl
 + "input[accept|alt|checked|disabled|maxlength|name|readonly|size|src|type|value],"
 + "kbd,label[for],legend,noscript,optgroup[label|disabled],option[disabled|label|selected|value],"
 + "q[cite],samp,select[disabled|multiple|name|size],small,"
++ "iframe[src|title|width|height|allowfullscreen|frameborder|class|id],object[classid|width|height|codebase|*],param[name|value|_value|*],embed[type|width|height|src|*]"
 + "textarea[cols|rows|disabled|name|readonly],tt,var,big";
 var mce_extended_valid_elements = "";
 var mce_invalid_elements = "";
