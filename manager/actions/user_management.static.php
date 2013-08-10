d356741a4fa751e1da5754646d01c0e3f3da5aac
Dmi3yy <dmi3yy@gmail.com>
2013-08-10 16:06:40 +0300

    fix html in user_management

--- manager/actions/user_management.static.php
+++ manager/actions/user_management.static.php
@@ -88,7 +88,7 @@ echo $cm->render();
 <input type="hidden" name="op" value="" />
 
 <h1><?php echo $_lang['user_management_title']; ?></h1>
-
+<div class="section">
 <div class="sectionBody">
 	<p><?php echo $_lang['user_management_msg']; ?></p>
 	<div class="searchbar">
@@ -151,5 +151,5 @@ echo $cm->render();
 	echo $grd->render();
 	?>
 	</div>
-</div>
+</div></div>
 </form>
