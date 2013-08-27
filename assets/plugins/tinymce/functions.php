c0e1bec1ebb7960b1569b79a1875731b9109703f
yama <yamamoto@kyms.jp>
2013-08-27 15:41:47 +0900

    Fix - TinyMCE onchange_callback

--- assets/plugins/tinymce/functions.php
+++ assets/plugins/tinymce/functions.php
@@ -436,7 +436,7 @@ class TinyMCE
 		$ph['date_format']             = $modx->toDateFormat(null, 'formatOnly');
 		$ph['time_format']             = '%H:%M:%S';
 		$ph['entity_encoding']         = $params['entity_encoding'];
-		$ph['onchange_callback']       = ($params['frontend']!==false)? "'myCustomOnChangeHandler'" : 'false';
+		$ph['onchange_callback']       = "'OnChangeHandler'";
 		$ph['terminate']               = (!empty($params['customparams'])) ? ',' : '';
 		$ph['customparams']            = rtrim($params['customparams'], ',');
 		$content_css[] = "{$mce_url}style/content.css";
