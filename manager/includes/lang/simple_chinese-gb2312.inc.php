7e6b9e2477863b8e64f396e60c2c86c8ef40bd20
yama <yamamoto@kyms.jp>
2013-08-31 20:03:18 +0900

    Fix @6fcd467 http://tracker.modx.com/issues/9863

--- manager/includes/lang/simple_chinese-gb2312.inc.php
+++ manager/includes/lang/simple_chinese-gb2312.inc.php
@@ -1,7 +1,9 @@
 <?php
     $filename = dirname(__FILE__) . '/simple_chinese-gb2312-utf8.inc.php';
     $contents = file_get_contents($filename);
-    $contents = mb_convert_encoding($contents, 'HTML-ENTITIES', 'UTF-8');
-    $contents = str_replace('UTF-8', 'gb2312', $contents);
+    $contents = mb_convert_encoding($contents, 'EUC-CN', 'UTF-8');
+//    $contents = str_replace('UTF-8', 'gb2312', $contents);
     eval('?>' . $contents);
+    $modx_lang_attribute = 'zh'; // Manager HTML and XML Language Attribute
+    $modx_manager_charset = 'gb2312';
 ?>
