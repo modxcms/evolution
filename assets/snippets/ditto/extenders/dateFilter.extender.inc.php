35ef716784cd0deba095159e05cefabe8598929a
yama <yamamoto@kyms.jp>
2013-09-04 18:13:24 +0900

    Fix - ditto/extenders/dateFilter.extender.inc.php

--- assets/snippets/ditto/extenders/dateFilter.extender.inc.php
+++ assets/snippets/ditto/extenders/dateFilter.extender.inc.php
@@ -81,7 +81,7 @@ $dateSource = isset($dateSource) ? $dateSource : "createdon";
 	Related:
 	- <dateFormat>
 */
-$default = isset($dateFilterDefault) ? $dateFilterDefault : 0;
+if(!isset($dateFilterDefault)) $dateFilterDefault = 0;
 /*
 	Param: dateFilterDefault
 
