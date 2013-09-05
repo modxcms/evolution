ad1a8f48407d370968761734bf6a52363027b886
Dmi3yy <dmi3yy@gmail.com>
2013-09-05 18:26:22 +0300

    fix version

--- manager/includes/version.inc.php
+++ manager/includes/version.inc.php
@@ -1,5 +1,5 @@
 <?php
-$modx_version = '1.0.11RC2';           // Current version number
-$modx_release_date = 'Sep 01, 2013'; // Date of release
+$modx_version = '1.0.11RC3';           // Current version number
+$modx_release_date = 'Sep 05, 2013'; // Date of release
 $modx_branch = 'Evolution';        // Codebase name
 $modx_full_appname = 'MODX '.$modx_branch.' '.$modx_version.' ('.$modx_release_date.')';
