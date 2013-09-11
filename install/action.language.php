71edebe55e850f915767e20a4eb1de0d207ab38f
yama <yamamoto@kyms.jp>
2013-09-11 22:39:35 +0900

    Fix font problem at the new install

--- install/action.language.php
+++ install/action.language.php
@@ -9,7 +9,7 @@ if( $handle = opendir('lang/') ) {
 sort( $langs );
 ?>
 <form name="install" id="install_form" action="index.php?action=mode" method="post">
-    <h2>Choose language:&nbsp;&nbsp;
+    <h2 style="display:inline;">Choose language:&nbsp;&nbsp;</h2>
     <select name="language">
 <?php
 foreach ($langs as $language) {
@@ -17,7 +17,7 @@ foreach ($langs as $language) {
 	echo '<option value="' . $language . '"'. ( ($abrv_language[0] == 'english') ? ' selected="selected"' : null ) .'>' . ucwords( $abrv_language[0] ). '</option>'."\n";
 }
 ?>
-    </select></h2>
+    </select>
         <p class="buttonlinks">
             <a style="display:inline;" href="javascript:document.getElementById('install_form').submit();" title="<?php echo $_lang['begin']?>"><span><?php echo $_lang['btnnext_value']?></span></a>
         </p>
