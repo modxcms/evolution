9ec4beb949261cfd36bc7fd98d9ecb5d39ee0680
Thomas Jakobi <thomas.jakobi@partout.info>
2013-12-30 16:18:45 +0100

    Updated/Compared with Transfix entries

--- assets/snippets/ditto/lang/francais-utf8.inc.php
+++ assets/snippets/ditto/lang/francais-utf8.inc.php
@@ -1,26 +1,26 @@
 <?php
-/*
- * Title: Ditto French Language File
- * Desc: Aggregates documents to create blogs, article/news
- * 		 collections, etc.,with full support for templating.
- * About: French language file for Ditto.
- * Author: Mark Kaplan, French Translation : David Mollière, Correction: Jean-Christophe Brebion (28/07/09)
- * Note: New language keys should added at the bottom of this page
- * Version: 2.0 RC1
+/**
+ * Title: Language File
+ * Purpose: Default French language file for Ditto
+ * Author: David Mollière, Jean-Christophe Brebion
+ *
+ * Please commit your language changes on Transifex (https://www.transifex.com/projects/p/modx-evolution/) or on GitHub (https://github.com/modxcms/evolution).
  */
 $_lang['language'] = "french";
 $_lang['abbr_lang'] = "fr";
 $_lang['file_does_not_exist'] = "n\'existe pas. Merci de vérifier le fichier.";
-$_lang['default_template'] = <<<TPL
+$_lang['extender_does_not_exist'] = "extender does not exist. Please check it.";
+$_lang['default_template'] = '
     <div class="ditto_item" id="ditto_item_[+id+]">
         <h3 class="ditto_pageTitle"><a href="[~[+id+]~]">[+pagetitle+]</a></h3>
         <div class="ditto_introText">[+introtext+]</div>
         <div class="ditto_documentInfo">par <strong>[+author+]</strong> le [+createdon:date=`%d-%b-%y %H:%M`+]</div>
     </div>
-TPL;
+';
+$_lang["bad_tpl"] = "<p>&[+tpl+] either does not contain any placeholders or is an invalid chunk name, code block, or filename. Please check it.</p>";
+$_lang['missing_placeholders_tpl'] = 'One of your Ditto templates are missing placeholders, please check the template below:';
 $_lang['no_documents'] = '<p>Aucune Ressource trouvée.</p>';
 $_lang['resource_array_error'] = 'Resource Array Error';
- 
 $_lang['prev'] = "&lt; Précédent";
 $_lang['next'] = "Suivant &gt;";
 $_lang['button_splitter'] = "|";
@@ -28,51 +28,32 @@ $_lang['default_copyright'] = "[(site_name)] 2009";
 $_lang['invalid_class'] = "La classe Ditto est invalide. Merci de la vérifier.";
 $_lang['none'] = "Aucun";
 $_lang['edit'] = "Éditer";
-$_lang['yes'] = "Oui";
-$_lang['no'] = "Non";
+$_lang['dateFormat'] = "%d-%b-%y %H:%M";
+
+// Debug Tab Names
+$_lang['info'] = "Info";
+$_lang['modx'] = "MODX";
+$_lang['fields'] = "Fields";
+$_lang['templates'] = "Templates";
+$_lang['filters'] = "Filters";
+$_lang['prefetch_data'] = "Prefetch Data";
+$_lang['retrieved_data'] = "Retreived Data";
+
+// Debug Text
+$_lang['placeholders'] = "Placeholders";
 $_lang['params'] = "Paramètres";
-$_lang['debug_head'] = "
-<h2>Ditto version [+version+]</h2>
-<h3>Information de debug</h3>
-Nombre de Ressources à afficher: [+summarize+]<br />
-Nombre de Ressources extraites de la base: [+recordCount+]<br />
-Trier par: [+sortBy+]<br />
-Direction du tri: [+sortDir+]<br />
-Commencer à l'item [+start+] et arrêter à [+stop+] parmi [+total+]<br />
-Prefetch: [+prefetch+]<br />
-<h3>IDs</h3>
-[+ids+]<br />
-<h3>Paramètres du Snippet</h3>
-[+call+]<br />
-<h3>Filtres</h3>
-[+filter+]<br />
-<h3>Champs</h3>
-<div class='ditto_dbg_fields'>
-[+fields+]
-</div><br />
-<h3>Données de la Ressource</h3>
-";
-$_lang["debug_styles"] = "
-<style>
-  .debug {
-  	border: 1px solid #888;
-  	border-left: 5px solid #888;
-  	background-color: white;
-  	padding: 3px !important;
-  	margin: 5px 3px !important;
-  }        
- 	table { border: 1px solid #888; margin: 0; padding: 0;}
-	table td table {border: 0;}
-	table th td {border: 1px solid #888;}
-	table td { background-color:#FFFFFF; padding: 2px;}
-	table th { background-color:#888; padding: 2px; border: 1px solid #888;}
-	table td th { background-color:#008CBA; color: white; border: 1px solid #888; }
-	table td {vertical-align: top !important;}
-	.ditto_dbg_fields table table table { width: 60px; display: block}
-	.ditto_dbg_fields table table table td{display: block; float: left;}
-	table table tr td tr td{border: 1px solid #888; }
-	
-</style>
-";
-$_lang['debug_item'] = "[+pagetitle+] ([+id+])";
-?>
+$_lang['basic_info'] = "Basic Info";
+$_lang['document_info'] = "Document Info";
+$_lang['debug'] = "Debug";
+$_lang['version'] = "Version";
+$_lang['summarize'] = "Summarize";
+$_lang['total'] = "Total";
+$_lang['sortBy'] = "Trier par";
+$_lang['sortDir'] = "Direction du tri";
+$_lang['start'] = "Commencer à";
+$_lang['stop'] = "Arrêter à ";
+$_lang['ditto_IDs'] = "IDs";
+$_lang['ditto_IDs_selected'] = "Selected IDs";
+$_lang['ditto_IDs_all'] = "All IDs";
+$_lang['open_dbg_console'] = "Open Debug Console";
+$_lang['save_dbg_console'] = "Save Debug Console";
