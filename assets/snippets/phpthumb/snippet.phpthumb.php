779f3791b1a195dcaafab7e827c20c5abdd36956
Dmi3yy <dmi3yy@gmail.com>
2014-02-06 12:25:31 +0200

    phpthumb 1.2   (not  include phpthumb class if have resized images)

--- assets/snippets/phpthumb/snippet.phpthumb.php
+++ assets/snippets/phpthumb/snippet.phpthumb.php
@@ -3,42 +3,41 @@ if(!defined('MODX_BASE_PATH')){die('What are you doing? Get out of here!');}
 //[[phpthumb? &input=`[+image+]` &options=`w=150,h=76,far=C,bg=FFFFFF`]]
 //Author: Bumkaka
 
-$base="assets/cache/images";   // тут можно менять путь
+$cacheFolder=isset($cacheFolder) ? $cacheFolder : "assets/cache/images";   // тут можно менять путь
 
-if($input === '' || !file_exists(MODX_BASE_PATH . $input)){
-  $input = 'assets/snippets/phpthumb/noimage.png';
+if(empty($input) || !file_exists(MODX_BASE_PATH . $input)){
+    $input = isset($noImage) ? $noImage : 'assets/snippets/phpthumb/noimage.png';
 }
 
-  $options = 'f=jpg&q=96&'.strtr($options, Array("," => "&", "_" => "=", '{' => '[', '}' => ']'));
-  $path_parts=pathinfo($input);
-  require_once MODX_BASE_PATH.'assets/snippets/phpthumb/phpthumb.class.php';
-  $phpThumb = new phpthumb();
-  $phpThumb->setSourceFilename(MODX_BASE_PATH . $input);
+$options = 'f=jpg&q=96&'.strtr($options, Array("," => "&", "_" => "=", '{' => '[', '}' => ']'));
+parse_str($options, $params);
 
-  parse_str($options, $params);
-  foreach ($params as $key => $value) {
-    $phpThumb->setParameter($key, $value);
-  }
-
-  $tmp=str_replace(MODX_BASE_PATH . "assets/images","",$path_parts['dirname']);
-  $tmp=str_replace("assets/images","",$tmp);
-  $tmp=explode("/",$tmp);
-  $folder=$base;  
+$path_parts=pathinfo($input);
+$tmpFolder=str_replace(MODX_BASE_PATH . "assets/images","",$path_parts['dirname']);
+$tmpFolder=str_replace("assets/images","",$tmpFolder);
+$tmpFolder=explode("/",$tmpFolder);
   
-  for($i=0;$i<count($tmp);$i++){
-    if ($tmp[$i]=='') continue;
-    $folder.="/".$tmp[$i];
-    if(!is_dir(MODX_BASE_PATH.$folder)) mkdir(MODX_BASE_PATH.$folder);
-  }
+foreach ($tmpFolder as $folder) {
+    if (!empty($folder)) {
+        $cacheFolder.="/".$folder;
+        if(!is_dir(MODX_BASE_PATH.$cacheFolder)) mkdir(MODX_BASE_PATH.$cacheFolder);
+    }
+}
   
-  $fname=$folder."/".$params['w']."x".$params['h'].'-'.$path_parts['filename'].".".substr(md5(serialize($params)),0,3).".".$params['f'];
-  $outputFilename =MODX_BASE_PATH.$fname;
-  if (!file_exists($outputFilename)) {
-	  if ($phpThumb->GenerateThumbnail()) {
-		  $phpThumb->RenderToFile($outputFilename);
-	  } else {
-		  $modx->logEvent(0, 3, implode('<br/>', $phpThumb->debugmessages), 'phpthumb');
-	  }
-  }
-  return $fname;
+$fname=$cacheFolder."/".$params['w']."x".$params['h'].'-'.$path_parts['filename'].".".substr(md5(serialize($params)),0,3).".".$params['f'];
+$outputFilename =MODX_BASE_PATH.$fname;
+if (!file_exists($outputFilename)) {
+    require_once MODX_BASE_PATH.'assets/snippets/phpthumb/phpthumb.class.php';
+    $phpThumb = new phpthumb();
+    $phpThumb->setSourceFilename(MODX_BASE_PATH . $input);
+    foreach ($params as $key => $value) {
+        $phpThumb->setParameter($key, $value);
+    }
+	if ($phpThumb->GenerateThumbnail()) {
+        $phpThumb->RenderToFile($outputFilename);
+	} else {
+        $modx->logEvent(0, 3, implode('<br/>', $phpThumb->debugmessages), 'phpthumb');
+    }
+}
+return $fname;
 ?>
