6d7732e454a152b24901dcf612f539d36100876c
Segr <segr@nashalife.ru>
2014-02-27 13:57:42 +0400

    Check 'cls' & 'id' when delete event_logs

--- manager/processors/delete_eventlog.processor.php
+++ manager/processors/delete_eventlog.processor.php
@@ -4,12 +4,15 @@ if(!$modx->hasPermission('delete_eventlog')) {
 	$modx->webAlertAndQuit($_lang["error_no_privileges"]);
 }
 
-$id = isset($_GET['id'])? intval($_GET['id']) : 0;
-if($id==0) {
-	$modx->webAlertAndQuit($_lang["error_no_id"]);
+if (isset($_GET['cls']) && $_GET['cls']==1) {
+	$where = '';
+} else {
+	$id = isset($_GET['id'])? intval($_GET['id']) : 0;
+	if($id==0) {
+		$modx->webAlertAndQuit($_lang["error_no_id"]);
+	}
+	$where = "id='{$id}'";
 }
-
-$where = (isset($_GET['cls']) && $_GET['cls']==1) ? '' : "id='{$id}'";
 
 // delete event log
 $modx->db->delete($modx->getFullTableName('event_log'), $where);
