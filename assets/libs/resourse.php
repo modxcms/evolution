145d97727dfc2fd9ecd3e5d1b8bf098c9da6b956
Pavel Yakimov <yakimov42@yandex.ru>
2014-03-09 12:27:02 +0700

    typo

--- assets/libs/resourse.php
+++ assets/libs/resourse.php
@@ -60,7 +60,7 @@ class resourse {
 	private $tvid = array();
 	private $log = array();
 	private $edit = 0;
-	private $dafeult_field ;
+	private $default_field ;
 	private $table=array('"'=>'_',"'"=>'_',' '=>'_','.'=>'_',','=>'_','а'=>'a','б'=>'b','в'=>'v',
 		'г'=>'g','д'=>'d','е'=>'e','ё'=>'e','ж'=>'zh','з'=>'z','и'=>'i','й'=>'y','к'=>'k',
 		'л'=>'l','м'=>'m','н'=>'n','о'=>'o','п'=>'p','р'=>'r','с'=>'s','т'=>'t','у'=>'u',
