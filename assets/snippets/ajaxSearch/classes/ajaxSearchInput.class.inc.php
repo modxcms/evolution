766e662229e7eb721d34f5a2d57e4ee94cc9608e
yama <yamamoto@kyms.jp>
2014-06-07 09:03:09 +0900

    Fix for CJK multi byte environment

--- assets/snippets/ajaxSearch/classes/ajaxSearchInput.class.inc.php
+++ assets/snippets/ajaxSearch/classes/ajaxSearchInput.class.inc.php
@@ -226,6 +226,8 @@ class AjaxSearchInput {
             $searchString = $this->_stripHtml($searchString);
 
             $searchString = $this->_htmlspecialchars($searchString, ENT_COMPAT, $pgCharset, False);
+
+            if(function_exists('mb_convert_kana')) $searchString = mb_convert_kana($searchString, 's', $pgCharset);
         }
         return $searchString;
     }
