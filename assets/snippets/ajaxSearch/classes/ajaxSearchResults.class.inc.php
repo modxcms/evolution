916dde4d2cec6d86ba8f1b98c0ffbd3c7d4578d1
yama <yamamoto@kyms.jp>
2014-06-22 12:06:35 +0900

    Revert missing commit
    https://github.com/modxcms/evolution/commit/2c4d02d89510637d910151f4868fe2e8c0477ead

--- assets/snippets/ajaxSearch/classes/ajaxSearchResults.class.inc.php
+++ assets/snippets/ajaxSearch/classes/ajaxSearchResults.class.inc.php
@@ -805,10 +805,6 @@ class AjaxSearchResults {
     */
     function _getChildIds($Ids, $depth) {
         global $modx;
-        if(method_exists($modx, 'setdocumentMap') && !isset($modx->documentMap))
-        {
-            $modx->setdocumentMap();
-        }
         $depth = intval($depth);
         $kids = array();
         $docIds = array();
