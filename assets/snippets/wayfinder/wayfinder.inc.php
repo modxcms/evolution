102fe60fc61b25dcfd7bd80c042d94504dab7551
Thomas Jakobi <thomas.jakobi@partout.info>
2014-06-25 09:00:13 +0200

    Fixing #226 - Wayfinder problems after upgrading MODX from 1.0.10 to 1.0.13

--- assets/snippets/wayfinder/wayfinder.inc.php
+++ assets/snippets/wayfinder/wayfinder.inc.php
@@ -23,24 +23,31 @@ class Wayfinder {
 	var $debugInfo = array();
 	
 	function __construct() {
-		$_[] = '[+wf.wrapper+]';
-		$_[] = '[+wf.classes+]';
-		$_[] = '[+wf.classnames+]';
-		$_[] = '[+wf.link+]';
-		$_[] = '[+wf.title+]';
-		$_[] = '[+wf.linktext+]';
-		$_[] = '[+wf.id+]';
-		$_[] = '[+wf.alias+]';
-		$_[] = '[+wf.attributes+]';
-		$_[] = '[+wf.docid+]';
-		$_[] = '[+wf.introtext+]';
-		$_[] = '[+wf.description+]';
-		$_[] = '[+wf.subitemcount+]';
-		$this->placeHolders['rowLevel'] = $_;
-		$this->placeHolders['wrapperLevel'] = array('[+wf.wrapper+]','[+wf.classes+]','[+wf.classnames+]','[+wf.level+]');
-		$this->placeHolders['tvs']          = array();
-		
-	}
+        $this->placeHolders = array(
+            'rowLevel' => array(
+                '[+wf.wrapper+]',
+                '[+wf.classes+]',
+                '[+wf.classnames+]',
+                '[+wf.link+]',
+                '[+wf.title+]',
+                '[+wf.linktext+]',
+                '[+wf.id+]',
+                '[+wf.alias+]',
+                '[+wf.attributes+]',
+                '[+wf.docid+]',
+                '[+wf.introtext+]',
+                '[+wf.description+]',
+                '[+wf.subitemcount+]'
+            ),
+            'wrapperLevel' => array(
+                '[+wf.wrapper+]',
+                '[+wf.classes+]',
+                '[+wf.classnames+]',
+                '[+wf.level+]'
+            ),
+            'tvs' => array()
+        );
+    }
 	
 	function run() {
 		global $modx;
@@ -216,21 +223,20 @@ class Wayfinder {
 		} else {
 			$phArray = array($useSub,$useClass,$classNames,$resource['link'],$resource['title'],$resource['linktext'],$useId,$resource['alias'],$resource['link_attributes'],$resource['id'],$resource['introtext'],$resource['description'],$numChildren);
 		}
+        $usePlaceholders = $this->placeHolders['rowLevel'];
         //Add document variables to the placeholder array
         foreach ($resource as $dvName => $dvVal) {
-            $this->placeHolders['rowLevel'][] = "[+".$dvName."+]";
+            $usePlaceholders[] = '[+' . $dvName . '+]';
             $phArray[] = $dvVal;
         }
-		//If tvs are used add them to the placeholder array
-		if (!empty($this->tvList)) {
-			$usePlaceholders = array_merge($this->placeHolders['rowLevel'],$this->placeHolders['tvs']);
-			foreach ($this->tvList as $tvName) {
-				$phArray[] = $resource[$tvName];
-			}
-		} else {
-			$usePlaceholders = $this->placeHolders['rowLevel'];
-		}
-		//Debug
+        //If tvs are used add them to the placeholder array
+        if (!empty($this->tvList)) {
+            $usePlaceholders = array_merge($usePlaceholders, $this->placeHolders['tvs']);
+            foreach ($this->tvList as $tvName) {
+                $phArray[] = $resource[$tvName];
+            }
+        }
+        //Debug
 		if ($this->_config['debug']) {
 			$debugDocInfo = array();
 			$debugDocInfo['template'] = $usedTemplate;
