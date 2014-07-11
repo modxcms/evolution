abc925a39b590c268ef91865c2d0d9c6db450dcd
Dmi3yy <dmi3yy@gmail.com>
2014-07-11 09:58:19 +0300

    revert  Use preg_replace with /e to make it evaluate PHP

--- manager/includes/document.parser.class.inc.php
+++ manager/includes/document.parser.class.inc.php
@@ -1288,7 +1288,7 @@ class DocumentParser {
 				$aliases[$val] = $key;
 				$isfolder[$val] = $this->aliasListing[$val]['isfolder'];
             }
-            $in= '!\[\~([0-9]+)\~\]!is';
+            $in= '!\[\~([0-9]+)\~\]!ise'; // Use preg_replace with /e to make it evaluate PHP
             $isfriendly= ($this->config['friendly_alias_urls'] == 1 ? 1 : 0);
             $pref= $this->config['friendly_url_prefix'];
             $suff= $this->config['friendly_url_suffix'];
