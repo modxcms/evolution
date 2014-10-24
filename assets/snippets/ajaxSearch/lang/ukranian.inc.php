9a0eb01695dee7bea07fc9153f5c18813af3de8d
Dmi3yy <dmi3yy@gmail.com>
2014-10-24 20:52:58 +0300

    add ukranian lang for eFrom and AjaxSearch

---
+++ assets/snippets/ajaxSearch/lang/ukranian.inc.php
@@ -0,0 +1,35 @@
+<?php
+/*
+ukranian.inc.php - for AjaxSearch 1.10.1
+Created by: Marina Ivanova & Webber
+Last Modified on 2014-10-15
+Description: Language strings for AjaxSearch
+*/
+$_lang['as_resultsIntroFailure'] = 'За вашим запитом нічого не знайдено. Спробуйте ввести схожі за змістом слова, щоб отримати кращий результат.';
+$_lang['as_searchButtonText'] = 'Знайти!';
+$_lang['as_boxText'] = 'Запит для пошуку ...';
+$_lang['as_introMessage'] = 'Введіть ваш запит для початку пошуку.';
+$_lang['as_resultsFoundTextSingle'] = '%d знайдений за запитом &laquo;%s&raquo;. ';
+$_lang['as_resultsFoundTextMultiple'] = '%d знайдено по запиту &laquo;%s&raquo;. ';
+$_lang['as_paginationTextSinglePage'] = '';
+$_lang['as_paginationTextMultiplePages'] = 'Сторінки з результатами пошуку:';
+$_lang['as_moreResultsText'] = 'Клацніть тут мишею, щоб побачити всі результати.';
+$_lang['as_moreResultsTitle'] = 'Всі результати пошуку';
+$_lang['as_maxWords'] = 'В пошуковому запиті допустимо максимум %d слів';
+$_lang['as_minChars'] = 'Необхідно не менше %d символів в запиті для пошуку';
+$_lang['as_maxChars'] = 'В пошуковому запиті допустимо максимум %d символів';
+$_lang['oneword'] = 'Одне слово';
+$_lang['allwords'] = 'Всі слова';
+$_lang['exactphrase'] = 'Точна фраза';
+$_lang['nowords'] = 'Немає слів';
+$_lang['as_cmtHiddenFieldIntro'] = 'Це поле повинно бути порожнім. <br /> Утримайтеся від введення в нього тексту. ';
+$_lang['as_cmtIntroMessage'] = 'Ви знайшли те, що шукали? Залиште тут свій коментар! ';
+$_lang['as_cmtSubmitText'] = 'Відправити';
+$_lang['as_cmtResetText'] = 'Очистити';
+$_lang['as_cmtThksMessage'] = 'Дякуємо за ваш коментар!';
+$_lang['as_resultsTextSingle'] = '%d знайдений. ';
+$_lang['as_resultsTextMultiple'] = '%d знайдено. ';
+$_lang['as_grpResultsDisplayedText'] = '(Результати %d &ndash; %d з %d)';
+$_lang['as_resultsDisplayed'] = 'Показано: %d.';
+$_lang['as_paging1Text'] = '%d &ndash; %d/%d ';
+$_lang['as_paging2Text'] = 'Показати решту %d';
