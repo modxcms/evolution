c3f4253fb5f1fa8511d5e7b64982e2744041fce1
Thomas Jakobi <thomas.jakobi@partout.info>
2014-11-06 15:43:13 +0100

    Update version.inc.php

--- manager/includes/version.inc.php
+++ manager/includes/version.inc.php
@@ -1,5 +1,5 @@
 <?php
 $modx_version = '1.0.15';           // Current version number
-$modx_release_date = 'Oct 31, 2014'; // Date of release
+$modx_release_date = 'Nov 06, 2014'; // Date of release
 $modx_branch = 'Evolution';        // Codebase name
 $modx_full_appname = 'MODX '.$modx_branch.' '.$modx_version.' ('.$modx_release_date.')';
