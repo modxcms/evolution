fb32017dbd37bb5ed685c63a746e9a4fcd860e06
Jason BIrd <jason@tebd.net>
2014-11-11 22:01:13 -0600

    [#364] Fixed to only set value if field_value is empty

--- manager/includes/tmplvars.inc.php
+++ manager/includes/tmplvars.inc.php
@@ -10,7 +10,7 @@
 	     	$eval_str = trim(substr($default_text, 7));
 	    	$default_text = eval($eval_str);
 	    }
-    	$field_value = $default_text;
+    	$field_value = ($field_value != '') ? $field_value : $default_text;
 	    
 		$field_html ='';
 
