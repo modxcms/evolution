bdf2d37d6534e1811f7eec1ca9f28fee2f3c7b5d
MrSwed <webmaster@collection.com.ua>
2014-11-19 18:39:05 +0300

    [*] word boundary do NOT working with utf8 on some servers Php5.2.17, PCRE version 8.20 2011-10-21 Compiled with UTF-8 support Unicode properties support

--- assets/snippets/ajaxSearch/classes/ajaxSearchResults.class.inc.php
+++ assets/snippets/ajaxSearch/classes/ajaxSearchResults.class.inc.php
@@ -529,7 +529,7 @@ class AjaxSearchResults {
                 $wordLength = $mbStrlen($searchTerm);
                 $wordLength2 = $wordLength / 2;
                 // $pattern = '/' . preg_quote($searchTerm, '/') . $lookAhead . '/' . $pcreModifier;
-                if ($advSearch == EXACTPHRASE) $pattern = '/\b' . preg_quote($searchTerm, '/') . '\b/' . $pcreModifier;
+                if ($advSearch == EXACTPHRASE) $pattern = '/(\b|\W)' . preg_quote($searchTerm, '/') . '(\b|\W)/' . $pcreModifier;
                 else $pattern = '/' . preg_quote($searchTerm, '/') . '/' . $pcreModifier;
                 $matches = array();
                 $nbr = preg_match_all($pattern, $text, $matches, PREG_OFFSET_CAPTURE);
@@ -592,7 +592,7 @@ class AjaxSearchResults {
                 if ($this->asCfg->cfg['highlightResult']) {
                     $rank = $extracts[$i]['rank'];
                     $searchTerm = $searchList[$rank - 1];
-                    if ($advSearch == EXACTPHRASE) $pattern = '/\b' . preg_quote($searchTerm, '/') . '\b/' . $pcreModifier;
+                    if ($advSearch == EXACTPHRASE) $pattern = '/(\b|\W)' . preg_quote($searchTerm, '/') . '(\b|\W)/' . $pcreModifier;
                     else $pattern = '/' . preg_quote($searchTerm, '/') . '/' . $pcreModifier;
                     $subject = '<span class="' . $highlightClass . ' ' . $highlightClass . $rank . '">\0</span>';
                     $extract = preg_replace($pattern, $subject, $extract);
@@ -881,7 +881,7 @@ class AjaxSearchResults {
                     $searchList = $this->asCtrl->getSearchWords($searchString, $advSearch);
                     $pcreModifier = $this->asCfg->pcreModifier;
                     foreach ($searchList as $searchTerm) {
-                        if ($advSearch == EXACTPHRASE) $pattern = '/\b' . preg_quote($searchTerm, '/') . '\b/' . $pcreModifier;
+                        if ($advSearch == EXACTPHRASE) $pattern = '/(\b|\W)' . preg_quote($searchTerm, '/') . '(\b|\W)/' . $pcreModifier;
                         else $pattern = '/' . preg_quote($searchTerm, '/') . '/' . $pcreModifier;
                         $matches = array();
                         $found = preg_match($pattern, $text, $matches, PREG_OFFSET_CAPTURE);
