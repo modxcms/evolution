69c09c5c870d48ee5bade9276c2b7a1c75fb29c8
The Boss <bossloper@users.noreply.github.com>
2015-03-10 09:37:17 +0000

    Update weblogin.inc.php
    HTTP_X_FORWARDED_FOR can return a list of IPs so take the last in the list.
    
    See: http://www.howgeek.com/2011/06/04/_serverhttp_x_forwarded_for-returns-multiple-ips-what-to-do/

--- assets/snippets/weblogin/weblogin.inc.php
+++ assets/snippets/weblogin/weblogin.inc.php
@@ -99,7 +99,7 @@ if(!isset($_SESSION['webValidated'])){
     $output= '';
 
     if (getenv("HTTP_CLIENT_IP")) $ip = getenv("HTTP_CLIENT_IP");
-    else if(getenv("HTTP_X_FORWARDED_FOR")) $ip = getenv("HTTP_X_FORWARDED_FOR");
+    else if(getenv("HTTP_X_FORWARDED_FOR")) $ip = trim(end(explode(',', getenv("HTTP_X_FORWARDED_FOR"))));
     else if(getenv("REMOTE_ADDR")) $ip = getenv("REMOTE_ADDR");
     else $ip = "UNKNOWN";$_SESSION['ip'] = $ip;
 
