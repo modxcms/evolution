63e64142016f6a9a290230890467f3ba734e8096
MrSwed <webmaster@sdcollection.com>
2015-05-31 15:56:17 +0300

    Starting the installation with the language of the user's browser

--- install/action.language.php
+++ install/action.language.php
@@ -14,7 +14,7 @@ sort( $langs );
 <?php
 foreach ($langs as $language) {
     $abrv_language = explode('-',$language);
-	echo '<option value="' . $language . '"'. ( ($abrv_language[0] == $install_language) ? ' selected="selected"' : null ) .'>' . ucwords( $abrv_language[0] ). '</option>'."\n";
+	echo '<option value="' . $language . '"'. ( ($language == $install_language) ? ' selected="selected"' : null ) .'>' . ucwords( $abrv_language[0] ). '</option>'."\n";
 }
 ?>
     </select>
