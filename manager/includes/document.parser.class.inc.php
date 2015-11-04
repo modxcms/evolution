bdb542d7e26e5c140069779cfd1d488589d8582c
dmi3yy <dmi3yy@gmail.com>
2015-11-04 22:39:18 +0200

    refactor BackTrace

--- manager/includes/document.parser.class.inc.php
+++ manager/includes/document.parser.class.inc.php
@@ -3714,207 +3714,193 @@ class DocumentParser {
     }
 
 	function messageQuit($msg= 'unspecified error', $query= '', $is_error= true, $nr= '', $file= '', $source= '', $text= '', $line= '', $output='') {
-		
-	    $version= isset ($GLOBALS['modx_version']) ? $GLOBALS['modx_version'] : '';
+
+		include_once('extenders/maketable.class.php');
+		$MakeTable = new MakeTable();
+		$MakeTable->setTableClass('grid');
+		$MakeTable->setRowRegularClass('gridItem');
+		$MakeTable->setRowAlternateClass('gridAltItem');
+		$MakeTable->setColumnWidths(array('100px'));
+
+		$table = array();
+
+		$version= isset ($GLOBALS['modx_version']) ? $GLOBALS['modx_version'] : '';
 		$release_date= isset ($GLOBALS['release_date']) ? $GLOBALS['release_date'] : '';
-	    $request_uri = "http://".$_SERVER['HTTP_HOST'].($_SERVER["SERVER_PORT"]==80?"":(":".$_SERVER["SERVER_PORT"])).$_SERVER['REQUEST_URI'];
-	    $request_uri = $this->htmlspecialchars($request_uri, ENT_QUOTES, $this->config['modx_charset']);
-	    $ua          = $this->htmlspecialchars($_SERVER['HTTP_USER_AGENT'], ENT_QUOTES, $this->config['modx_charset']);
-	    $referer     = $this->htmlspecialchars($_SERVER['HTTP_REFERER'], ENT_QUOTES, $this->config['modx_charset']);
-	    if ($is_error) {
-	        $str = '<h3 style="color:red">&laquo; MODX Parse Error &raquo;</h3>
-	                <table border="0" cellpadding="1" cellspacing="0">
-	                <tr><td colspan="2">MODX encountered the following error while attempting to parse the requested resource:</td></tr>
-	                <tr><td colspan="2"><b style="color:red;">&laquo; ' . $msg . ' &raquo;</b></td></tr>';
-	    } else {
-	        $str = '<h3 style="color:#003399">&laquo; MODX Debug/ stop message &raquo;</h3>
-	                <table border="0" cellpadding="1" cellspacing="0">
-	                <tr><td colspan="2">The MODX parser recieved the following debug/ stop message:</td></tr>
-	                <tr><td colspan="2"><b style="color:#003399;">&laquo; ' . $msg . ' &raquo;</b></td></tr>';
-	    }
-	
-	    if (!empty ($query)) {
-	        $str .= '<tr><td colspan="2"><div style="font-weight:bold;border:1px solid #ccc;padding:8px;color:#333;background-color:#ffffcd;">SQL &gt; <span id="sqlHolder">' . $query . '</span></div>
-	                </td></tr>';
-	    }
-	
-	    $errortype= array (
-	        E_ERROR             => "ERROR",
-	        E_WARNING           => "WARNING",
-	        E_PARSE             => "PARSING ERROR",
-	        E_NOTICE            => "NOTICE",
-	        E_CORE_ERROR        => "CORE ERROR",
-	        E_CORE_WARNING      => "CORE WARNING",
-	        E_COMPILE_ERROR     => "COMPILE ERROR",
-	        E_COMPILE_WARNING   => "COMPILE WARNING",
-	        E_USER_ERROR        => "USER ERROR",
-	        E_USER_WARNING      => "USER WARNING",
-	        E_USER_NOTICE       => "USER NOTICE",
-	        E_STRICT            => "STRICT NOTICE",
-	        E_RECOVERABLE_ERROR => "RECOVERABLE ERROR",
-	        E_DEPRECATED        => "DEPRECATED",
-	        E_USER_DEPRECATED   => "USER DEPRECATED"
-	    );
-	
+		$request_uri = "http://".$_SERVER['HTTP_HOST'].($_SERVER["SERVER_PORT"]==80?"":(":".$_SERVER["SERVER_PORT"])).$_SERVER['REQUEST_URI'];
+		$request_uri = $this->htmlspecialchars($request_uri, ENT_QUOTES, $this->config['modx_charset']);
+		$ua          = $this->htmlspecialchars($_SERVER['HTTP_USER_AGENT'], ENT_QUOTES, $this->config['modx_charset']);
+		$referer     = $this->htmlspecialchars($_SERVER['HTTP_REFERER'], ENT_QUOTES, $this->config['modx_charset']);
+		if ($is_error) {
+			$str = '<h2 style="color:red">&laquo; MODX Parse Error &raquo;</h2>';
+			if($msg != 'PHP Parse Error') {
+				$str .= '<h3 style="color:red">' . $msg . '</h3>';
+			}
+		} else {
+			$str = '<h2 style="color:#003399">&laquo; MODX Debug/ stop message &raquo;</h2>';
+			$str .= '<h3 style="color:red">'.$msg.'</h3>';
+		}
+
+		if (!empty ($query)) {
+			$str .= '<div style="font-weight:bold;border:1px solid #ccc;padding:8px;color:#333;background-color:#ffffcd;margin-bottom:15px;">SQL &gt; <span id="sqlHolder">' . $query . '</span></div>';
+		}
+
+		$errortype= array (
+			E_ERROR             => "ERROR",
+			E_WARNING           => "WARNING",
+			E_PARSE             => "PARSING ERROR",
+			E_NOTICE            => "NOTICE",
+			E_CORE_ERROR        => "CORE ERROR",
+			E_CORE_WARNING      => "CORE WARNING",
+			E_COMPILE_ERROR     => "COMPILE ERROR",
+			E_COMPILE_WARNING   => "COMPILE WARNING",
+			E_USER_ERROR        => "USER ERROR",
+			E_USER_WARNING      => "USER WARNING",
+			E_USER_NOTICE       => "USER NOTICE",
+			E_STRICT            => "STRICT NOTICE",
+			E_RECOVERABLE_ERROR => "RECOVERABLE ERROR",
+			E_DEPRECATED        => "DEPRECATED",
+			E_USER_DEPRECATED   => "USER DEPRECATED"
+		);
+
 		if(!empty($nr) || !empty($file))
 		{
-			$str .= '<tr><td colspan="2"><b>PHP error debug</b></td></tr>';
 			if ($text != '')
 			{
-				$str .= '<tr><td colspan="2"><div style="font-weight:bold;border:1px solid #ccc;padding:8px;color:#333;background-color:#ffffcd;">Error : ' . $text . '</div></td></tr>';
+				$str .= '<div style="font-weight:bold;border:1px solid #ccc;padding:8px;color:#333;background-color:#ffffcd;margin-bottom:15px;">Error : ' . $text . '</div>';
 			}
 			if($output!='')
 			{
-				$str .= '<tr><td colspan="2"><div style="font-weight:bold;border:1px solid #ccc;padding:8px;color:#333;background-color:#ffffcd;">' . $output . '</div></td></tr>';
+				$str .= '<div style="font-weight:bold;border:1px solid #ccc;padding:8px;color:#333;background-color:#ffffcd;margin-bottom:15px;">' . $output . '</div>';
 			}
-			$str .= '<tr><td valign="top">ErrorType[num] : </td>';
-			$str .= '<td>' . $errortype [$nr] . "[{$nr}]</td>";
-			$str .= '</tr>';
-			$str .= "<tr><td>File : </td><td>{$file}</td></tr>";
-			$str .= "<tr><td>Line : </td><td>{$line}</td></tr>";
+			$table[] = array('ErrorType[num]' , $errortype [$nr] ."[".$nr."]");
+			$table[] = array("File", $file);
+			$table[] = array("Line", $line);
+
 		}
-	    
-	    if ($source != '')
-	    {
-	        $str .= "<tr><td>Source : </td><td>{$source}</td></tr>";
-	    }
-	
-	    $str .= '<tr><td colspan="2"><b>Basic info</b></td></tr>';
-	
-	    $str .= '<tr><td valign="top" style="white-space:nowrap;">REQUEST_URI : </td>';
-	    $str .= "<td>{$request_uri}</td>";
-	    $str .= '</tr>';
-	    
-	    if(isset($_GET['a']))      $action = $_GET['a'];
-	    elseif(isset($_POST['a'])) $action = $_POST['a'];
-	    if(isset($action) && !empty($action))
-	    {
-	    	include_once(MODX_MANAGER_PATH . 'includes/actionlist.inc.php');
-	    	global $action_list;
-	    	if(isset($action_list[$action])) $actionName = " - {$action_list[$action]}";
-	    	else $actionName = '';
-			$str .= '<tr><td valign="top">Manager action : </td>';
-			$str .= "<td>{$action}{$actionName}</td>";
-			$str .= '</tr>';
-	    }
-	    
-	    if(preg_match('@^[0-9]+@',$this->documentIdentifier))
-	    {
-	    	$resource  = $this->getDocumentObject('id',$this->documentIdentifier);
-	    	$url = $this->makeUrl($this->documentIdentifier,'','','full');
-	    	$link = '<a href="' . $url . '" target="_blank">' . $resource['pagetitle'] . '</a>';
-			$str .= '<tr><td valign="top">Resource : </td>';
-			$str .= '<td>[' . $this->documentIdentifier . ']' . $link . '</td></tr>';
-	    }
-	
-	    if(!empty($this->currentSnippet))
-	    {
-	        $str .= "<tr><td>Current Snippet : </td>";
-	        $str .= '<td>' . $this->currentSnippet . '</td></tr>';
-	    }
-	
-	    if(!empty($this->event->activePlugin))
-	    {
-	        $str .= "<tr><td>Current Plugin : </td>";
-	        $str .= '<td>' . $this->event->activePlugin . '(' . $this->event->name . ')' . '</td></tr>';
-	    }
-	
-	    $str .= "<tr><td>Referer : </td><td>{$referer}</td></tr>";
-	    $str .= "<tr><td>User Agent : </td><td>{$ua}</td></tr>";
-	
-	    $str .= "<tr><td>IP : </td>";
-	    $str .= '<td>' . $_SERVER['REMOTE_ADDR'] . '</td>';
-	    $str .= '</tr>';
-	
-	    $str .= '<tr><td colspan="2"><b>Benchmarks</b></td></tr>';
-	
-	    $str .= "<tr><td>MySQL : </td>";
-	    $str .= '<td>[^qt^] ([^q^] Requests)</td>';
-	    $str .= '</tr>';
-	
-	    $str .= "<tr><td>PHP : </td>";
-	    $str .= '<td>[^p^]</td>';
-	    $str .= '</tr>';
-	
-	    $str .= "<tr><td>Total : </td>";
-	    $str .= '<td>[^t^]</td>';
-	    $str .= '</tr>';
-	
-	    $str .= "<tr><td>Memory : </td>";
-	    $str .= '<td>[^m^]</td>';
-	    $str .= '</tr>';
-	    
-	    $str .= "</table>\n";
-	
-	    $totalTime= ($this->getMicroTime() - $this->tstart);
-	
+
+		if ($source != '')
+		{
+			$table[] = array("Source", $source);
+		}
+
+		if(!empty($this->currentSnippet)) {
+			$table[] = array('Current Snippet' , $this->currentSnippet);
+		}
+
+		if(!empty($this->event->activePlugin)) {
+			$table[] = array('Current Plugin' , $this->event->activePlugin . '(' . $this->event->name . ')');
+		}
+
+		$str .= $MakeTable->create($table, array('Error information',''));
+		$str .= "<br />";
+
+		$table = array();
+		$table[] = array('REQUEST_URI' , $request_uri);
+
+		if(isset($_GET['a']))      $action = $_GET['a'];
+		elseif(isset($_POST['a'])) $action = $_POST['a'];
+		if(isset($action) && !empty($action))
+		{
+			include_once(MODX_MANAGER_PATH . 'includes/actionlist.inc.php');
+			global $action_list;
+			if(isset($action_list[$action])) $actionName = " - {$action_list[$action]}";
+			else $actionName = '';
+
+			$table[] = array('Manager action' , $action.$actionName);
+		}
+
+		if(preg_match('@^[0-9]+@',$this->documentIdentifier))
+		{
+			$resource  = $this->getDocumentObject('id',$this->documentIdentifier);
+			$url = $this->makeUrl($this->documentIdentifier,'','','full');
+			$table[] = array('Resource', '['.$this->documentIdentifier.'] <a href="'.$url.'" target="_blank">'.$resource['pagetitle'].'</a>');
+		}
+		$table[] = array('Referer' , $referer);
+		$table[] = array('User Agent' , $ua);
+		$table[] = array('IP' , $_SERVER['REMOTE_ADDR']);
+		$table[] = array('Current time' , date("Y-m-d H:i:s", time() + $this->config['server_offset_time']));
+		$str .= $MakeTable->create($table, array('Basic info',''));
+		$str .= "<br />";
+
+		$table = array();
+		$table[] = array('MySQL', '[^qt^] ([^q^] Requests)');
+		$table[] = array('PHP', '[^p^]');
+		$table[] = array('Total', '[^t^]');
+		$table[] = array('Memory', '[^m^]');
+		$str .= $MakeTable->create($table, array('Benchmarks',''));
+		$str .= "<br />";
+
+		$totalTime= ($this->getMicroTime() - $this->tstart);
+
 		$mem = memory_get_peak_usage(true);
 		$total_mem = $mem - $this->mstart;
 		$total_mem = ($total_mem / 1024 / 1024) . ' mb';
-		
-	    $queryTime= $this->queryTime;
-	    $phpTime= $totalTime - $queryTime;
-	    $queries= isset ($this->executedQueries) ? $this->executedQueries : 0;
-	    $queryTime= sprintf("%2.4f s", $queryTime);
-	    $totalTime= sprintf("%2.4f s", $totalTime);
-	    $phpTime= sprintf("%2.4f s", $phpTime);
-	
-	    $str= str_replace('[^q^]', $queries, $str);
-	    $str= str_replace('[^qt^]',$queryTime, $str);
-	    $str= str_replace('[^p^]', $phpTime, $str);
-	    $str= str_replace('[^t^]', $totalTime, $str);
-	    $str= str_replace('[^m^]', $total_mem, $str);
-	
-	    if(isset($php_errormsg) && !empty($php_errormsg)) $str = "<b>{$php_errormsg}</b><br />\n{$str}";
-		$str .= '<br />' . $this->get_backtrace(debug_backtrace()) . "\n";
-		
-	    // Log error
-	    if(!empty($this->currentSnippet)) $source = 'Snippet - ' . $this->currentSnippet;
-	    elseif(!empty($this->event->activePlugin)) $source = 'Plugin - ' . $this->event->activePlugin;
-	    elseif($source!=='') $source = 'Parser - ' . $source;
-	    elseif($query!=='')  $source = 'SQL Query';
-	    else             $source = 'Parser';
-	    if(isset($actionName) && !empty($actionName)) $source .= $actionName;
-	    switch($nr)
-	    {
-	    	case E_DEPRECATED :
-	    	case E_USER_DEPRECATED :
-	    	case E_STRICT :
-	    	case E_NOTICE :
-	    	case E_USER_NOTICE :
-	    		$error_level = 2;
-	    		break;
-	    	default:
-	    		$error_level = 3;
-	    }
-	    $this->logEvent(0, $error_level, $str,$source);
-	
-        if($error_level === 2 && $this->error_reporting!=='99') return true;
-        if($this->error_reporting==='99' && !isset($_SESSION['mgrValidated'])) return true;
-    
-	    // Set 500 response header
-	    if($error_level !== 2) header('HTTP/1.1 500 Internal Server Error');
-	
-	    // Display error
-	    if (isset($_SESSION['mgrValidated']))
-	    {
-	        echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd"><html><head><title>MODX Content Manager ' . $version . ' &raquo; ' . $release_date . '</title>
+
+		$queryTime= $this->queryTime;
+		$phpTime= $totalTime - $queryTime;
+		$queries= isset ($this->executedQueries) ? $this->executedQueries : 0;
+		$queryTime= sprintf("%2.4f s", $queryTime);
+		$totalTime= sprintf("%2.4f s", $totalTime);
+		$phpTime= sprintf("%2.4f s", $phpTime);
+
+		$str= str_replace('[^q^]', $queries, $str);
+		$str= str_replace('[^qt^]',$queryTime, $str);
+		$str= str_replace('[^p^]', $phpTime, $str);
+		$str= str_replace('[^t^]', $totalTime, $str);
+		$str= str_replace('[^m^]', $total_mem, $str);
+
+		if(isset($php_errormsg) && !empty($php_errormsg)) $str = "<b>{$php_errormsg}</b><br />\n{$str}";
+		$str .= $this->get_backtrace(debug_backtrace());
+		// Log error
+		if(!empty($this->currentSnippet)) $source = 'Snippet - ' . $this->currentSnippet;
+		elseif(!empty($this->event->activePlugin)) $source = 'Plugin - ' . $this->event->activePlugin;
+		elseif($source!=='') $source = 'Parser - ' . $source;
+		elseif($query!=='')  $source = 'SQL Query';
+		else             $source = 'Parser';
+		if(isset($actionName) && !empty($actionName)) $source .= $actionName;
+		switch($nr)
+		{
+			case E_DEPRECATED :
+			case E_USER_DEPRECATED :
+			case E_STRICT :
+			case E_NOTICE :
+			case E_USER_NOTICE :
+				$error_level = 2;
+				break;
+			default:
+				$error_level = 3;
+		}
+		$this->logEvent(0, $error_level, $str,$source);
+
+		if($error_level === 2 && $this->error_reporting!=='99') return true;
+		if($this->error_reporting==='99' && !isset($_SESSION['mgrValidated'])) return true;
+
+		// Set 500 response header
+		if($error_level !== 2) header('HTTP/1.1 500 Internal Server Error');
+
+		// Display error
+		if (isset($_SESSION['mgrValidated']))
+		{
+		echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd"><html><head><title>MODX Content Manager ' . $version . ' &raquo; ' . $release_date . '</title>
 	             <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	             <link rel="stylesheet" type="text/css" href="' . $this->config['site_manager_url'] . 'media/style/' . $this->config['manager_theme'] . '/style.css" />
 	             <style type="text/css">body { padding:10px; } td {font:inherit;}</style>
 	             </head><body>
 	             ' . $str . '</body></html>';
-	    
-	    }
-	    else  echo 'Error';
-	    ob_end_flush();
+
+		}
+		else  echo 'Error';
+		ob_end_flush();
 		exit;
 	}
-	
+
 	function get_backtrace($backtrace) {
-	
-		$str = "<p><b>Backtrace</b></p>\n";
-		$str  .= '<table>';
+		include_once('extenders/maketable.class.php');
+		$MakeTable = new MakeTable();
+		$MakeTable->setTableClass('grid');
+		$MakeTable->setRowRegularClass('gridItem');
+		$MakeTable->setRowAlternateClass('gridAltItem');
+		$table = array();
 		$backtrace = array_reverse($backtrace);
 		foreach ($backtrace as $key => $val)
 		{
@@ -3932,11 +3918,51 @@ class DocumentParser {
 				default:
 					$functionName = $val['function'];
 			}
-			$str .= "<tr><td valign=\"top\">{$key}</td>";
-			$str .= "<td>{$functionName}()<br />{$path} on line {$val['line']}</td>";
+			$tmp = 1;
+			$args = array_pad(array(), count($val['args']), '$var');
+			$args = implode(", ", $args);
+			$modx = $this;
+			$args = preg_replace_callback('/\$var/', function() use($modx, &$tmp, $val){
+				$arg = $val['args'][$tmp - 1];
+				switch(true){
+					case is_null($arg):{
+						$out = 'NULL';
+						break;
+					}
+					case is_numeric($arg):{
+						$out = $arg;
+						break;
+					}
+					case is_scalar($arg):{
+						$out = strlen($arg) > 20 ? 'string $var'.$tmp : ("'" . $modx->htmlspecialchars(str_replace("'", "\\'", $arg)) . "'");
+						break;
+					}
+					case is_bool($arg):{
+						$out = $arg ? 'TRUE' : 'FALSE';
+						break;
+					}
+					case is_array($arg):{
+						$out = 'array $var'.$tmp;
+						break;
+					}
+					case is_object($arg):{
+						$out = get_class($arg).' $var'.$tmp;
+						break;
+					}
+					default:{
+						$out = '$var'.$tmp;
+					}
+				}
+				$tmp++;
+				return $out;
+			}, $args);
+			$line = array(
+				"<strong>".$functionName."</strong>(".$args.")",
+				$path." on line ".$val['line']
+			);
+			$table[] = array(implode("<br />", $line));
 		}
-		$str .= '</table>';
-		return $str;
+		return $MakeTable->create($table, array('Backtrace'));
 	}
 
     function getRegisteredClientScripts() {
