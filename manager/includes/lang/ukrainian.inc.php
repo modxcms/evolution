a76c83056435a893a8ae626dfc304b1bdcadabf8
yamamoto <yamamoto@kyms.jp>
2016-02-15 20:03:42 +0900

    Add - Ukrainian manager language file

---
+++ manager/includes/lang/ukrainian.inc.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * MODX Manager language file
+ *
+ * @version 1.0.15
+ * @date 2014/02/24
+ * @author The MODX Project Team
+ *
+ * @language Ukraine
+ * @package modx
+ * @subpackage manager
+ *
+ * Please commit your language changes on Transifex (https://www.transifex.com/projects/p/modx-evolution/) or on GitHub (https://github.com/modxcms/evolution).
+ */
+
+$filename = dirname(__FILE__) . '/russian-UTF8.inc.php';
+$contents = file_get_contents($filename);
+eval('?>' . $contents);
+$modx_lang_attribute = 'uk'; // Manager HTML/XML Language Attribute see http://en.wikipedia.org/wiki/ISO_639-1
