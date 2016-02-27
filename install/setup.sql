6db670331140e5a7e8b83e03649a963b9e45ed71
Deesen <dh@fuseit.de>
2016-02-27 12:09:38 +0100

    modified "upgrade-able" block

--- install/setup.sql
+++ install/setup.sql
@@ -765,6 +765,10 @@ ALTER TABLE `{PREFIX}web_user_attributes`
 
 ALTER TABLE `{PREFIX}site_content` ADD COLUMN `alias_visible` INT(2) NOT NULL DEFAULT '1' COMMENT 'Hide document from alias path';
 
+#1.1
+ALTER TABLE `{PREFIX}modx_site_templates`
+ ADD COLUMN `selectable` TINYINT(4) NOT NULL DEFAULT '1' AFTER `locked`;
+
 # ]]upgrade-able
 
 
