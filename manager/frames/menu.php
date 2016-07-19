d0f3ab58f784c73eedbf5d2eb6133a07018165e2
Piotr Matysiak <web@pm-fx.com>
2016-07-19 12:36:25 +0200

    Items moved from top nav
    Removed "Messages" and moved "Help" from top nav to welcome page.
    "Messages" was in welcome page already so no need for to instances.
    Preview:
    http://prntscr.com/buqqcw

--- manager/frames/menu.php
+++ manager/frames/menu.php
@@ -196,29 +196,24 @@ $mxla = $modx_lang_attribute ? $modx_lang_attribute : 'en';
 
 <body id="topMenu" class="<?php echo $modx_textdir ? 'rtl':'ltr'?>">
 
-<div id="tocText"<?php echo $modx_textdir ? ' class="tocTextRTL"' : '' ?>></div>
+<div id="tocText" <?php echo $modx_textdir ? ' class="tocTextRTL"' : '' ?>></div>
 <div id="topbar">
-<div id="topbar-container">
-	<div id="statusbar">
-		<span id="buildText"></span>
-		<span id="workText"></span>
-	</div>
+  <div id="topbar-container">
+   
+    <div id="statusbar">
+      <span id="buildText"></span>
+      <span id="workText"></span>
+    </div>
 
-	<div id="supplementalNav">
-	<?php 
+    <div id="supplementalNav">
+      <?php 
       echo '<span class="username">' . $modx->getLoginUserName() . '</span>' . ($modx->hasPermission('change_password') ? ' <a onclick="this.blur();" href="index.php?a=28" target="main">'.$_lang['change_password'].'</a>'."\n" : "\n") 
-    ?>
-<?php if($modx->hasPermission('messages')) { ?>
-	<span id="newMail"><a href="index.php?a=10" title="<?php echo $_lang['you_got_mail']?>" target="main"> <img src="<?php echo $_style['icons_mail']?>" width="16" height="16" /></a></span>
-	<a onclick="this.blur();" href="index.php?a=10" target="main"><?php echo $_lang['messages']?> <span id="msgCounter">( ? / ? )</span></a>
-<?php }
-if($modx->hasPermission('help')) { ?>
-	<a href="index.php?a=9" target="main"><?php echo $_lang['help']?></a>
-<?php } ?>
-	<a href="index.php?a=8" target="_top"><?php echo $_lang['logout']?></a>
-	<span class="systemversion" title="<?php echo $site_name ?> &ndash; <?php echo $modx->getVersionData('full_appname') ?>"><?php echo $modx->getVersionData('version') ?></span>&nbsp;
-	<!-- close #supplementalNav --></div>
-</div>
+      ?>
+      <a href="index.php?a=8" target="_top"><?php echo $_lang['logout']?></a>
+      <span class="systemversion" title="<?php echo $site_name ?> &ndash; <?php echo $modx->getVersionData('full_appname') ?>"><?php echo $modx->getVersionData('version') ?></span>&nbsp;
+    </div>
+
+  </div>
 </div>
 
 <form name="menuForm" action="l4mnu.php" class="clear">
