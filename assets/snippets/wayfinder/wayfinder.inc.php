152b18cd277995cdeaccf125ab676496f976967b
yamamoto <yamamoto@kyms.jp>
2016-08-27 15:04:04 +0900

    Wayfinder - Version update 2.1

--- assets/snippets/wayfinder/wayfinder.inc.php
+++ assets/snippets/wayfinder/wayfinder.inc.php
@@ -3,7 +3,7 @@
 ::::::::::::::::::::::::::::::::::::::::
  Snippet name: Wayfinder
  Short Desc: builds site navigation
- Version: 2.0.5
+ Version: 2.1
  Authors: 
     Kyle Jaebker (muddydogpaws.com)
     Ryan Thrash (vertexworks.com)
