d1f6a1baeb1fdc7e3f32f7100f14c724c5988fc0
Dmytro Lukianenko <dmi3yy@gmail.com>
2016-10-01 15:41:03 +0300

    add D3X theme

---
+++ manager/media/style/D3X/frames/1.php
@@ -0,0 +1,165 @@
+<?php
+if(IN_MANAGER_MODE!="true") die("<b>INCLUDE_ORDERING_ERROR</b><br /><br />Please use the MODX Content Manager instead of accessing this file directly.");
+header("X-XSS-Protection: 0");
+$_SESSION['browser'] = (strpos($_SERVER['HTTP_USER_AGENT'],'MSIE 1')!==false) ? 'legacy_IE' : 'modern';
+$mxla = $modx_lang_attribute ? $modx_lang_attribute : 'en';
+if(!isset($modx->config['manager_menu_height'])) $modx->config['manager_menu_height'] = '70';
+if(!isset($modx->config['manager_tree_width']))  $modx->config['manager_tree_width']  = '260';
+$modx->invokeEvent('OnManagerPreFrameLoader',array('action'=>$action));
+?>
+<!DOCTYPE html>
+<html <?php echo (isset($modx_textdir) && $modx_textdir ? 'dir="rtl" lang="' : 'lang="').$mxla.'" xml:lang="'.$mxla.'"'; ?>>
+<head>
+    <title><?php echo $site_name?> - (MODX CMS Manager)</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=<?php echo $modx_manager_charset?>" />
+    <link href='http://fonts.googleapis.com/css?family=Ubuntu&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
+    <style>
+        html, body { margin: 0; padding: 0; width: 100%; height: 100% }
+        body { position: relative; }
+        #mainMenu, #tree, #main { position: absolute }
+        #mainMenu iframe, #tree iframe, #main iframe, #mask_resizer { position: absolute; width: 100%; height: 100%; }
+        #mainMenu { height: 85px; width: 100%; box-shadow: 0px 0px 8px #030303; z-index:1000;}
+        #tree { width: 250px; top: 85px; left: 0; bottom: 0; overflow:hidden;}
+        #main { top: 85px; left: 250px; right: 0; bottom: 0; }
+        #resizer { position: absolute; top: 85px; bottom: 0; left: 250px; width: 3px; cursor: col-resize; z-index: 999;border-left:1px solid #a4b9cc!important;}
+        #resizer #hideMenu {display:block;
+            margin-top:10px;
+            margin-left:-9px;
+            cursor:pointer;
+            background:transparent url(media/style/<?php echo $modx->config['manager_theme']; ?>/images/icons/application_side_contract.png)!important;
+            width:16px;
+            height:16px;
+        }
+        #resizer2 { position: absolute; top: 85px; right: 15px; width: 3px; z-index: 999;}
+        #resizer2 #hideTopMenu {display:block;
+            margin-top:-4px;
+            margin-left:-9px;
+            cursor:pointer;
+            background:transparent url(media/style/<?php echo $modx->config['manager_theme']; ?>/images/icons/application_get.png)!important;
+            width:16px;
+            height:16px;
+        }
+    </style>
+</head>
+<body>
+    <div id="resizer">
+        <a id="hideMenu" onclick="mainMenu.toggleTreeFrame();"></a>
+    </div>
+    <div id="resizer2">
+        <a id="hideTopMenu" onclick="mainMenu.toggleMenuFrame();"></a>
+    </div>
+    <div id="mainMenu">
+        <iframe name="mainMenu" src="index.php?a=1&amp;f=menu" scrolling="no" frameborder="0" noresize="noresize"></iframe>
+    </div>
+    <div id="tree">
+        <iframe name="tree" src="index.php?a=1&amp;f=tree" scrolling="no" frameborder="0" onresize="mainMenu.resizeTree();"></iframe>
+    </div>
+
+    <div id="main">
+        <iframe name="main" id="mainframe" src="index.php?a=2" scrolling="auto" frameborder="0" onload="if (mainMenu.stopWork()) mainMenu.stopWork(); scrollWork();"></iframe>
+    </div>
+    
+    <!--<script type='text/javascript' src='http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js'></script>-->
+    <script language="JavaScript" type="text/javascript">
+        var _startY = 85;
+        var _dragElement;
+        var _oldZIndex = 999;
+        var _left;
+        var mask = document.createElement('div');
+        mask.id = 'mask_resizer';
+        mask.style.zIndex = _oldZIndex;
+
+        InitDragDrop();
+
+        function InitDragDrop() {
+            document.getElementById('resizer').onmousedown = OnMouseDown;
+            document.getElementById('resizer').onmouseup = OnMouseUp
+        }
+
+        function OnMouseDown(e) {
+            if (e == null) e = window.event;
+            _dragElement = e.target != null ? e.target : e.srcElement;
+            if ((e.button == 1 && window.event != null || e.button == 0) && _dragElement.id == 'resizer') {
+                _oldZIndex = _dragElement.style.zIndex;
+                _dragElement.style.zIndex = 10000;
+                _dragElement.style.background = '#a4b9cc';
+                document.body.appendChild(mask)
+                document.onmousemove = OnMouseMove;
+                document.body.focus();
+                document.onselectstart = function () {
+                    return false
+                };
+                _dragElement.ondragstart = function () {
+                    return false
+                };
+                return false
+            }
+        }
+
+        function ExtractNumber(value) {
+            var n = parseInt(value);
+            return n == null || isNaN(n) ? 0 : n
+        }
+
+        function OnMouseMove(e) {
+            if (e == null) var e = window.event;
+            _dragElement.style.left = e.clientX + 'px';
+            _dragElement.style.top = _startY + 'px';
+            document.getElementById('tree').style.width = e.clientX + 'px';
+            document.getElementById('main').style.left = e.clientX + 'px'
+        }
+
+        function OnMouseUp(e) {
+            if (_dragElement != null) {
+                _dragElement.style.zIndex = _oldZIndex;
+                _dragElement.style.background = 'transparent';
+                _dragElement.ondragstart = null;
+                _dragElement = null;
+                document.body.removeChild(mask);
+                document.onmousemove = null;
+                document.onselectstart = null
+            }
+        }
+
+        //save scrollPosition
+        function getQueryVariable(variable, query) {
+            var vars = query.split('&');
+            for (var i = 0; i < vars.length; i++) {
+                var pair = vars[i].split('=');
+                if (decodeURIComponent(pair[0]) == variable) {
+                    return decodeURIComponent(pair[1]);
+                }
+            }
+        }
+
+        function scrollWork() {
+            var frm = document.getElementById("mainframe").contentWindow;
+            currentPageY = localStorage.getItem('page_y');
+            pageUrl = localStorage.getItem('page_url');
+            if (currentPageY === undefined) {
+                localStorage.setItem('page_y', 0);
+            }
+            if (pageUrl === null) {
+                pageUrl = frm.location.search.substring(1);
+            }
+            if ( getQueryVariable('a', pageUrl) == getQueryVariable('a', frm.location.search.substring(1)) ) {
+                if ( getQueryVariable('id', pageUrl) == getQueryVariable('id', frm.location.search.substring(1)) ){
+                    frm.scrollTo(0,currentPageY);
+                }
+            }
+
+            frm.onscroll = function(){
+                if (frm.pageYOffset > 0) {
+                    localStorage.setItem('page_y', frm.pageYOffset);
+                    localStorage.setItem('page_url', frm.location.search.substring(1));
+                }
+            }        
+        }
+    </script>
+    <?php
+    $modx->invokeEvent('OnManagerFrameLoader',array('action'=>$action));
+    ?>
+</body>
+</html>
+<?php
+//
---
+++ manager/media/style/D3X/frames/menu.php
@@ -0,0 +1,268 @@
+<?php
+if(IN_MANAGER_MODE!="true") die("<b>INCLUDE_ORDERING_ERROR</b><br /><br />Please use the MODX Content Manager instead of accessing this file directly.");
+if (!array_key_exists('mail_check_timeperiod', $modx->config) || !is_numeric($modx->config['mail_check_timeperiod'])) {
+	$modx->config['mail_check_timeperiod'] = 5;
+}
+$modx_textdir = isset($modx_textdir) ? $modx_textdir : null;
+$mxla = $modx_lang_attribute ? $modx_lang_attribute : 'en';
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html <?php echo ($modx_textdir ? 'dir="rtl" lang="' : 'lang="').$mxla.'" xml:lang="'.$mxla.'"'; ?>>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=<?php echo $modx_manager_charset?>" />
+	<title>nav</title>
+	<link rel="stylesheet" type="text/css" href="media/style/<?php echo $modx->config['manager_theme']; ?>/style.css" />
+	<script src="media/script/mootools/mootools.js" type="text/javascript"></script>
+	<script src="media/script/mootools/moodx.js" type="text/javascript"></script>
+	<script type="text/javascript" src="media/script/session.js"></script>
+	<script type="text/javascript">
+	// TREE FUNCTIONS - FRAME
+	// These functions affect the tree frame and any items that may be pointing to the tree.
+	var currentFrameState = 'open';
+	var defaultFrameWidth = '<?php echo !$modx_textdir ? '260,*' : '*,260'?>';
+	var userDefinedFrameWidth = '<?php echo !$modx_textdir ? '260,*' : '*,260'?>';
+
+	var workText;
+	var buildText;
+
+	// Create the AJAX mail update object before requesting it
+	var updateMailerAjx = new Ajax('index.php', {method:'post', postBody:'updateMsgCount=true', onComplete:showResponse});
+	function updateMail(now) {
+		try {
+			// if 'now' is set, runs immediate ajax request (avoids problem on initial loading where periodical waits for time period before making first request)
+			if (now)
+				updateMailerAjx.request();
+			return false;
+		} catch(oException) {
+			// Delay first run until we're ready...
+			xx=updateMail.delay(1000 * 60,'',true);
+		}
+	}
+
+	function showResponse(request) {
+		var counts = request.split(',');
+		var elm = $('msgCounter');
+		if (elm) elm.innerHTML ='(' + counts[0] + ' / ' + counts[1] + ')';
+		var elm = $('newMail');
+		if (elm) elm.style.display = counts[0] >0 ? 'inline' :  'none';
+	}
+
+	window.addEvent('load', function() {
+		updateMail(true); // First run update
+		updateMail.periodical(<?php echo $modx->config['mail_check_timeperiod'] * 1000 ?>, '', true); // Periodical Updater
+		if(top.__hideTree) {
+			// display toc icon
+			var elm = $('tocText');
+			if(elm) elm.innerHTML = "<a href='#' onclick='defaultTreeFrame();'><img src='<?php echo $_style['show_tree']?>' alt='<?php echo $_lang['show_tree']?>' width='16' height='16' /></a>";
+		}
+	});
+
+
+	function setTreeFrameWidth(pos) {
+		parent.document.getElementById('tree').style.width    = pos + 'px';
+		parent.document.getElementById('resizer').style.left = pos + 'px';
+		parent.document.getElementById('main').style.left    = pos + 'px';
+
+	}
+
+	function toggleTreeFrame() {
+		var pos = parseInt(parent.document.getElementById('tree').style.width) != 0?0:250;
+		setTreeFrameWidth(pos);
+	}
+
+
+	function hideTreeFrame() {
+		var pos = 0;
+		setTreeFrameWidth(pos);
+	}
+
+	function defaultTreeFrame() {
+		var pos = 250;
+		setTreeFrameWidth(pos);
+	}
+
+
+	//toggle TopMenu Frame
+		function setMenuFrameHeight(pos) {
+		parent.document.getElementById('tree').style.top    = pos + 'px';
+		parent.document.getElementById('resizer').style.top = pos + 'px';
+		parent.document.getElementById('resizer2').style.top = pos + 'px';
+		parent.document.getElementById('main').style.top    = pos + 'px';
+		parent.document.getElementById('mainMenu').style.height    = pos + 'px';
+
+	}
+
+	function toggleMenuFrame() {
+		var pos = parseInt(parent.document.getElementById('mainMenu').style.height) != 5?5:85;
+		setMenuFrameHeight(pos);
+	}
+
+
+	function hideMenuFrame() {
+		var pos = 5;
+		setMenuFrameHeight(pos);
+	}
+
+	function defaultMenuFrame() {
+		var pos = 85;
+		setMenuFrameHeight(pos);
+	}
+
+
+
+	// TREE FUNCTIONS - Expand/ Collapse
+	// These functions affect the expanded/collapsed state of the tree and any items that may be pointing to it
+	function expandTree() {
+		try {
+			parent.tree.d.openAll();  // dtree
+		} catch(oException) {
+			zz=window.setTimeout('expandTree()', 1000);
+		}
+	}
+
+	function collapseTree() {
+		try {
+			parent.tree.d.closeAll();  // dtree
+		} catch(oException) {
+			yy=window.setTimeout('collapseTree()', 1000);
+		}
+	}
+
+	// GENERAL FUNCTIONS - Refresh
+	// These functions are used for refreshing the tree or menu
+	function reloadtree() {
+		var elm = $('buildText');
+		if (elm) {
+			elm.innerHTML = "&nbsp;&nbsp;<img src='<?php echo $_style['icons_loading_doc_tree']?>' width='16' height='16' />&nbsp;<?php echo $_lang['loading_doc_tree']?>";
+			elm.style.display = 'block';
+		}
+		top.tree.saveFolderState(); // save folder state
+		setTimeout('top.tree.restoreTree()',200);
+	}
+
+	function reloadmenu() {
+		<?php if($manager_layout==0) { ?>
+			var elm = $('buildText');
+			if (elm) {
+				elm.innerHTML = "&nbsp;&nbsp;<img src='<?php echo $_style['icons_working']?>' width='16' height='16' />&nbsp;<?php echo $_lang['loading_menu']?>";
+				elm.style.display = 'block';
+			}
+			parent.mainMenu.location.reload();
+			<?php } ?>
+		}
+
+		function startrefresh(rFrame){
+			if(rFrame==1){
+				x=window.setTimeout('reloadtree()',500);
+			}
+			if(rFrame==2) {
+				x=window.setTimeout('reloadmenu()',500);
+			}
+			if(rFrame==9) {
+				x=window.setTimeout('reloadmenu()',500);
+				y=window.setTimeout('reloadtree()',500);
+			}
+			if(rFrame==10) {
+				window.top.location.href = "../<?php echo MGR_DIR;?>";
+			}
+		}
+
+	// GENERAL FUNCTIONS - Work
+	// These functions are used for showing the user the system is working
+	function work() {
+		var elm = $('workText');
+		if (elm) elm.innerHTML = "&nbsp;<img src='<?php echo $_style['icons_working']?>' width='16' height='16' />&nbsp;<?php echo $_lang['working']?>";
+		else w=window.setTimeout('work()', 50);
+	}
+
+	function stopWork() {
+		var elm = $('workText');
+		if (elm) elm.innerHTML = "";
+		else  ww=window.setTimeout('stopWork()', 50);
+	}
+
+	// GENERAL FUNCTIONS - Remove locks
+	// This function removes locks on documents, templates, parsers, and snippets
+	function removeLocks() {
+		if(confirm("<?php echo $_lang['confirm_remove_locks']?>")==true) {
+			top.main.document.location.href="index.php?a=67";
+		}
+	}
+
+	function showWin() {
+		window.open('../');
+	}
+
+	function stopIt() {
+		top.mainMenu.stopWork();
+	}
+
+	function openCredits() {
+		parent.main.document.location.href = "index.php?a=18";
+		xwwd = window.setTimeout('stopIt()', 2000);
+	}
+
+	function NavToggle(element) {
+		// This gives the active tab its look
+		var navid = document.getElementById('nav');
+		var navs = navid.getElementsByTagName('li');
+		var navsCount = navs.length;
+		for(j = 0; j < navsCount; j++) {
+			active = (navs[j].id == element.parentNode.id) ? "active" : "";
+			navs[j].className = active;
+		}
+
+		// remove focus from top nav
+		if(element) element.blur();
+	}
+</script>
+	<!--[if lt IE 7]>
+	<style type="text/css">
+	body { behavior: url(media/script/forIE/htcmime.php?file=csshover.htc) }
+	img { behavior: url(media/script/forIE/htcmime.php?file=pngbehavior.htc); }
+	</style>
+	<![endif]-->
+</head>
+
+<body id="topMenu" class="<?php echo $modx_textdir ? 'rtl':'ltr'?>">
+
+	<div id="tocText"<?php echo $modx_textdir ? ' class="tocTextRTL"' : '' ?>></div>
+	<div id="topbar">
+		<div id="topbar-container">
+			<div id="statusbar">
+				<span id="buildText"></span>
+				<span id="workText"></span>
+			</div>
+
+			<div id="supplementalNav">
+				<?php echo $modx->getLoginUserName(). ($modx->hasPermission('change_password') ? ': <a onclick="this.blur();" href="index.php?a=28" target="main">'.$_lang['change_password'].'</a>'."\n" : "\n") ?>
+				<?php if($modx->hasPermission('messages')) { ?>
+				| <span id="newMail"><a href="index.php?a=10" title="<?php echo $_lang['you_got_mail']?>" target="main"> <img src="<?php echo $_style['icons_mail']?>" width="16" height="16" /></a></span>
+				<a onclick="this.blur();" href="index.php?a=10" target="main"><?php echo $_lang['messages']?> <span id="msgCounter">( ? / ? )</span></a>
+				<?php }
+				if($modx->hasPermission('help')) { ?>
+				| <a href="index.php?a=9" target="main"><?php echo $_lang['help']?></a>
+				<?php } ?>
+				| <a href="index.php?a=8" target="_top"><?php echo $_lang['logout']?></a>
+				| <span title="<?php echo $site_name ?> &ndash; <?php echo $modx->getVersionData('full_appname') ?>"><?php echo $modx->getVersionData('version') ?></span>&nbsp;
+				<!-- close #supplementalNav --></div>
+			</div>
+		</div>
+		<div id="searchform">
+			<form  action="index.php?a=71#results" method="post" target="main">
+				<input type="hidden" value="Search" name="submitok" />
+				<input type="text" name="searchid" size="25" class="form-control input-sm" placeholder="<?php echo $_lang['search']?>">
+			</form>
+		</div>
+
+		<form name="menuForm" action="l4mnu.php" class="clear">
+			<input type="hidden" name="sessToken" id="sessTokenInput" value="<?php echo md5(session_id());?>" />
+			<div id="Navcontainer">
+				<div id="divNav">
+					<?php include(MODX_MANAGER_PATH.'media/style/'.$manager_theme.'/frames/mainmenu.php'); ?></div>
+				</div>
+			</form>
+
+			<div id="menuSplitter"></div>
+		</body>
+		</html>
