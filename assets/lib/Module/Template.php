93d378c448e8b577a260ab040864b0efba097eca
Dmytro Lukianenko <dmi3yy@gmail.com>
2016-10-11 13:10:35 +0300

    update lib/modules

--- assets/lib/Module/Template.php
+++ assets/lib/Module/Template.php
@@ -1,11 +1,31 @@
 <?php namespace Module;
 
-abstract class Template{
+/**
+ * Class Template
+ * @package Module
+ */
+abstract class Template
+{
+    /**
+     * @var \DocumentParser|null
+     */
     protected $_modx = null;
+    /**
+     * @var null|string
+     */
     protected $_tplFolder = null;
-	protected $_publicFolder;
+    /**
+     * @var string
+     */
+    protected $_publicFolder;
+    /**
+     *
+     */
     const TPL_EXT = 'html';
 
+    /**
+     * @var array
+     */
     public $vars = array(
         'modx_lang_attribute',
         'modx_textdir',
@@ -18,55 +38,97 @@ abstract class Template{
         'incPath',
         'content'
     );
+    /**
+     * @var bool
+     */
     protected static $_ajax = false;
 
-    public function __construct(\DocumentParser $modx, $ajax = false, $tplFolder = null){
+    /**
+     * Template constructor.
+     * @param \DocumentParser $modx
+     * @param bool $ajax
+     * @param null $tplFolder
+     */
+    public function __construct(\DocumentParser $modx, $ajax = false, $tplFolder = null)
+    {
         $this->_modx = $modx;
-        self::$_ajax = (boolean) $ajax;
+        self::$_ajax = (boolean)$ajax;
         $this->loadVars();
-        if(is_null($tplFolder)){
-		  $tplFolder = dirname(dirname(__FILE__));
+        if (is_null($tplFolder)) {
+            $tplFolder = dirname(dirname(__FILE__));
         }
         $FS = \Helpers\FS::getInstance();
         $tplFolder = $FS->relativePath($tplFolder);
 
-		$this->_publicFolder = "/".$tplFolder."/public/";
-        $this->_tplFolder = MODX_BASE_PATH.$tplFolder."/template/";
+        $this->_publicFolder = "/" . $tplFolder . "/public/";
+        $this->_tplFolder = MODX_BASE_PATH . $tplFolder . "/template/";
 
-        if(!defined('MODX_MAIN_URL')){
+        if (!defined('MODX_MAIN_URL')) {
             define('MODX_MAIN_URL', MODX_SITE_URL);
         }
     }
-    public static function isAjax(){
+
+    /**
+     * @return bool
+     */
+    public static function isAjax()
+    {
         return self::$_ajax;
     }
-	public function publicFolder(){
-		return $this->_publicFolder;
-	}
-	public function src($path){
-		return rtrim(MODX_MAIN_URL, '/').$this->publicFolder().ltrim($path, '/');
-	}
 
-    public function showHeader(){
+    /**
+     * @return string
+     */
+    public function publicFolder()
+    {
+        return $this->_publicFolder;
+    }
+
+    /**
+     * @param $path
+     * @return string
+     */
+    public function src($path)
+    {
+        return rtrim(MODX_MAIN_URL, '/') . $this->publicFolder() . ltrim($path, '/');
+    }
+
+    /**
+     * @return string
+     */
+    public function showHeader()
+    {
         return $this->_getMainTpl('header.inc.php');
     }
-    protected function _getMainTpl($name){
+
+    /**
+     * @param $name
+     * @return string
+     */
+    protected function _getMainTpl($name)
+    {
         $content = '';
-        if( ! self::isAjax()){
+        if (!self::isAjax()) {
 
             ob_start();
             extract($this->vars);
-            if(file_exists($incPath . $name)){
+            if (file_exists($incPath . $name)) {
                 include($incPath . $name);
                 $content = ob_get_contents();
             }
             ob_end_clean();
         }
+
         return $content;
     }
-    public function loadVars(){
+
+    /**
+     *
+     */
+    public function loadVars()
+    {
         $vars = array();
-        foreach($this->vars as $item){
+        foreach ($this->vars as $item) {
             global $$item;
             $vars[$item] = $$item;
         }
@@ -74,23 +136,53 @@ abstract class Template{
         $this->vars['tplClass'] = $this;
         $this->vars['modx'] = $this->_modx;
     }
-    public function showFooter(){
+
+    /**
+     * @return string
+     */
+    public function showFooter()
+    {
         return $this->_getMainTpl('footer.inc.php');
     }
-    public function showBody($TplName, array $tplParams = array()){
+
+    /**
+     * @param $TplName
+     * @param array $tplParams
+     * @return string
+     */
+    public function showBody($TplName, array $tplParams = array())
+    {
         ob_start();
-        if(file_exists($this->_tplFolder.$TplName.".".self::TPL_EXT)){
+        if (file_exists($this->_tplFolder . $TplName . "." . self::TPL_EXT)) {
             extract($this->vars);
-            include($this->_tplFolder.$TplName.".".self::TPL_EXT);
+            include($this->_tplFolder . $TplName . "." . self::TPL_EXT);
         }
         $content = ob_get_contents();
         ob_end_clean();
+
         return $content;
     }
-    public static function getParam($key, array $param = array(), $default = null){
+
+    /**
+     * @param $key
+     * @param array $param
+     * @param null $default
+     * @return mixed|null
+     */
+    public static function getParam($key, array $param = array(), $default = null)
+    {
         return isset($param[$key]) ? $param[$key] : $default;
     }
-    public function makeUrl($action, array $data = array(), $module = null, $full = false){
+
+    /**
+     * @param $action
+     * @param array $data
+     * @param null $module
+     * @param bool $full
+     * @return string
+     */
+    public function makeUrl($action, array $data = array(), $module = null, $full = false)
+    {
         $action = is_scalar($action) ? $action : '';
         $content = self::getParam('content', $this->vars, array());
         $data = array_merge(
@@ -99,19 +191,29 @@ abstract class Template{
             ),
             $data,
             array(
-                'a' => 112,
+                'a'      => 112,
                 'action' => $action,
-                'id' => empty($module) ? self::getParam('id', $content, 0) : (int)$module
+                'id'     => empty($module) ? self::getParam('id', $content, 0) : (int)$module
             )
         );
-		$out = implode("?", array($this->_modx->getManagerPath(), http_build_query($data)));
-		if ($full) {
-			$out = $this->_modx->getConfig('site_url') . ltrim($out, '/');
-		}
+        $out = implode("?", array($this->_modx->getManagerPath(), http_build_query($data)));
+        if ($full) {
+            $out = $this->_modx->getConfig('site_url') . ltrim($out, '/');
+        }
+
         return $out;
     }
-    public static function showLog(){
+
+    /**
+     * @return string
+     */
+    public static function showLog()
+    {
         return self::isAjax() ? 'log' : 'main';
     }
+
+    /**
+     * @return mixed
+     */
     abstract public function Lists();
 }
