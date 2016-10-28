220c972a15758d518b9ced052d258738eb3fe38f
Deesen <dh@fuseit.de>
2016-10-28 06:43:48 +0200

    Corrected link

--- assets/templates/help/01Documentation.php
+++ assets/templates/help/01Documentation.php
@@ -4,7 +4,7 @@ if(IN_MANAGER_MODE!="true") die("<b>INCLUDE_ORDERING_ERROR</b><br /><br />Please
 
 <div class="sectionHeader"><?php echo $_lang['documentation']; ?></div><div class="sectionBody">
     <table border="0" cellspacing="0" cellpadding="0">
-        <tr><td><a href="http://rtfm.modx.com/evolution/1.0/getting-started/what-is-modx" target="_blank">What is MODX?</a></td><td></td><td>Official 1.0 documentation</td></tr>
+        <tr><td><a href="https://docs.modx.com/evolution/1.0" target="_blank">What is MODX?</a></td><td></td><td>Official 1.0 documentation</td></tr>
         <tr><td><a href="http://www.modx.com/" target="_blank">MODX.com</a></td><td></td><td>Home of MODX</td></tr>
         <tr><td><a href="https://forums.modx.com/board/48/evo-discussions-support" target="_blank">MODX Forums</a></td><td></td><td>General support & discussions</td></tr>
         <tr><td><a href="http://docs.evolution-cms.com/" target="_blank">docs.evolution-cms.com</a></td><td><a href="https://translate.google.com/translate?hl=en&sl=auto&tl=en&u=http://docs.evolution-cms.com/" target="_blank">Google Translate</a></td><td>Further documentation</td></tr>
