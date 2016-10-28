38559bb38c19ef5dc9e506f269ffc0a23eb0789a
Deesen <dh@fuseit.de>
2016-10-28 06:44:56 +0200

    Added download-links and modified credits

--- assets/templates/help/04About_MODX.php
+++ assets/templates/help/04About_MODX.php
@@ -1,9 +1,28 @@
 <?php
 if(IN_MANAGER_MODE!="true") die("<b>INCLUDE_ORDERING_ERROR</b><br /><br />Please use the MODX Content Manager instead of accessing this file directly.");
+
+$downloadLinks = array(
+	0=>array('title'=>'Information','link'=>'https://modx.com/community/modx-evolution'),
+	1=>array('title'=>'Download','link'=>'https://modx.com/download/evolution/'),
+	2=>array('title'=>'Previous Releases','link'=>'https://modx.com/download/evolution/previous-releases.html'),
+	3=>array('title'=>'Extras','link'=>'https://modx.com/extras/?product=evolution'),
+);
 ?>
 
+<div class="sectionHeader">Evolution Downloads</div><div class="sectionBody">
+	<table width="500"  border="0" cellspacing="0" cellpadding="0">
+	<?php foreach($downloadLinks as $row) { ?>
+		<tr>
+			<td align="left"><strong><?php echo $row["title"]; ?></strong></td>
+			<td align="left"><a href="<?php echo $row["link"]; ?>" target="_blank"><?php echo $row["link"]; ?></a></td>
+		</tr>
+	<?php } ?>
+		
+	</table> 
+</div>
+
 <div class="sectionHeader"><?php echo $_lang['about_title']; ?></div><div class="sectionBody">
-<?php echo $_lang['about_msg']; ?>
+<?php echo $_lang['about_msg']; ?> <?php echo $_lang['credits_shouts_msg']; ?>
 </div>
 
 <div class="sectionHeader"><?php echo $_lang['help_title']; ?></div><div class="sectionBody">
@@ -14,28 +33,10 @@ if(IN_MANAGER_MODE!="true") die("<b>INCLUDE_ORDERING_ERROR</b><br /><br />Please
 <table width="500"  border="0" cellspacing="0" cellpadding="0">
   <tr height="70">
     <td align="center"><a href="http://www.php.net" target="_blank"><img src="media/style/<?php echo $modx->config['manager_theme']; ?>/images/credits/php.gif" border="0"></a></td>
-    <td align="left"><?php echo $_lang['credits_php']; ?></td>
-  </tr>
-  <tr height="70">
     <td align="center"><a href="http://www.mysql.com" target="_blank"><img src="media/style/<?php echo $modx->config['manager_theme']; ?>/images/credits/mysql.gif" border="0"></a></td>
-    <td align="left"><?php echo $_lang['credits_mysql']; ?></td>
-  </tr>
-  <tr height="70">
     <td align="center"><a href="http://www.destroydrop.com" target="_blank"><img src="media/style/<?php echo $modx->config['manager_theme']; ?>/images/credits/dtree.gif" border="0"></a></td>
-    <td align="left"><?php echo $_lang['credits_dTree']; ?></td>
-  </tr>
-  <tr height="70">
     <td align="center"><a href="http://www.everaldo.com" target="_blank"><img src="media/style/<?php echo $modx->config['manager_theme']; ?>/images/credits/penguin.gif" border="0"></a></td>
-    <td align="left"><?php echo $_lang['credits_everaldo']; ?></td>
-  </tr>
-  <tr>
-    <td colspan="2">&nbsp;</td>
-  </tr>
-  <tr>
-    <td align="center" valign="top"><?php echo $_lang['credits_shouts_title']; ?></td>
-	<td align="left"><?php echo $_lang['credits_shouts_msg']; ?></td>
   </tr>
 </table>
-
 </div>
 
