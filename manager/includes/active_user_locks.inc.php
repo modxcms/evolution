3bfe7f0ff4b7ba5904c08c6ea271909210900b25
Deesen <dh@fuseit.de>
2016-11-13 06:42:29 +0100

    Remove interval first to avoid server-requests when user does not confirm immediately, changed CSS-class to avoid conflict with Boostrap

--- manager/includes/active_user_locks.inc.php
+++ manager/includes/active_user_locks.inc.php
@@ -12,10 +12,10 @@ if($lockElementId > 0) {
 			onComplete: function(sessionResponse) {
 				resp = Json.evaluate(sessionResponse);
 				if(resp.status != 'ok') {
-					var errorMsg = '<?php echo sprintf($_lang["lock_element_unknown_error"], $lockElementType, $lockElementId, $_lang["lock_element_type_".$lockElementType]);?>';
-					jQuery('body').prepend('<div class="alert alert-error">'+errorMsg+'</div>');
-					alert(errorMsg);
 					clearInterval(keepMeLockedInterval);
+					var errorMsg = '<?php echo sprintf($_lang["lock_element_unknown_error"], $lockElementType, $lockElementId, $_lang["lock_element_type_".$lockElementType]);?>';
+					jQuery('body').prepend('<div class="modx-alert alert-error">'+errorMsg+'</div>');
+					alert(errorMsg);
 				}
 			}
 		}).request();
