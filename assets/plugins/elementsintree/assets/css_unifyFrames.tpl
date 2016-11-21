de142583fd4cf475d5b43e6b61140060315d608e
Piotr Matysiak <web@pm-fx.com>
2016-11-21 16:45:04 +0100

    ElementsInTree 1.5.3

---
+++ assets/plugins/elementsintree/assets/css_unifyFrames.tpl
@@ -0,0 +1,16 @@
+        /* Unify Frames */
+        body,
+        div.treeframebody {
+            background-color: #f2f2f2 !important;
+        }
+
+        div.treeframebody {
+            background-color: transparent !important;
+            -webkit-box-shadow: none !important;
+            box-shadow: none !important;
+        }
+
+        #treeMenu {
+            background-color: transparent !important;
+            border-bottom-color: transparent !important;
+        }
--- assets/plugins/elementsintree/plugin.elementsintree.php
+++ assets/plugins/elementsintree/plugin.elementsintree.php
@@ -8,22 +8,21 @@
 
 if(!defined('MODX_BASE_PATH')) die('What are you doing? Get out of here!');
 
-$inc_path = str_replace('\\','/',dirname(__FILE__)) . '/includes/';
-include_once($inc_path.'functions.inc.php');
+$eit_base_path = str_replace('\\','/',dirname(__FILE__)) . '/';
+include_once($eit_base_path.'includes/functions.inc.php');
 
 global $_lang;
-
 $e = &$modx->event;
 
 if(!isset($_SESSION['elementsInTree'])) $_SESSION['elementsInTree'] = array();
 
 switch($e->name) {
     case 'OnManagerMainFrameHeaderHTMLBlock': // Trigger reloading tree for relevant actions
-        include_once($inc_path.'on_manager_main_frame_header_html_block.inc.php'); break;
+        include_once($eit_base_path.'includes/on_manager_main_frame_header_html_block.inc.php'); break;
     case 'OnManagerTreePrerender': // Main elementsInTree-part
-        include_once($inc_path.'on_manager_tree_prerender.inc.php'); break;
+        include_once($eit_base_path.'includes/on_manager_tree_prerender.inc.php'); break;
     case 'OnManagerTreeRender':
-        if(hasAnyPermission()) include_once($inc_path.'on_manager_tree_render.inc.php'); break;
+        if(hasAnyPermission()) include_once($eit_base_path.'includes/on_manager_tree_render.inc.php'); break;
     case 'OnTempFormSave':
     case 'OnTVFormSave':
     case 'OnChunkFormSave':
@@ -36,7 +35,7 @@ switch($e->name) {
     case 'OnSnipFormDelete':
     case 'OnPluginFormDelete':
     case 'OnModFormDelete':
-// Set reloadTree = true for this events
+        // Set reloadTree = true for this events
         $_SESSION['elementsInTree']['reloadTree'] = true; break;
     default:
         if($_GET['r'] == 2) $_SESSION['elementsInTree']['reloadTree'] = true;
