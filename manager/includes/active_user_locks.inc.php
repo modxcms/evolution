6d7c1cda2c21471b683cc7c871835155c543528f
Deesen <dh@fuseit.de>
2016-11-30 18:22:22 +0100

    Rely on Navigator.sendBeacon instead of async AJAX for sending unlock-requests on window.unload - https://developer.mozilla.org/en-US/docs/Web/API/Navigator/sendBeacon - http://stackoverflow.com/a/33553050/2354531

--- manager/includes/active_user_locks.inc.php
+++ manager/includes/active_user_locks.inc.php
@@ -6,30 +6,64 @@ $lockElementId = intval($lockElementId);
 if($lockElementId > 0) {
 ?>
 <script>
-	// Trigger unlock when leaving window
-	var form_save = false;
-	jQuery(window).on('beforeunload', function(){
-		var stay = jQuery('#stay').val();
-		var lastClickedElement = localStorage.getItem('MODX_lastClickedElement');
-		var sameElement = false; 
-		if(lastClickedElement != null) {
-			try {
-				lastClickedElement = JSON.parse( lastClickedElement );
-				sameElement = lastClickedElement[0]==<?php echo $lockElementType;?> && lastClickedElement[1]==<?php echo $lockElementId;?> ? true : false;
-			} catch(err) {
-				console.log(err);
-			}
-		}
-		
-		// Trigger unlock 
-		if((stay != 2 || !form_save) && !sameElement) {
-			// console.log('unlock triggered:', 'stay='+stay, 'form_save='+form_save, 'sameElement='+sameElement, lastClickedElement);
-			var unlockRequest = new Ajax('index.php?a=67&type=<?php echo $lockElementType;?>&id=<?php echo $lockElementId;?>&o=' + Math.random(), {
-				method: 'get'
-			}).request();
+    // Polyfill for Navigator.sendBeacon
+    (function(root) {
+        'use strict';
+
+        function sendBeacon(url, data) {
+            var xhr = ('XMLHttpRequest' in window) ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+            xhr.open('POST', url, false);
+            xhr.setRequestHeader('Accept', '*/*');
+            if (typeof data === 'string') {
+                xhr.setRequestHeader('Content-Type', 'text/plain;charset=UTF-8');
+                xhr.responseType = 'text/plain';
+            } else if (Object.prototype.toString.call(data) === '[object Blob]') {
+                if (data.type) {
+                    xhr.setRequestHeader('Content-Type', data.type);
+                }
+            }
+            xhr.send(data);
+            return true;
+        }
+
+        if (typeof exports !== 'undefined') {
+            if (typeof module !== 'undefined' && module.exports) {
+                exports = module.exports = sendBeacon;
+            }
+            exports.sendBeacon = sendBeacon;
+        } else if (typeof define === 'function' && define.amd) {
+            define([], function() {
+                return sendBeacon;
+            });
+        } else if ('navigator' in root && !('sendBeacon' in root.navigator)) {
+            root.navigator.sendBeacon = sendBeacon;
+        }
+    })(this);
+    
+    // Trigger unlock when leaving window
+    var form_save = false;
+    window.addEventListener('unload', unlockThisElement, false);
+
+    function unlockThisElement() {
+        var stay = jQuery('#stay').val();
+        var lastClickedElement = localStorage.getItem('MODX_lastClickedElement');
+        var sameElement = false;
+        if(lastClickedElement != null) {
+            try {
+                lastClickedElement = JSON.parse( lastClickedElement );
+                sameElement = lastClickedElement[0]==<?php echo $lockElementType;?> && lastClickedElement[1]==<?php echo $lockElementId;?> ? true : false;
+            } catch(err) {
+                console.log(err);
+            }
+        }
+
+        // Trigger unlock
+        if((stay != 2 || !form_save) && !sameElement) {
+            // console.log('unlock triggered:', 'stay='+stay, 'form_save='+form_save, 'sameElement='+sameElement, lastClickedElement);
+            navigator.sendBeacon('index.php?a=67&type=<?php echo $lockElementType;?>&id=<?php echo $lockElementId;?>&o=' + Math.random());
             top.mainMenu.reloadtree();
-		}
-	});
+        }
+    }
 </script>
 <?php
 }
