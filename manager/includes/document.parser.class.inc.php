b5c11f7ea745967831436ec5a6a101e49ed3357d
Deesen <dh@fuseit.de>
2016-12-01 20:54:30 +0100

    #1024 Added "sid" to DB-table (using $modx->sid = md5(session_id()); as unique session-id)

--- manager/includes/document.parser.class.inc.php
+++ manager/includes/document.parser.class.inc.php
@@ -71,7 +71,8 @@ class DocumentParser {
     var $snipLapCount;
     var $messageQuitCount;
     var $time;
-	
+    var $sid;
+
     /**
      * Document constructor
      *
@@ -2510,14 +2511,15 @@ class DocumentParser {
         $type = intval($type);
         $id = intval($id);
         if(!$type || !$id || !$userId) return false;
-
-        $sql = sprintf('REPLACE INTO %s (internalKey, elementType, elementId, lasthit)
-                VALUES (%d, %d, %d, %d)',
+        
+        $sql = sprintf('REPLACE INTO %s (internalKey, elementType, elementId, lasthit, sid)
+                VALUES (%d, %d, %d, %d, \'%s\')',
             $this->getFullTableName('active_user_locks'),
             $userId,
             $type,
             $id,
-            $this->time
+            $this->time,
+            $this->sid
         );
         $this->db->query($sql);
     }
@@ -2561,15 +2563,15 @@ class DocumentParser {
         elseif ($cip = getenv("HTTP_X_FORWARDED_FOR")) $ip = $cip;
         elseif ($cip = getenv("REMOTE_ADDR"))          $ip = $cip;
         else                                           $ip = "UNKNOWN";
-
         $_SESSION['ip'] = $ip;
-
-        $sql = sprintf('REPLACE INTO %s (internalKey, lasthit, ip)
-            VALUES (%d, %d, \'%s\')',
+        
+        $sql = sprintf('REPLACE INTO %s (internalKey, lasthit, ip, sid)
+            VALUES (%d, %d, \'%s\', \'%s\')',
             $this->getFullTableName('active_user_sessions'),
             $this->getLoginUserID(),
             $this->time,
-            $ip
+            $ip,
+            $this->sid
         );
         $this->db->query($sql);
     }
