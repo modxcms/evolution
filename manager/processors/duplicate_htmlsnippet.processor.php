3c17ce374cc4655bbf9bd2e528a931660e9ee394
Deesen <dh@fuseit.de>
2016-12-06 13:41:15 +0100

    #200 O: Make "Duplicate of" translatable when duplicating elements/resources

--- manager/processors/duplicate_htmlsnippet.processor.php
+++ manager/processors/duplicate_htmlsnippet.processor.php
@@ -11,7 +11,7 @@ if($id==0) {
 
 // count duplicates
 $name = $modx->db->getValue($modx->db->select('name', $modx->getFullTableName('site_htmlsnippets'), "id='{$id}'"));
-$count = $modx->db->getRecordCount($modx->db->select('name', $modx->getFullTableName('site_htmlsnippets'), "name LIKE '{$name} Duplicate%'"));
+$count = $modx->db->getRecordCount($modx->db->select('name', $modx->getFullTableName('site_htmlsnippets'), "name LIKE '{$name} {$_lang['duplicated_el_suffix']}%'"));
 if($count>=1) $count = ' '.($count+1);
 else $count = '';
 
@@ -23,7 +23,7 @@ $newid = $modx->db->insert(
 		'snippet'=>'',
 		'category'=>'',
 		), $modx->getFullTableName('site_htmlsnippets'), // Insert into
-	"CONCAT(name, ' Duplicate{$count}') AS name, description, snippet, category", $modx->getFullTableName('site_htmlsnippets'), "id='{$id}'"); // Copy from
+	"CONCAT(name, ' {$_lang['duplicated_el_suffix']}{$count}') AS name, description, snippet, category", $modx->getFullTableName('site_htmlsnippets'), "id='{$id}'"); // Copy from
 
 // Set the item name for logger
 $name = $modx->db->getValue($modx->db->select('name', $modx->getFullTableName('site_htmlsnippets'), "id='{$newid}'"));
