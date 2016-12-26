8cbb924969f97af009fe5a3a909d8e26f10de004
Deesen <dh@fuseit.de>
2016-12-26 14:20:31 +0100

    [I] #1024 Added unique icon for users logged-in with multiple browsers using same manager-login

--- manager/actions/welcome.static.php
+++ manager/actions/welcome.static.php
@@ -172,7 +172,7 @@ $timetocheck = (time() - (60 * 20)); //+$server_offset_time;
 
 include_once(MODX_MANAGER_PATH.'includes/actionlist.inc.php');
 
-$rs    = $modx->db->select('*', $modx->getFullTableName('active_user_sessions')." us LEFT JOIN {$modx->getFullTableName('active_users')} au ON au.internalKey=us.internalKey", "", 'username ASC');
+$rs    = $modx->db->select('*, count(au.sid) AS count', $modx->getFullTableName('active_user_sessions')." us LEFT JOIN {$modx->getFullTableName('active_users')} au ON au.internalKey=us.internalKey GROUP BY au.sid HAVING au.action <> '8'", "", 'username ASC, au.sid ASC');
 $limit = $modx->db->getRecordCount($rs);
 if ($limit < 1) {
     $html = "<p>" . $_lang['no_active_users_found'] . "</p>";
@@ -192,11 +192,12 @@ if ($limit < 1) {
                   <tbody>
         ';
     while ($activeusers = $modx->db->getRow($rs)) {
-        $currentaction = getAction($activeusers['action'], $activeusers['id']);
+        $idle          = $activeusers['lasthit'] < $timetocheck ? ' class="userIdle"' : '';
+        $multipleSessions = $activeusers['count'] > 1 ? ' class="userMultipleSessions"' : '';
         $webicon       = ($activeusers['internalKey'] < 0) ? sprintf('<img src="%s" alt="Web user" />',$_style["tree_globe"]) : '';
-	    $idle          = $activeusers['lasthit'] < $timetocheck ? ' class="userIdle"' : '';
-        $params = array($idle, $activeusers['username'], $webicon, abs($activeusers['internalKey']), $activeusers['ip'], strftime('%H:%M:%S', $activeusers['lasthit'] + $server_offset_time),$currentaction);
-        $html .= vsprintf('<tr%s><td><strong>%s</strong></td><td>%s&nbsp;%s</td><td>%s</td><td>%s</td><td>%s</td></tr>', $params);
+        $currentaction = getAction($activeusers['action'], $activeusers['id']);
+        $params = array($idle, $multipleSessions, $activeusers['username'], $webicon, abs($activeusers['internalKey']), $activeusers['ip'], strftime('%H:%M:%S', $activeusers['lasthit'] + $server_offset_time),$currentaction);
+        $html .= vsprintf('<tr%s><td><strong%s>%s</strong></td><td>%s&nbsp;%s</td><td>%s</td><td>%s</td><td>%s</td></tr>', $params);
     }
     $html .= '
                 </tbody>
