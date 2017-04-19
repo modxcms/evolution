bf07b997e9ca5e643eaf16693175823607b4a20a
Nicola <modxit@modx.it>
2017-04-19 16:00:30 +0200

    Search mobile actionbutton
    Search mobile actionbutton

--- manager/actions/search.static.php
+++ manager/actions/search.static.php
@@ -74,7 +74,7 @@ if(isset($_REQUEST['searchid'])) {
           <td colspan="4">
             <ul class="actionButtons">
               <li>
-                <a class="default" href="#" onClick="document.searchform.submitok.click();"><i class="<?php echo $_style["actions_save"] ?>" aria-hidden="true"></i> <?php echo $_lang['search'] ?>
+                <a class="default" href="#" onClick="document.searchform.submitok.click();"><i class="<?php echo $_style["actions_search"] ?>" aria-hidden="true"></i> <span><?php echo $_lang['search'] ?></span>
                 </a>
               </li>
               <li>
