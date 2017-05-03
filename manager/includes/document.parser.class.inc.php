21f7d67e1b27a643fdc10e79ad20ca9eaa306cb8
yamamoto <yamamoto@kyms.jp>
2017-05-03 12:16:06 +0900

    [F] #1320 Comments in snippets call are not work correct now
    https://github.com/modxcms/evolution/issues/1320

--- manager/includes/document.parser.class.inc.php
+++ manager/includes/document.parser.class.inc.php
@@ -1558,7 +1558,7 @@ class DocumentParser {
                 if(in_array($delim, array('"', "'", '`')))
                 {
                     list($null, $value, $_tmp) = explode($delim, $_tmp, 3);
-                    if(substr(trim($_tmp), 0, 2)==='//') $_tmp = strstr($_tmp, "\n");
+                    if(substr(trim($_tmp), 0, 2)==='//') $_tmp = strstr(trim($_tmp), "\n");
                     $i=0;
                     while($delim==='`' && substr(trim($_tmp),0,1)!=='&' && 1<substr_count($_tmp,'`')) {
                         list($inner, $outer, $_tmp) = explode('`', $_tmp, 3);
