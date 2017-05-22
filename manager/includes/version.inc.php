30ea56aa608ecf5dae7690c6e834a1298a86758a
dmi3yy <dmi3yy@gmail.com>
2017-05-22 10:29:01 +0300

    Up version. 1.2.2-beta3

--- manager/includes/version.inc.php
+++ manager/includes/version.inc.php
@@ -1,5 +1,5 @@
 <?php
-$modx_version      = '1.2.2-beta2';      // Current version number
-$modx_release_date = 'Jan 30, 2017';     // Date of release
+$modx_version      = '1.2.2-beta3';      // Current version number
+$modx_release_date = 'May 22, 2017';     // Date of release
 $modx_branch       = 'Evolution';        // Codebase name
 $modx_full_appname = "MODX {$modx_branch} {$modx_version} ({$modx_release_date})";
