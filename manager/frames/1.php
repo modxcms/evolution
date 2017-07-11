2ff3f724f06fd08e5f613aedef340d3825af4d03
64j <64j@mail.ru>
2017-07-12 00:06:35 +0300

    update loader

--- manager/frames/1.php
+++ manager/frames/1.php
@@ -309,13 +309,7 @@ if($user['which_browser'] == 'default') {
 	</div>
 	<div id="main">
 		<iframe name="main" id="mainframe" src="index.php?a=<?php echo $initMainframeAction; ?>" scrolling="auto" frameborder="0" onload="modx.main.onload()"></iframe>
-		<div id="mainloader">
-			<div class="preLoaderText">
-				<img src="media/style/common/images/misc/logo.png" />
-				<p><?php echo $_lang['loading_page'] ?></p>
-				<i class="fa fa-cog fa-spin"></i>
-			</div>
-		</div>
+		<div id="mainloader"></div>
 	</div>
 	<div id="resizer"></div>
 	<div id="searchresult"></div>
