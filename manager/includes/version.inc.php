f1ec7e9c2485c4db15010b515d7eaf5a822ab521
dmi3yy <dmi3yy@gmail.com>
2017-07-19 12:52:49 +0300

    1.3.1

--- manager/includes/version.inc.php
+++ manager/includes/version.inc.php
@@ -1,5 +1,5 @@
 <?php
-$modx_version      = '1.3.0';      // Current version number
+$modx_version      = '1.3.1';      // Current version number
 $modx_release_date = 'Jul 19, 2017';     // Date of release
 $modx_branch       = 'Evolution CMS';        // Codebase name
 $modx_full_appname = "{$modx_branch} {$modx_version} ({$modx_release_date})";
