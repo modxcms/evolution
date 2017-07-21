94ddd2fd310b3829c5bab6c0c30b3c9a683677e8
yamamoto <yamamoto@kyms.jp>
2017-07-21 23:34:29 +0900

    Refactor

--- install/connection.collation.php
+++ install/connection.collation.php
@@ -14,41 +14,46 @@ if(!defined('MGR_DIR') && is_dir("{$base_path}manager")) {
 }
 require_once('lang.php');
 
-if ($conn = mysqli_connect($host, $uid, $pwd)) {
-    // get collation
-    $rs = mysqli_query($conn, "SHOW COLLATION");
-    if (mysqli_num_rows($rs) > 0) {
-        $output = '<select id="database_collation" name="database_collation">';
-        $_ = array();
-        while ($row = mysqli_fetch_row($rs)) {
-            $_[$row[0]] = '';
-        }
-        
-        $database_collation = htmlentities($_POST['database_collation']);
-        $recommend_collation = $_lang['recommend_collation'];
-        
-        if    (isset($_[$recommend_collation])) $_[$recommend_collation] = ' selected';
-        elseif(isset($_['utf8mb4_general_ci'])) $_['utf8mb4_general_ci'] = ' selected';
-        elseif(isset($_['utf8_general_ci']))    $_['utf8_general_ci']    = ' selected';
-        elseif(isset($_[$database_collation]))  $_[$database_collation]  = ' selected';
-        
-        $_ = sortItem($_,$_lang['recommend_collations_order']);
-        
-        foreach($_ as $collation=>$selected) {
-            $collation = htmlentities($collation);
-            // if(substr($collation,0,4)!=='utf8') continue;
-            if(strpos($collation,'sjis')===0) continue;
-            if($collation=='recommend')
-                $output .= '<optgroup label="recommend">';
-            elseif($collation=='unrecommend')
-                $output .= '</optgroup><optgroup label="unrecommend">';
-            else
-                $output .= sprintf('<option value="%s" %s>%s</option>', $collation, $selected, $collation);
-        }
-        $output .= '</optgroup></select>';
+$conn = mysqli_connect($host, $uid, $pwd);
+if(!$conn) exit('can not connect');
+
+// get collation
+$rs = mysqli_query($conn, "SHOW COLLATION");
+if (mysqli_num_rows($rs) > 0) {
+    $output = '<select id="database_collation" name="database_collation">';
+    $_ = array();
+    while ($row = mysqli_fetch_row($rs)) {
+        $_[$row[0]] = '';
     }
+    
+    $database_collation = htmlentities($_POST['database_collation']);
+    $recommend_collation = $_lang['recommend_collation'];
+    
+    if    (isset($_[$recommend_collation])) $_[$recommend_collation] = ' selected';
+    elseif(isset($_['utf8mb4_general_ci'])) $_['utf8mb4_general_ci'] = ' selected';
+    elseif(isset($_['utf8_general_ci']))    $_['utf8_general_ci']    = ' selected';
+    elseif(isset($_[$database_collation]))  $_[$database_collation]  = ' selected';
+    
+    $_ = sortItem($_,$_lang['recommend_collations_order']);
+    
+    foreach($_ as $collation=>$selected) {
+        $collation = htmlentities($collation);
+        // if(substr($collation,0,4)!=='utf8') continue;
+        if(strpos($collation,'sjis')===0) continue;
+        if($collation=='recommend')
+            $output .= '<optgroup label="recommend">';
+        elseif($collation=='unrecommend')
+            $output .= '</optgroup><optgroup label="unrecommend">';
+        else
+            $output .= sprintf('<option value="%s" %s>%s</option>', $collation, $selected, $collation);
+    }
+    $output .= '</optgroup></select>';
 }
+
 echo $output;
+exit;
+
+
 
 function sortItem($array=array(),$order='utf8mb4,utf8') {
     $rs = array('recommend'=>'');
