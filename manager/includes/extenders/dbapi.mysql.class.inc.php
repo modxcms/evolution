d7239159cb91417e09a86710dcdcdf8cd58881d2
Pathologic <maxx@np.by>
2017-08-08 21:08:39 +0300

    fix #167

--- manager/includes/extenders/dbapi.mysql.class.inc.php
+++ manager/includes/extenders/dbapi.mysql.class.inc.php
@@ -235,7 +235,7 @@ class DBAPI {
          $from = $this->replaceFullTableName($from);
          $where   = !empty($where)   ? (strpos(ltrim($where),   "WHERE")!==0    ? "WHERE {$where}"      : $where)   : '';
          $orderby = !empty($orderby) ? (strpos(ltrim($orderby), "ORDER BY")!==0 ? "ORDER BY {$orderby}" : $orderby) : '';
-         $limit   = !empty($limit)   ? (strpos(ltrim($limit),   "LIMIT")!==0    ? "LIMIT {$limit}"      : $limit)   : '';
+         $limit   = $limit !== ''   ? (strpos(ltrim($limit),   "LIMIT")!==0    ? "LIMIT {$limit}"      : $limit)   : '';
          return $this->query("DELETE FROM {$from} {$where} {$orderby} {$limit}");
       }
    }
@@ -263,7 +263,7 @@ class DBAPI {
       $limit   = trim($limit);
       if($where && stripos($where,'WHERE')!==0)     $where   = "WHERE {$where}";
       if($orderby && stripos($orderby,'ORDER')!==0) $orderby = "ORDER BY {$orderby}";
-      if($limit && stripos($limit,'LIMIT')!==0)     $limit   = "LIMIT {$limit}";
+      if($limit !== '' && stripos($limit,'LIMIT')!==0)     $limit   = "LIMIT {$limit}";
       return $this->query("SELECT {$fields} FROM {$from} {$where} {$orderby} {$limit}");
    }
 
@@ -314,7 +314,7 @@ class DBAPI {
                   $fromtable = $this->replaceFullTableName($fromtable);
                   $fields = "(".implode(",", array_keys($fields)).")";
                   $where = !empty($where) ? (strpos(ltrim($where), "WHERE")!==0 ? "WHERE {$where}" : $where) : '';
-                  $limit = !empty($limit) ? (strpos(ltrim($limit), "LIMIT")!==0 ? "LIMIT {$limit}" : $limit) : '';
+                  $limit = $limit !== '' ? (strpos(ltrim($limit), "LIMIT")!==0 ? "LIMIT {$limit}" : $limit) : '';
                   $rt = $this->query("INSERT INTO {$intotable} {$fields} SELECT {$fromfields} FROM {$fromtable} {$where} {$limit}");
                } else {
                   $ds = $this->select($fromfields, $fromtable, $where, '', $limit);
