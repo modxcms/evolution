fc7cf3ac25ea6df6bc3078197ee46be280cf8b22
dmi3yy <dmi3yy@gmail.com>
2017-09-04 13:19:31 +0300

    1.3.6

---
+++ manager/actions/help/version_notices/1.3.6.php
@@ -0,0 +1,9 @@
+<?php
+if(IN_MANAGER_MODE!="true") die("<b>INCLUDE_ORDERING_ERROR</b><br /><br />Please use the MODX Content Manager instead of accessing this file directly.");
+?>
+<p></p>
+<ul>
+    <li>[S] refactor ajax.php, close SQL injection (64j)</li>
+    <li>fix ditto error on 61 str. in ditto.class.inc.php (Dmi3yy)</li>
+</ul>
+
