3827f51b055a26a21c1fdbc1d56f42b2c2c3d66d
yamamoto <yamamoto@kyms.jp>
2017-09-12 21:15:01 +0900

    Revert - snippet.if.php

--- assets/snippets/if/snippet.if.php
+++ assets/snippets/if/snippet.if.php
@@ -18,15 +18,14 @@
 if(!defined('MODX_BASE_PATH')){die('What are you doing? Get out of here!');}
 
 $s = isset($separator) ? $separator: ':';
-if(!isset($math)) $math = 'off';
+$math = isset($math) ? $math : 'off';
 $lp = 0;
 $opers=explode($s,$is);
-$subject = $opers[0];
-$eq     = true;
-$and    = false;
-$or     = false;
-if(!isset($else)) $else = '';
-
+$subject=$opers[0];
+$eq=true;
+$and=false;
+$or = false;
+$else = isset($else) ? $else : '';
 // Prepare custom conditions
 $customConditions = array();
 if(!empty($custom)) {
@@ -45,8 +44,7 @@ if(!empty($custom)) {
     }
 }
 
-$total = count($opers);
-for ($i=1;$i<$total;$i++){
+for ($i=1;$i<count($opers);$i++){
     if ($opers[$i]=='or') {$or=true;$part_eq=$eq;$eq=true;continue;}
     if ($or) {$subject=$opers[$i];$or=false;continue;}
 
@@ -62,79 +60,94 @@ for ($i=1;$i<$total;$i++){
     $operator = $opers[$i];
     $operand  = isset($opers[$i+1]) ? $opers[$i+1] : '';
 
-    if (!isset($subject)) continue;
-    if (empty($operator)) continue;
-        
-    if(isset($customConditions[$operator])) {
-        $output = call_user_func($operator, $subject, $operand);
-        if(is_array($output)) {
-            if(isset($output[1]) && $output[1]) $i++; // $operand was used
-            $output = $output[0];
-        }
-        $output = (bool)$output;
-        $operator = 'custom';
-    }
-    
-    if ($math=='on' && !empty($subject)) {
-        $subject = preg_replace('@([a-zA-Z\n\r\t\s])@','',$subject);
-        $subject = $modx->safeEval('return ' . $subject.';');
-    }
-
-    switch (strtolower($operator)) {
-        case '%'  :$output = ($subject %$operand==0) ? true : false; break;
-        case '!=':
-        case 'not':$output = ($subject != $operand)  ? true : false; break;
-        case '<':
-        case 'lt' :$output = ($subject < $operand)   ? true : false; break;
-        case '>':
-        case 'gt' :$output = ($subject > $operand)   ? true : false; break;
-        case '<=':
-        case 'lte':$output = ($subject <= $operand)  ? true : false; break;
-        case '>=':
-        case 'gte':$output = ($subject >= $operand)  ? true : false; break;
-        case 'isempty':
-        case 'empty':$output = empty($subject)       ? true : false; break;
+    if (isset($subject)) {
+        if (!empty($operator)) {
             
-        case '!empty':
-        case 'notempty':
-        case 'not_empty':
-        case 'isnotempty':$output = empty($subject) || $subject == '' ? false : true;
-            break;
-        case 'isnull':
-        case 'null':$output = $subject == null || strtolower($subject) == 'null' ? true : false;
-            break;
-        case 'inarray':
-        case 'in_array':
-        case 'in':
-            $operand = explode(',',$operand);
-            $output = in_array($subject,$operand) ? true : false;
-            break;
-        case 'not_in':
-        case '!in':
-        case '!inarray':
-            $operand = explode(',',$operand);
-            $output = in_array($subject,$operand) ? false : true;
-            break;
-        case 'contains':
-            $output = (strpos($subject,$operand) !== false) ? true : false;
-            break;
-        case '!contains':
-        case 'not_contains':
-            $output = (strpos($subject,$operand) !== false) ? false : true;
-            break;
-        case 'custom':
-            break;
-        case '==':
-        case '=':
-        case 'eq':
-        case 'is':
-        default:
-            $output = ((string)$subject == (string)$operand) ? true : false;
-            break;
+            if(isset($customConditions[$operator])) {
+                $output = call_user_func($operator, $subject, $operand);
+                if(is_array($output)) {
+                    if(isset($output[1]) && $output[1]) $i++; // $operand was used
+                    $output = $output[0];
+                }
+                $output = (bool)$output;
+                $operator = 'custom';
+            }
+            
+            if ($math=='on' && !empty($subject)) {
+                $subject = preg_replace('@([a-zA-Z\n\r\t\s])@','',$subject);
+                $subject = $modx->safeEval('return ' . $subject.';');
+            }
+            $operator = strtolower($operator);
+
+            switch ($operator) {
+                case '%':
+                    $output = ($subject %$operand==0) ? true: false;$i++;
+                    break;
+                case '!=':
+                case 'not':$output = ($subject != $operand) ? true: false;$i++;
+                    break;
+                case '<':
+                case 'lt':$output = ($subject < $operand) ? true : false;$i++;
+                    break;
+                case '>':
+                case 'gt':$output = ($subject > $operand) ? true : false;$i++;
+                    break;
+                case '<=':
+                case 'lte':$output = ($subject <= $operand) ? true : false;$i++;
+                    break;
+                case '>=':
+                case 'gte':$output = ($subject >= $operand) ? true : false;$i++;
+                    break;
+                case 'isempty':
+                case 'empty':$output = empty($subject) ? true : false;
+                    break;
+                case '!empty':
+                case 'notempty':
+                case 'not_empty':
+                case 'isnotempty':$output = empty($subject) || $subject == '' ? false : true;
+                    break;
+                case 'isnull':
+                case 'null':$output = $subject == null || strtolower($subject) == 'null' ? true : false;
+                    break;
+                case 'inarray':
+                case 'in_array':
+                case 'in':
+                    $operand = explode(',',$operand);
+                    $output = in_array($subject,$operand) ? true : false;
+                    $i++;
+                    break;
+                case 'not_in':
+                case '!in':
+                case '!inarray':
+                    $operand = explode(',',$operand);
+                    $output = in_array($subject,$operand) ? false : true;
+                    $i++;
+                    break;
+                case 'contains':
+                    $output = (strpos($subject,$operand) !== false) ? true : false;
+                    $i++;
+                    break;
+                case '!contains':
+                case 'not_contains':
+                    $output = (strpos($subject,$operand) !== false) ? false : true;
+                    $i++;
+                    break;
+                case 'custom':
+                    break;
+                case '==':
+                case '=':
+                case 'eq':
+                case 'is':
+                default:
+                    $output = ((string)$subject == (string)$operand) ? true : false;
+                    $i++;
+                    break;
+            }
+
+            $eq=$output?$eq:false;
+
+        }
     }
-    $eq=$output?$eq:false;
-    $_ = explode(',','isempty,empty,!empty,notempty,not_empty,isnotempty,isnull,null,custom,==,=,eq,is');
-    if(!in_array(strtolower($operator), $_)) $i++;
 }
 if ($lp==1){
     if ($left_part) {
