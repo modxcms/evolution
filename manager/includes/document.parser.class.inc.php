722b6e7d3ce7eed6c80691d1dc84d6cc456336da
yamamoto <yamamoto@kyms.jp>
2017-09-24 00:12:40 +0900

    Fix - parse IF tas

--- manager/includes/document.parser.class.inc.php
+++ manager/includes/document.parser.class.inc.php
@@ -1532,10 +1532,10 @@ class DocumentParser
      */
     function mergeConditionalTagsContent($content, $iftag = '<@IF:', $elseiftag = '<@ELSEIF:', $elsetag = '<@ELSE>', $endiftag = '<@ENDIF>')
     {
-        $content = $this->_prepareCTag($content, $iftag, $elseiftag, $elsetag, $endiftag);
         if (strpos($content, $iftag) === false) {
             return $content;
         }
+        $content = $this->_prepareCTag($content, $iftag, $elseiftag, $elsetag, $endiftag);
 
         $sp = '#' . md5('ConditionalTags' . $_SERVER['REQUEST_TIME']) . '#';
         $content = str_replace(array('<?php', '?>'), array("{$sp}b", "{$sp}e"), $content);
