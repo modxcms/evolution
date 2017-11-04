7d4acbf38472b7cf498b2ccb3a2f1f235dac552f
dmi3yy <dmi3yy@gmail.com>
2017-11-04 21:45:25 +0300

    [C] move ajaxSearch to Extras (https://github.com/extras-evolution/ajaxSearch)

--- assets/snippets/ajaxSearch/lang/chinese_simplified.inc.php
+++
@@ -1,5 +0,0 @@
-<?php
-    $filename = dirname(__FILE__) . '/chinese_simplified-utf8.inc.php';
-    $contents = file_get_contents($filename);
-    $contents = mb_convert_encoding($contents, 'EUC-CN', 'UTF-8');
-    eval('?>' . $contents);
