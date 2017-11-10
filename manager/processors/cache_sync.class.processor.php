0147a55751a9b0511088ef5d1b2411b4622e2cfa
64j <64j@mail.ru>
2017-11-10 17:30:14 +0300

    fix minify cache for chunks

--- manager/processors/cache_sync.class.processor.php
+++ manager/processors/cache_sync.class.processor.php
@@ -265,6 +265,7 @@ class synccache
         $content .= '$c=&$this->chunkCache;';
         while ($doc = $modx->db->getRow($rs)) {
             if ($modx->config['minifyphp_incache']) {
+                $doc['snippet'] = $this->php_strip_whitespace($doc['snippet']);
                 $doc['snippet'] = preg_replace(array('|\s+|', '|<!--|', '|-->|', '|-->\s+<!--|'), array(' ', "\n" . '<!--', '-->' . "\n", '-->' . "\n" . '<!--'), $doc['snippet']);
             }
             $content .= '$c[\'' . $doc['name'] . '\']=\'' . ($doc['disabled'] ? '' : $this->escapeSingleQuotes($doc['snippet'])) . '\';';
