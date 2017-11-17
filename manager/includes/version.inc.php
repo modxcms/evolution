8f2fd4d748610ed3b1bfa169fd2e2c16b06ab709
dmi3yy <dmi3yy@gmail.com>
2017-11-17 15:26:43 +0200

    1.4.0.RC2

--- manager/includes/version.inc.php
+++ manager/includes/version.inc.php
@@ -1,5 +1,5 @@
 <?php
-$modx_version      = '1.4.0.RC';      // Current version number
-$modx_release_date = 'Nov 06, 2017';     // Date of release
+$modx_version      = '1.4.0.RC2';      // Current version number
+$modx_release_date = 'Nov 17, 2017';     // Date of release
 $modx_branch       = 'Evolution';        // Codebase name
 $modx_full_appname = "{$modx_branch} {$modx_version} ({$modx_release_date})";
