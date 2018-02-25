f626f88a62cd018e4c917b8c9fcf3e7c401b09b2
Agel_Nash <agel_nash@xaker.ru>
2018-02-25 15:04:00 +0500

    public method

--- manager/processors/user_documents_permissions.class.php
+++ manager/processors/user_documents_permissions.class.php
@@ -2,13 +2,13 @@
 
 class udperms{
 
-	var $user;
-	var $document;
-	var $role;
-	var $duplicateDoc = false;
-	
-	function checkPermissions() {
-		
+    public $user;
+    public $document;
+    public $role;
+    public $duplicateDoc = false;
+
+    public function checkPermissions() {
+
 		global $udperms_allowroot;
 		global $modx;
 
@@ -22,17 +22,17 @@ class udperms{
 		if($role==1) {
 			return true;  // administrator - grant all document permissions
 		}
-		
+
 		if($modx->config['use_udperms']==0 || $modx->config['use_udperms']=="" || !isset($modx->config['use_udperms'])) {
 			return true; // permissions aren't in use
 		}
-		
+
 		$parent = $modx->db->getValue($modx->db->select('parent', $tblsc, "id='{$this->document}'"));
 		if($document == 0 && $parent == NULL && $udperms_allowroot == 1) return true; // User is allowed to create new document in root
 		if (($this->duplicateDoc==true || $document==0) && $parent==0 && $udperms_allowroot==0) {
 			return false; // deny duplicate || create new document at root if Allow Root is No
 		}
-		
+
 		// get document groups for current user
 		if($_SESSION['mgrDocgroups']) {
 			$docgrp = implode(" || dg.document_group = ",$_SESSION['mgrDocgroups']);
@@ -40,14 +40,14 @@ class udperms{
 
 		/* Note:
 			A document is flagged as private whenever the document group that it
-			belongs to is assigned or links to a user group. In other words if 
-			the document is assigned to a document group that is not yet linked 
-			to a user group then that document will be made public. Documents that 
-			are private to the manager users will not be private to web users if the 
+			belongs to is assigned or links to a user group. In other words if
+			the document is assigned to a document group that is not yet linked
+			to a user group then that document will be made public. Documents that
+			are private to the manager users will not be private to web users if the
 			document group is not assigned to a web user group and visa versa.
 		 */
 		$permissionsok = false;  // set permissions to false
-		
+
 		$rs = $modx->db->select(
 			'count(DISTINCT sc.id)',
 			"{$tblsc} AS sc 
@@ -57,7 +57,7 @@ class udperms{
 			);
 		$limit = $modx->db->getValue($rs);
 		if($limit==1) $permissionsok = true;
-		
+
 		return $permissionsok;
 	}
 }
