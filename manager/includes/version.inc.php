fe37ac6c23dccfb2039dee72df75306b7ddeaee7
dmi3yy <dmi3yy@gmail.com>
2018-03-02 10:06:53 +0200

    version

--- manager/includes/version.inc.php
+++ manager/includes/version.inc.php
@@ -1,5 +1,5 @@
 <?php
-$modx_version      = '1.5.0-beta';      // Current version number
-$modx_release_date = 'Jan 26, 2018';     // Date of release
+$modx_version      = '1.4.1';      // Current version number
+$modx_release_date = 'Mar 02, 2018';     // Date of release
 $modx_branch       = 'Evolution';        // Codebase name
 $modx_full_appname = "{$modx_branch} {$modx_version} ({$modx_release_date})";
