ca9591e0c8c67f5bc3e8f198cd81aafa1118c9c7
Serg <64j@mail.ru>
2018-06-09 01:01:27 +0300

    Fix parseProperties when install

--- install/src/functions.php
+++ install/src/functions.php
@@ -534,39 +534,39 @@ if (!function_exists('parse_docblock')) {
     }
 }
 
-if (!function_exists('parseProperties')) {
-    /**
-     * parses a resource property string and returns the result as an array
-     * duplicate of method in documentParser class
-     *
-     * @param string $propertyString
-     * @return array
-     */
-    function parseProperties($propertyString)
-    {
-        $parameter = array();
-        if (!empty ($propertyString)) {
-            $tmpParams = explode("&", $propertyString);
-            $countParams = count($tmpParams);
-            for ($x = 0; $x < $countParams; $x++) {
-                if (strpos($tmpParams[$x], '=', 0)) {
-                    $pTmp = explode("=", $tmpParams[$x]);
-                    $pvTmp = explode(";", trim($pTmp[1]));
-                    if ($pvTmp[1] == 'list' && $pvTmp[3] != "") {
-                        $parameter[trim($pTmp[0])] = $pvTmp[3];
-                    } //list default
-                    else {
-                        if ($pvTmp[1] != 'list' && $pvTmp[2] != "") {
-                            $parameter[trim($pTmp[0])] = $pvTmp[2];
-                        }
-                    }
-                }
-            }
-        }
-
-        return $parameter;
-    }
-}
+//if (!function_exists('parseProperties')) {
+//    /**
+//     * parses a resource property string and returns the result as an array
+//     * duplicate of method in documentParser class
+//     *
+//     * @param string $propertyString
+//     * @return array
+//     */
+//    function parseProperties($propertyString)
+//    {
+//        $parameter = array();
+//        if (!empty ($propertyString)) {
+//            $tmpParams = explode("&", $propertyString);
+//            $countParams = count($tmpParams);
+//            for ($x = 0; $x < $countParams; $x++) {
+//                if (strpos($tmpParams[$x], '=', 0)) {
+//                    $pTmp = explode("=", $tmpParams[$x]);
+//                    $pvTmp = explode(";", trim($pTmp[1]));
+//                    if ($pvTmp[1] == 'list' && $pvTmp[3] != "") {
+//                        $parameter[trim($pTmp[0])] = $pvTmp[3];
+//                    } //list default
+//                    else {
+//                        if ($pvTmp[1] != 'list' && $pvTmp[2] != "") {
+//                            $parameter[trim($pTmp[0])] = $pvTmp[2];
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//
+//        return $parameter;
+//    }
+//}
 
 if (!function_exists('propUpdate')) {
     /**
