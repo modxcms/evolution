eae5c3354c3ed2305521ed118d898238c6291b53
Agel_Nash <agel_nash@xaker.ru>
2018-06-20 21:02:08 +0500

    Composer dependency

--- core/legacy.php
+++ core/legacy.php
@@ -1,14 +1,5 @@
 <?php
-/**
- *    MODX Document Parser
- *    Function: This class contains the main document parsing functions
- *
- */
 
-class DocumentParser extends EvolutionCMS\Core
-{
-}
-
-class SystemEvent extends EvolutionCMS\Event
-{
-}
+class DocumentParser extends EvolutionCMS\Core{}
+class SystemEvent extends EvolutionCMS\Event{}
+class Snoopy extends Snoopy\Snoopy{}
