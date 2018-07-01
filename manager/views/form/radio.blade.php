f9fd6097b51e2f088c18ff6deabd67d574520c64
Serg <64j@mail.ru>
2018-07-01 04:04:06 +0300

    Form views

--- manager/views/form/radio.blade.php
+++ manager/views/form/radio.blade.php
@@ -10,15 +10,22 @@
     </label>
     <div class="col-7 col-md-9 col-lg-10">
         @if(!empty($options))
-            @foreach ($options as $option)
+            @foreach ($options as $key => $option)
                 <div class="radio">
                     <label>
-                        <input type="radio" name="{{ $name }}" value="{{ $option['value'] }}"
-                                {!! $option['attributes'] or '' !!}
-                                @if($value == $option['value']) checked="checked" @endif
-                        @if($disabled || $option['disabled']) disabled @endif
-                        />
-                        {!! $option['text'] !!}
+                        @if(is_string($option))
+                            <input type="radio" name="{{ $name }}" value="{{ $key }}"
+                                    @if($value == $key) checked="checked" @endif
+                            />
+                            {!! $option !!}
+                        @else
+                            <input type="radio" name="{{ $name }}" value="{{ $option['value'] }}"
+                                    {!! $option['attributes'] or '' !!}
+                                    @if($value == $option['value']) checked="checked" @endif
+                            @if($disabled || $option['disabled']) disabled @endif
+                            />
+                            {!! $option['text'] !!}
+                        @endif
                     </label>
                 </div>
             @endforeach
