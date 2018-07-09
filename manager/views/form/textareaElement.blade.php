f4fabd1c6d4c69ce9d55821598cb53f443a50934
Serg <64j@mail.ru>
2018-07-10 02:47:49 +0300

    Add view form/textareaElement

---
+++ manager/views/form/textareaElement.blade.php
@@ -0,0 +1,5 @@
+<textarea class="form-control {{ $class or '' }}" name="{{ $name }}" id="{{ $id or $name }}" rows="{{ $rows or '3' }}"
+    @if(!empty($placeholder)) placeholder="{{ $placeholder }}" @endif
+{!! $attributes or '' !!}
+@if(!empty($readonly)) readonly @endif
+>{!! $value !!}</textarea>
