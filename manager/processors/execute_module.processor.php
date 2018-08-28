473725d08715ed8ba16a32a91b2b28f42c47bc1f
Serg <64j@mail.ru>
2018-08-28 20:18:58 +0300

    fixed #794

--- manager/processors/execute_module.processor.php
+++ manager/processors/execute_module.processor.php
@@ -59,12 +59,7 @@ $parameter = $modx->parseProperties($content["properties"], $content["guid"], 'm
 // Set the item name for logger
 $_SESSION['itemname'] = $content['name'];
 
-
-$output = evalModule($content["modulecode"],$parameter);
-if (strpos(trim($output),'<')===0 && strpos(trim($output),'<?xml')!==0) {
-	echo "<style>@supports (-webkit-overflow-scrolling: touch) {body,html {-webkit-overflow-scrolling: touch;overflow:auto!important;height:100%!important}}</style>"; // for iframe scroller
-}
-echo $output;
+echo evalModule($content["modulecode"], $parameter);
 include MODX_MANAGER_PATH."includes/sysalert.display.inc.php";
 
 /**
