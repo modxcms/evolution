46eaa39e63d0280c9848ac3d09ee92c967788ee5
Agel_Nash <agel-nash@yandex.com>
2018-10-12 16:20:24 +0000

    Resolve review in PR #779

--- assets/lib/class.modxRTEbridge.php
+++ assets/lib/class.modxRTEbridge.php
@@ -966,7 +966,7 @@ class modxRTEbridge
 
         $templatesArr = array();
 
-        if (IN_MANAGER_MODE) {
+        if ($modx->getLoginUserType() === 'manager' || IN_MANAGER_MODE) {
 
             $modx->getSettings();
             $ids    = $modx->config[$this->editorKey.'_template_docs'];
