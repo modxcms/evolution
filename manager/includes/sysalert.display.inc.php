7043a5321108334ac063c70088436b42a3eb04c6
Serg <64j@mail.ru>
2018-10-29 22:48:23 +0300

    fix sysalert.display.inc.php

--- manager/includes/sysalert.display.inc.php
+++ manager/includes/sysalert.display.inc.php
@@ -1,46 +1,46 @@
 <?php
 
-	/**
-	 *	System Alert Message Queue Display file
-	 *	Written By Raymond Irving, April, 2005
-	 *
-	 *	Used to display system alert messages inside the browser
-	 *
-	 */
+/**
+ *    System Alert Message Queue Display file
+ *    Written By Raymond Irving, April, 2005
+ *
+ *    Used to display system alert messages inside the browser
+ *
+ */
 
-	require_once(dirname(__FILE__).'/protect.inc.php');
+require_once(dirname(__FILE__) . '/protect.inc.php');
 
-	$sysMsgs = "";
-	$limit = count($SystemAlertMsgQueque);
-	for($i=0;$i<$limit;$i++) {
-		$sysMsgs .= $SystemAlertMsgQueque[$i]."<hr sys/>";
-	}
-	// reset message queque
-	unset($_SESSION['SystemAlertMsgQueque']);
-	$_SESSION['SystemAlertMsgQueque'] = array();
-	$SystemAlertMsgQueque = &$_SESSION['SystemAlertMsgQueque'];
+$sysMsgs = '';
+$limit = count($SystemAlertMsgQueque);
+for ($i = 0; $i < $limit; $i++) {
+    $sysMsgs .= $SystemAlertMsgQueque[$i] . '<hr sys/>';
+}
+// reset message queque
+unset($_SESSION['SystemAlertMsgQueque']);
+$_SESSION['SystemAlertMsgQueque'] = array();
+$SystemAlertMsgQueque = &$_SESSION['SystemAlertMsgQueque'];
 
-	if($sysMsgs!="") {
-?>
-
-<?php // fetch the styles
-	echo '<link rel="stylesheet" type="text/css" href="'.MODX_MANAGER_URL.'media/style/'.$manager_theme.'/style.css'.'" />';
-?>
-<script type="text/javascript">
-// <![CDATA[
-window.addEvent('domready', function() {
-			var sysAlert = new Element('div').setProperties({
-				'class': 'sysAlert'
-			});
-			sysAlert.innerHTML = '<?php echo $modx->db->escape($sysMsgs);?>';
-			var boxHtml = new MooPrompt('<?php echo $_lang['sys_alert']; ?>', sysAlert, {
-				buttons: 1,
-				button1: 'Ok',
-				width: 500
-			});
-});
-// ]]>
-</script>
-<?php
-	}
+if ($sysMsgs != '') {
+    // fetch the styles
+    ?>
+    <link rel="stylesheet" type="text/css" href="<?= MODX_MANAGER_URL ?>media/style/<?= $manager_theme ?>/style.css" />
+    <script>
+      document.addEventListener('DOMContentLoaded', function() {
+        if (parent.modx) {
+          parent.modx.popup({
+            title: '<?= $_lang['sys_alert'] ?>',
+            content: '<?= $modx->db->escape($sysMsgs) ?>',
+            wrap: document.body,
+            type: 'warning',
+            width: '400px',
+            hide: 0,
+            hover: 0,
+            overlay: 1,
+            overlayclose: 1
+          });
+        }
+      });
+    </script>
+    <?php
+}
 ?>
