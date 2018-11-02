118766ddc9ab6d9512f7d5e66ffae8643674d4ab
Dmi3yy <dmi3yy@gmail.com>
2018-11-02 11:29:40 +0200

    update readme and version

--- manager/includes/version.inc.php
+++ manager/includes/version.inc.php
@@ -1,5 +1,5 @@
 <?php
-$modx_version      = '1.4.5'; // Current version number
-$modx_release_date = 'Aug 07, 2018'; // Date of release
+$modx_version      = '1.4.6'; // Current version number
+$modx_release_date = 'Nov 02, 2018'; // Date of release
 $modx_branch       = 'Evolution'; // Codebase name
 $modx_full_appname = "{$modx_branch} {$modx_version} ({$modx_release_date})";
