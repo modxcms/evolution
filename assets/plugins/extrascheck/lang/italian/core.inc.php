c7cd2ccce3fac8509dce4732788151dcc931f4b7
Agel_Nash <agel-nash@yandex.com>
2018-11-02 00:54:56 +0000

    Update OutdatedExtrasCheck

---
+++ assets/plugins/extrascheck/lang/italian/core.inc.php
@@ -0,0 +1,32 @@
+<?php
+// ---------------------------------------------------------------
+// :: OutdatedExtrasCheck
+// ----------------------------------------------------------------
+//
+//
+//
+// ----------------------------------------------------------------
+// :: Copyright & Licencing
+// ----------------------------------------------------------------
+//
+//   GNU General Public License (GPL - http://www.gnu.org/copyleft/gpl.html)
+//
+$_lang = array();
+
+$_lang['title'] = 'Controllo Compatibilità Extras';
+$_lang['outdated'] = 'è <b>obsoleto</b>';
+$_lang['isoutdated'] = 'è <b>obsoleto</b> e non compatibile con';
+$_lang['please_update'] = 'Aggiorna';
+$_lang['please_delete'] = 'Per favore cancellalo';
+$_lang['please_download_and_install'] = 'Per favore scarica e installa';
+$_lang["to_latest"] = 'all\'ultima versione';
+$_lang['min_required'] = 'minima richiesta ';
+$_lang['from'] = 'dal';
+$_lang['extras_module'] = 'Modulo Extras';
+$_lang['or_move_to'] = 'o passa a';
+$_lang['not_used'] = 'non è più utilizzato da';
+$_lang['from_folder'] = 'dalla cartella';
+$_lang['if_dont_use'] = 'Se non lo usi per altri scopi';
+$_lang['security_reason'] = 'vulnerabile';
+
+return $_lang;
