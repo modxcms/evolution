8f2f8f3f0351e964145e322d129709d04283bd50
Dmi3yy <dmi3yy@gmail.com>
2018-11-02 11:29:40 +0200

    update readme and version

--- core/factory/version.php
+++ core/factory/version.php
@@ -1,7 +1,7 @@
 <?php
 return [
-    'version' => '2.0.0 beta', // Current version number
-    'release_date' => 'Jun 25, 2018', // Date of release
+    'version' => '2.0.0 alpha', // Current version number
+    'release_date' => 'Nov 02, 2018', // Date of release
     'branch' => 'Evolution CMS', // Codebase name
     'full_appname' => 'Evolution CMS 2.0.0 beta (Jun 25, 2018)'
 ];
