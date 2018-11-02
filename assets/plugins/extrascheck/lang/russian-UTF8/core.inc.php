9f87428e96b3acd03f183f91c27b0970a84d452e
Agel_Nash <agel-nash@yandex.com>
2018-11-02 00:54:56 +0000

    Update OutdatedExtrasCheck

---
+++ assets/plugins/extrascheck/lang/russian-UTF8/core.inc.php
@@ -0,0 +1,32 @@
+<?php
+// ---------------------------------------------------------------
+// :: OutdatedExtrasCheck
+// ----------------------------------------------------------------
+//
+//
+//
+// ----------------------------------------------------------------
+// :: Copyright & Licencing
+// ----------------------------------------------------------------
+//
+//   GNU General Public License (GPL - http://www.gnu.org/copyleft/gpl.html)
+//
+$_lang = array();
+
+$_lang['title'] = 'Проверка совместимости дополнений';
+$_lang['outdated'] = '<b>устаревший</b>';
+$_lang['isoutdated'] = 'больше не совместим с';
+$_lang['please_update'] = 'Пожалуйста, обновите';
+$_lang['please_delete'] = 'Пожалуйста, удалите';
+$_lang['please_download_and_install'] = 'скачать и установить';
+$_lang['to_latest'] = 'до последней версии';
+$_lang['min_required'] = 'минимально необходимый';
+$_lang['from'] = 'через';
+$_lang['extras_module'] = 'Extras Модуль';
+$_lang['or_move_to'] = 'или замените на';
+$_lang['not_used'] = 'больше не используется';
+$_lang['from_folder'] = 'его из папки';
+$_lang['if_dont_use'] = 'Если вы не используете';
+$_lang['security_reason'] = 'уязвим';
+
+return $_lang;
