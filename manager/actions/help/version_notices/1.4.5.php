735650a5282bdba8edd90a00e6aab820767b3889
Nicola <nlambathakis@gmail.com>
2018-11-05 14:09:14 +0100

    1.4.5 Version Noticies (help)

---
+++ manager/actions/help/version_notices/1.4.5.php
@@ -0,0 +1,15 @@
+<?php
+if( ! defined('IN_MANAGER_MODE') || IN_MANAGER_MODE !== true) {
+    die("<b>INCLUDE_ORDERING_ERROR</b><br /><br />Please use the EVO Content Manager instead of accessing this file directly.");
+}
+?>
+<p></p>
+<p>In 1.4.5, the main attention was paid to correcting errors and also checking that everything worked as stably as possible.</p>
+<p>It was corrected a lot of mistakes and added some useful things.</p>
+<ul>
+  <li>[U] Updated KCFinder and added new events to it</li>
+	<li>[I] Fixed issues with TinyMCE4</li>
+	<li>[I] Support for MySql 8</li>
+</ul>
+<p></p>
+<p>You can see the full list of 1.4.5 changes in the changelog <a href="https://github.com/evolution-cms/evolution/blob/1.4.x/assets/docs/changelog.txt" target="_blank">https://github.com/evolution-cms/evolution/blob/1.4.x/assets/docs/changelog.txt</a></p>
