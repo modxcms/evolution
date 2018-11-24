181e3827228291a2759c21b7a596b8a5b96af43f
Serg <64j@mail.ru>
2018-11-24 22:17:10 +0300

    add function getDefaultTemplate for action mutate_content

--- core/functions/actions/mutate_content.php
+++ core/functions/actions/mutate_content.php
@@ -1,5 +1,57 @@
 <?php
 
+if (!function_exists('getDefaultTemplate')) {
+    /**
+     * @return string
+     */
+    function getDefaultTemplate()
+    {
+        $modx = evolutionCMS();
+
+        $default_template = '';
+        switch ($modx->getConfig('auto_template_logic')) {
+            case 'sibling':
+                if (!isset($_GET['pid']) || empty($_GET['pid'])) {
+                    $site_start = $modx->getConfig('site_start');
+                    $where = "sc.isfolder=0 AND sc.id!='{$site_start}'";
+                    $sibl = $modx->getDocumentChildren($_REQUEST['pid'], 1, 0, 'template,menuindex', $where,
+                        'menuindex', 'ASC', 1);
+                    if (isset($sibl[0]['template']) && $sibl[0]['template'] !== '') {
+                        $default_template = $sibl[0]['template'];
+                    }
+                } else {
+                    $sibl = $modx->getDocumentChildren($_REQUEST['pid'], 1, 0, 'template,menuindex', 'isfolder=0',
+                        'menuindex', 'ASC', 1);
+                    if (isset($sibl[0]['template']) && $sibl[0]['template'] !== '') {
+                        $default_template = $sibl[0]['template'];
+                    } else {
+                        $sibl = $modx->getDocumentChildren($_REQUEST['pid'], 0, 0, 'template,menuindex', 'isfolder=0',
+                            'menuindex', 'ASC', 1);
+                        if (isset($sibl[0]['template']) && $sibl[0]['template'] !== '') {
+                            $default_template = $sibl[0]['template'];
+                        }
+                    }
+                }
+                if (isset($default_template)) {
+                    break;
+                } // If $default_template could not be determined, fall back / through to "parent"-mode
+            case 'parent':
+                if (isset($_REQUEST['pid']) && !empty($_REQUEST['pid'])) {
+                    $parent = $modx->getPageInfo($_REQUEST['pid'], 0, 'template');
+                    if (isset($parent['template'])) {
+                        $default_template = $parent['template'];
+                    }
+                }
+                break;
+            case 'system':
+            default: // default_template is already set
+                $default_template = $modx->getConfig('default_template');
+        }
+
+        return empty($default_template) ? $modx->getConfig('default_template') : $default_template;
+    }
+}
+
 if (! function_exists('ProcessTVCommand')) {
     /**
      * @param string $value
