e8c30d156956b1ee2ecdfdcc5f8fb2f0b400b12d
Agel_Nash <agel-nash@yandex.com>
2018-11-26 14:20:29 +0000

    Up phpthumb to 1.3.3

--- assets/snippets/phpthumb/snippet.phpthumb.php
+++ assets/snippets/phpthumb/snippet.phpthumb.php
@@ -5,7 +5,7 @@
  * PHPThumb creates thumbnails and altered images on the fly and caches them
  *
  * @category    snippet
- * @version    1.3.1
+ * @version    1.3.3
  * @license    http://www.gnu.org/copyleft/gpl.html GNU Public License (GPL)
  * @internal    @properties
  * @internal    @modx_category Content
@@ -16,50 +16,46 @@
  * @link        noimage.png here [+site_url+]assets/snippets/phpthumb/noimage.png
  * @author      Bumkaka
  * @author      Many contributors since then
- * @lastupdate  30/10/2018
+ * @lastupdate  26/11/2018
  */
-if (!defined('MODX_BASE_PATH')) {
+if (! defined('MODX_BASE_PATH')) {
     die('What are you doing? Get out of here!');
 }
 
 $newFolderAccessMode = $modx->getConfig('new_folder_permissions');
 $newFolderAccessMode = empty($new) ? 0777 : octdec($newFolderAccessMode);
 
-$cacheFolder = isset($cacheFolder) ? $cacheFolder : $modx->getCacheFolder() . "/images";
+$cacheFolder = isset($cacheFolder) ? $cacheFolder : $modx->getCacheFolder() . 'images';
+$phpThumbPath = isset($phpThumbPath) ? $phpThumbPath : 'assets/snippets/phpthumb/';
+
 /**
  * @see: https://github.com/kalessil/phpinspectionsea/blob/master/docs/probable-bugs.md#mkdir-race-condition
  */
 $path = MODX_BASE_PATH . $cacheFolder;
-if (!file_exists($path) && mkdir($path) && is_dir($path)) {
-    chmod(MODX_BASE_PATH . $cacheFolder, $newFolderAccessMode);
+if (! file_exists($path) && mkdir($path) && is_dir($path)) {
+    chmod($path, $newFolderAccessMode);
 }
 
-$tmpFolder = $modx->getCacheFolder() . '/tmp';
 if (!empty($input)) {
     $input = rawurldecode($input);
 }
 
-if (empty($input) || !file_exists(MODX_BASE_PATH . $input)) {
-    $input = isset($noImage) ? $noImage : 'assets/snippets/phpthumb/noimage.png';
+if (empty($input) || ! file_exists(MODX_BASE_PATH . $input)) {
+    $input = isset($noImage) ? $noImage : $phpThumbPath . 'noimage.png';
 }
 
 /**
  * allow read in phpthumb cache folder
  */
-if (!file_exists(MODX_BASE_PATH . $cacheFolder . '/.htaccess') &&
+if (! file_exists(MODX_BASE_PATH . $cacheFolder . '/.htaccess') &&
     $cacheFolder !== $modx->getCacheFolder() &&
     strpos($cacheFolder, $modx->getCacheFolder()) === 0
 ) {
     file_put_contents(MODX_BASE_PATH . $cacheFolder . '/.htaccess', "order deny,allow\nallow from all\n");
 }
 
-$path = MODX_BASE_PATH . $tmpFolder;
-if (!file_exists($path) && mkdir($path) && is_dir($path)) {
-    chmod($path, $newFolderAccessMode);
-}
-
 $path_parts = pathinfo($input);
-$tmpImagesFolder = str_replace('assets/images', '', $tmpImagesFolder);
+$tmpImagesFolder = str_replace('assets/images', '', $path_parts['dirname']);
 $tmpImagesFolder = explode('/', $tmpImagesFolder);
 $ext = strtolower($path_parts['extension']);
 $options = 'f=' . (in_array($ext, array('png', 'gif', 'jpeg')) ? $ext : 'jpg&q=85') . '&' .
@@ -67,10 +63,10 @@ $options = 'f=' . (in_array($ext, array('png', 'gif', 'jpeg')) ? $ext : 'jpg&q=8
 
 parse_str($options, $params);
 foreach ($tmpImagesFolder as $folder) {
-    if (!empty($folder)) {
+    if (! empty($folder)) {
         $cacheFolder .= '/' . $folder;
         $path = MODX_BASE_PATH . $cacheFolder;
-        if (!file_exists($path) && mkdir($path) && is_dir($path)) {
+        if (! file_exists($path) && mkdir($path) && is_dir($path)) {
             chmod($path, $newFolderAccessMode);
         }
     }
@@ -79,15 +75,18 @@ foreach ($tmpImagesFolder as $folder) {
 $fNamePref = rtrim($cacheFolder, '/') . '/';
 $fName = $path_parts['filename'];
 $fNameSuf = '-' .
-    $params['w'] .'x' . $params['h'] . '-' .
+    (isset($params['w']) ? $params['w'] : '') .'x' . (isset($params['h']) ? $params['h'] : '') . '-' .
     substr(md5(serialize($params) . filemtime(MODX_BASE_PATH . $input)), 0, 3) .
     '.' . $params['f'];
 
 $outputFilename = MODX_BASE_PATH . $fNamePref . $fName . $fNameSuf;
-if (!file_exists($outputFilename)) {
-    require_once MODX_BASE_PATH . 'assets/snippets/phpthumb/phpthumb.class.php';
+if (! file_exists($outputFilename)) {
+    if (! class_exists('phpthumb')) {
+        require_once MODX_BASE_PATH . $phpThumbPath . '/phpthumb.class.php';
+    }
     $phpThumb = new phpthumb();
-    $phpThumb->config_temp_directory = $tmpFolder;
+    $phpThumb->config_cache_directory = MODX_BASE_PATH . $modx->getCacheFolder();
+    $phpThumb->config_temp_directory = $modx->getCacheFolder();
     $phpThumb->config_document_root = MODX_BASE_PATH;
     $phpThumb->setSourceFilename(MODX_BASE_PATH . $input);
     foreach ($params as $key => $value) {
