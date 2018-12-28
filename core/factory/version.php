25cd7c0f6c0f67ebf112d27fa79847c075424743
Dmi3yy <dmi3yy@gmail.com>
2018-12-28 19:21:44 +0200

    fix version

--- core/factory/version.php
+++ core/factory/version.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'version' => '2.0.0 alpha', // Current version number
+    'version' => '2.0.0-alpha', // Current version number
     'release_date' => 'Dec 23, 2018', // Date of release
     'branch' => 'Evolution CMS', // Codebase name
     'full_appname' => 'Evolution CMS 2.0.0 alpha (Dec 23, 2018)'
