f41121dc84b0f40d9e59aed586a8c4e7fe6ab6a0
Serious <serious2008@gmail.com>
2019-04-17 12:25:29 +0300

    fix(snippet): update webp validator

--- assets/snippets/phpthumb/snippet.phpthumb.php
+++ assets/snippets/phpthumb/snippet.phpthumb.php
@@ -109,8 +109,9 @@ if (!file_exists($outputFilename)) {
 
 if (isset($webp) && class_exists('\WebPConvert\WebPConvert')) {
     if (strpos($_SERVER['HTTP_ACCEPT'], 'image/webp') !== false
-        || (strpos($_SERVER['HTTP_USER_AGENT'], ' Safari/') !== false
-            && strpos($_SERVER['HTTP_USER_AGENT'], ' Version/') === false) || strpos($_SERVER['HTTP_USER_AGENT'], ' Safari/') === false) {
+            || (strpos($_SERVER['HTTP_USER_AGENT'], ' Safari/') !== false
+            && strpos($_SERVER['HTTP_USER_AGENT'], ' Version/') === false)
+            || strpos($_SERVER['HTTP_USER_AGENT'], ' Safari/') === false) {
         if (file_exists($outputFilename . '.webp')) {
             $fNameSuf .= '.webp';
         } else {
