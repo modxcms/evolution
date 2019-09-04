91ba911539e4454f3e54dda29348d553628f15a0
Dmi3yy <dmi3yy@gmail.com>
2019-09-04 14:15:10 +0300

    Revert "set $templatePath = 'views/';"
    This reverts commit 335547860d0a842d67a3f98e3704503535345e89.

--- assets/snippets/DocLister/lib/DLTemplate.class.php
+++ assets/snippets/DocLister/lib/DLTemplate.class.php
@@ -19,7 +19,7 @@ class DLTemplate
      */
     protected static $instance;
 
-    protected $templatePath = 'views/';
+    protected $templatePath = 'assets/templates/';
 
     protected $templateExtension = 'html';
 
