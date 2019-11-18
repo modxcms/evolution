4c1795a7386da8e0be893f6de8e414caed4948b3
Ser1ous <serious2008@gmail.com>
2019-11-18 14:08:50 +0200

    remove (core) Remove siteContentObject from Core

--- core/src/Core.php
+++ core/src/Core.php
@@ -2889,8 +2889,7 @@ class Core extends AbstractLaravel implements Interfaces\CoreInterface
                 if (isset($this->documentObject['id'])) {
                     $data = [
                         'modx' => $this,
-                        'documentObject' => $this->makeDocumentObject($this->documentObject['id']),
-                        'siteContentObject' => SiteContent::find($this->documentObject['id'])
+                        'documentObject' => $this->makeDocumentObject($this->documentObject['id'])
                     ];
                 } else {
                     $data = [
