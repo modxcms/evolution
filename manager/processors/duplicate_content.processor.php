e57bb549abea7402ba5b2171ce1d377f29e91ffe
freefalI <andriyshmatko1999@gmail.com>
2020-06-19 14:23:55 +0300

    fix (core) rewrite getdatabase query to models in duplicate processors

--- manager/processors/duplicate_content.processor.php
+++ manager/processors/duplicate_content.processor.php
@@ -28,7 +28,7 @@ if(!$udperms->checkPermissions()) {
 $id = duplicateDocument($id);
 
 // Set the item name for logger
-$name = $modx->getDatabase()->getValue($modx->getDatabase()->select('pagetitle', $modx->getDatabase()->getFullTableName('site_content'), "id='{$id}'"));
+$name = EvolutionCMS\Models\SiteContent::select('pagetitle')->findOrFail($id)->pagetitle;
 $_SESSION['itemname'] = $name;
 
 // finish cloning - redirect
