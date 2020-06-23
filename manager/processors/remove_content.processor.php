3d12ff6f92940afb5ccee0da82b89ee1afd1821f
Serious <serious2008@gmail.com>
2020-06-24 02:00:17 +0300

    core (fix) clear trashed resource

--- manager/processors/remove_content.processor.php
+++ manager/processors/remove_content.processor.php
@@ -6,8 +6,7 @@ if(!$modx->hasPermission('delete_document')) {
 	$modx->webAlertAndQuit($_lang["error_no_privileges"]);
 }
 
-$rs = $modx->getDatabase()->select('id', $modx->getDatabase()->getFullTableName('site_content'), "deleted=1");
-$ids = $modx->getDatabase()->getColumn('id', $rs);
+$ids = \EvolutionCMS\Models\SiteContent::query()->withTrashed()->where('deleted',1)->pluck('id')->toArray();
 
 // invoke OnBeforeEmptyTrash event
 $modx->invokeEvent("OnBeforeEmptyTrash",
@@ -16,21 +15,13 @@ $modx->invokeEvent("OnBeforeEmptyTrash",
 						));
 
 // remove the document groups link.
-$sql = "DELETE document_groups
-		FROM ".$modx->getDatabase()->getFullTableName('document_groups')." AS document_groups
-		INNER JOIN ".$modx->getDatabase()->getFullTableName('site_content')." AS site_content ON site_content.id = document_groups.document
-		WHERE site_content.deleted=1";
-$modx->getDatabase()->query($sql);
+\EvolutionCMS\Models\DocumentGroup::query()->whereIn('document', $ids)->delete();
 
 // remove the TV content values.
-$sql = "DELETE site_tmplvar_contentvalues
-		FROM ".$modx->getDatabase()->getFullTableName('site_tmplvar_contentvalues')." AS site_tmplvar_contentvalues
-		INNER JOIN ".$modx->getDatabase()->getFullTableName('site_content')." AS site_content ON site_content.id = site_tmplvar_contentvalues.contentid
-		WHERE site_content.deleted=1";
-$modx->getDatabase()->query($sql);
+\EvolutionCMS\Models\SiteTmplvarContentvalue::query()->whereIn('contentid', $ids)->delete();
 
 //'undelete' the document.
-$modx->getDatabase()->delete($modx->getDatabase()->getFullTableName('site_content'), "deleted=1");
+\EvolutionCMS\Models\SiteContent::query()->where('deleted', 1)->forceDelete();
 
 	// invoke OnEmptyTrash event
 	$modx->invokeEvent("OnEmptyTrash",
