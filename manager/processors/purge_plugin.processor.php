8041b7198c47db06ce57527185e975344e992b38
aleksandrpaskevic <serious2008@gmail.com>
2020-06-24 23:20:37 +0300

    fix (core) change getDatabase to Models in purge plugin

--- manager/processors/purge_plugin.processor.php
+++ manager/processors/purge_plugin.processor.php
@@ -1,47 +1,51 @@
 <?php
-if( ! defined('IN_MANAGER_MODE') || IN_MANAGER_MODE !== true) {
+if (!defined('IN_MANAGER_MODE') || IN_MANAGER_MODE !== true) {
     exit();
 }
 
-if(!$modx->hasPermission('delete_plugin')) {
-	$e->setError(3);
-	$e->dumpError();
+if (!$modx->hasPermission('delete_plugin')) {
+    $e->setError(3);
+    $e->dumpError();
 }
 
-$tbl_site_plugins = $modx->getDatabase()->getFullTableName('site_plugins');
-$tbl_site_plugin_events = $modx->getDatabase()->getFullTableName('site_plugin_events');
 
 // Get unique list of latest added plugins by highest sql-id
-$rs = $modx->getDatabase()->query("SELECT t1.id FROM {$tbl_site_plugins} t1 LEFT JOIN {$tbl_site_plugins} t2 ON (t1.name = t2.name AND t1.id < t2.id) WHERE t2.id IS NULL;");
+$plugins = \EvolutionCMS\Models\SitePlugin::query()->select('site_plugins.id')->leftJoin('site_plugins as t2', function ($join) {
+    $join->on('site_plugins.name', '=', 't2.name')->on('site_plugins.id', '<', 't2.id');
+})->whereNull('t2.id');
 $latestIds = array();
-while($row = $modx->getDatabase()->getRow($rs)) {
+foreach ($plugins->get()->toArray() as $row) {
     $latestIds[] = $row['id'];
 }
 
 // Get list of plugins with disabled and enabled versions
-$rs = $modx->getDatabase()->query("SELECT id FROM {$tbl_site_plugins} t1 WHERE disabled = 1 AND name IN (SELECT name FROM {$tbl_site_plugins} t2 WHERE t1.name = t2.name AND t1.id != t2.id)");
+$plugins = \EvolutionCMS\Models\SitePlugin::query()->select('site_plugins.id')->join('site_plugins as t2', function ($join) {
+    $join->on('site_plugins.name', '=', 't2.name')->on('site_plugins.id', '!=', 't2.id');
+})->where('site_plugins.disabled', 1);
 
-while($row = $modx->getDatabase()->getRow($rs)) {
+foreach ($plugins->get()->toArray() as $row) {
 
     $id = $row['id'];
 
-    if(in_array($id,$latestIds)) continue;	// Keep latest version of disabled plugins
+    if (in_array($id, $latestIds)) continue;    // Keep latest version of disabled plugins
 
     // invoke OnBeforePluginFormDelete event
-    $modx->invokeEvent('OnBeforePluginFormDelete', array('id'=> $id));
+    $modx->invokeEvent('OnBeforePluginFormDelete', array('id' => $id));
 
     // delete the plugin.
-    if (!$modx->getDatabase()->delete($tbl_site_plugins, "id={$id}")) {
+
+    if (!\EvolutionCMS\Models\SitePlugin::query()->where('id', $id)->delete()) {
         echo "Something went wrong while trying to delete plugin {$id}";
         exit;
     } else {
         // delete the plugin events.
-        if (!$modx->getDatabase()->delete($tbl_site_plugin_events, "pluginid={$id}")) {
+
+        if (!\EvolutionCMS\Models\SitePluginEvent::query()->where('pluginid', $id)->delete()) {
             echo "Something went wrong while trying to delete the plugin events for plugin {$id}";
             exit;
         } else {
             // invoke OnPluginFormDelete event
-            $modx->invokeEvent('OnPluginFormDelete', array('id'=>$id));
+            $modx->invokeEvent('OnPluginFormDelete', array('id' => $id));
         }
     }
 }
