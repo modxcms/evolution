8377c9a7443d420c6242ebd69763289a82c37d50
aleksandrpaskevic <serious2008@gmail.com>
2020-09-23 17:59:18 +0300

    core (fix) fix Site:make command for get actual version and get from user repository

--- core/src/Console/SiteUpdateCommand.php
+++ core/src/Console/SiteUpdateCommand.php
@@ -27,6 +27,7 @@ class SiteUpdateCommand extends Command
     {
         parent::__construct();
     }
+
     /**
      * Execute the console command.
      *
@@ -47,78 +48,134 @@ class SiteUpdateCommand extends Command
 
     public function startUpdate()
     {
+        $evo = EvolutionCMS();
+        $updateRepository = $evo->getConfig('UpgradeRepository');
+        if ($updateRepository == '') {
+            $updateRepository = 'evolution-cms/evolution';
+        }
+        $ch = curl_init();
+        $url = 'https://api.github.com/repos/' . $updateRepository . '/tags';
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_REFERER, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('User-Agent: updateNotify widget'));
+        $info = curl_exec($ch);
+        curl_close($ch);
+        if (substr($info, 0, 1) != '[') {
+            return;
+        }
+        $currentVersion = $evo->getVersionData();
+        $arrayVersion = explode('.', $currentVersion['version']);
+        $currentMajorVersion = array_shift($arrayVersion);
 
-        $url = 'https://github.com/evolution-cms/evolution/archive/2.1.x.zip';
-        echo "Start download EvolutionCMS\n";
-        $url = file_get_contents($url);
-        $file = MODX_BASE_PATH.'new_version.zip';
+        $info = json_decode($info, true);
+        foreach ($info as $key => $val) {
 
-        file_put_contents($file, $url);
-        echo "Start unpacking EvolutionCMS\n";
+            $arrayVersion = explode('.', $val['name']);
+            if ($currentMajorVersion == array_shift($arrayVersion)) {
 
+                $git['version'] = $val['name'];
 
-        $temp_dir = MODX_BASE_PATH.'_temp'.md5(time());
-        //run unzip and install
-
-        $zip = new \ZipArchive;
-        $res = $zip->open($file);
-        $zip->extractTo($temp_dir);
-        $zip->close();
-        unlink($file);
-
-        if ($handle = opendir($temp_dir)) {
-            while (false !== ($name = readdir($handle))) {
-                if ($name != '.' && $name != '..') $dir = $name;
+                if (strpos($val['name'], 'alpha')) {
+                    $git['alpha'] = $val['name'];
+                    continue;
+                } elseif (strpos($val['name'], 'beta')) {
+                    $git['beta'] = $val['name'];
+                    continue;
+                } else {
+                    $git['stable'] = $val['name'];
+                    break;
+                }
             }
-            closedir($handle);
+        }
+        $git['version'] = '';
+        if (isset($git['stable'])) {
+            if (version_compare($currentVersion['version'], $git['stable'], '!=')) {
+                $git['version'] = $git['stable'];
+            }
         }
 
-        SELF::moveFiles($temp_dir.'/'.$dir, MODX_BASE_PATH);
-        SELF::rmdirs($temp_dir);
-        echo "Run Migrations\n";
+        if ($git['version'] != '') {
+            $url = 'https://github.com/evolution-cms/evolution/archive/' . $git['version'] . '.zip';
+            echo "Start download EvolutionCMS\n";
+            $url = file_get_contents($url);
+            $file = MODX_BASE_PATH . 'new_version.zip';
 
-        exec('php  ../install/cli-install.php --typeInstall=2 --removeInstall=y');
-        echo "Remove Install Directory\n";
-        self::rmdirs(MODX_BASE_PATH.'install');
-        putenv('COMPOSER_HOME=' . EVO_CORE_PATH . 'composer');
-        $input = new ArrayInput(array('command' => 'update'));
-        $application = new Application();
-        $application->setAutoExit(false);
-        $application->run($input);
+            file_put_contents($file, $url);
+            echo "Start unpacking EvolutionCMS\n";
+
+            $temp_dir = MODX_BASE_PATH . '_temp' . md5(time());
+            //run unzip and install
+
+            $zip = new \ZipArchive;
+            $res = $zip->open($file);
+            $zip->extractTo($temp_dir);
+            $zip->close();
+            unlink($file);
+
+            if ($handle = opendir($temp_dir)) {
+                while (false !== ($name = readdir($handle))) {
+                    if ($name != '.' && $name != '..') $dir = $name;
+                }
+                closedir($handle);
+            }
+
+            SELF::moveFiles($temp_dir . '/' . $dir, MODX_BASE_PATH);
+            SELF::rmdirs($temp_dir);
+            echo "Run Migrations\n";
+
+            exec('php  ../install/cli-install.php --typeInstall=2 --removeInstall=y');
+            echo "Remove Install Directory\n";
+            self::rmdirs(MODX_BASE_PATH . 'install');
+            putenv('COMPOSER_HOME=' . EVO_CORE_PATH . 'composer');
+            $input = new ArrayInput(array('command' => 'update'));
+            $application = new Application();
+            $application->setAutoExit(false);
+            $application->run($input);
+        } else {
+            echo 'You use almost current version';
+        }
     }
-    static public function moveFiles($src, $dest) {
+
+    static public function moveFiles($src, $dest)
+    {
         $path = realpath($src);
         $dest = realpath($dest);
         $objects = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($path), \RecursiveIteratorIterator::SELF_FIRST);
-        foreach($objects as $name => $object) {
+        foreach ($objects as $name => $object) {
             $startsAt = substr(dirname($name), strlen($path));
-            self::mmkDir($dest.$startsAt);
-            if ( $object->isDir() ) {
-                self::mmkDir($dest.substr($name, strlen($path)));
+            self::mmkDir($dest . $startsAt);
+            if ($object->isDir()) {
+                self::mmkDir($dest . substr($name, strlen($path)));
             }
 
-            if(is_writable($dest.$startsAt) && $object->isFile()) {
-                rename((string)$name, $dest.$startsAt.'/'.basename($name));
+            if (is_writable($dest . $startsAt) && $object->isFile()) {
+                rename((string)$name, $dest . $startsAt . '/' . basename($name));
             }
         }
     }
 
-    static public function rmdirs($dir) {
+    static public function rmdirs($dir)
+    {
         if (is_dir($dir)) {
             $objects = scandir($dir);
             foreach ($objects as $object) {
                 if ($object != "." && $object != "..") {
-                    if (is_dir($dir."/".$object) && !is_link($dir."/".$object))
-                        self::rmdirs($dir."/".$object);
+                    if (is_dir($dir . "/" . $object) && !is_link($dir . "/" . $object))
+                        self::rmdirs($dir . "/" . $object);
                     else
-                        unlink($dir."/".$object);
+                        unlink($dir . "/" . $object);
                 }
             }
             rmdir($dir);
         }
     }
-    static public function mmkDir($folder, $perm=0777) {
-        if(!is_dir($folder)) {
+
+    static public function mmkDir($folder, $perm = 0777)
+    {
+        if (!is_dir($folder)) {
             mkdir($folder, $perm);
         }
     }
