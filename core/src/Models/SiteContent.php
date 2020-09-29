a0a0ad807f7c03e9475eb7dd6e945eff1ef76520
aleksandrpaskevic <serious2008@gmail.com>
2020-09-29 15:22:43 +0300

    core (fix) small refactor SiteContent model

--- core/src/Models/SiteContent.php
+++ core/src/Models/SiteContent.php
@@ -2049,34 +2049,7 @@ class SiteContent extends Eloquent\Model
         return $query->where('published', '1')->where('deleted', '0');
     }
 
-    public function scopeDepth($query, $parents = '', $depth = 1, $showParent = false)
-    {
-        $parents = explode(',', $parents);
-        if (count($parents) > 0) {
-            $ids = static::getIDs($parents, $depth);
-            if (!empty($ids)) {
-                $query = $query->whereIn('parent', $ids);
-                if (!$showParent) {
-                    $query = $query->whereNotIn('id', $ids);
-                }
-            }
-        }
-        return $query;
-    }
 
-    public static function getIDs($parents = array(), $depth = 1, $result = array())
-    {
-        $result = array_merge($result, $parents);
-        if ($depth <= 1) return $result;
-        $ids = static::whereIn('parent', $parents)->where('isfolder', '=', '1')->get()->pluck('id')->toArray();
-        if (!empty($ids)) {
-            $depth--;
-            $result = static::getIDs($ids, $depth, $result);
-        } else {
-            return $result;
-        }
-        return $result;
-    }
 
     public function scopeWithTVs($query, $tvList = array(), $sep = ':')
     {
