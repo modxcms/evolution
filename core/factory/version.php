b79407129560953b0f97032ca602aab488d5a6a5
Dmi3yy <dmi3yy@gmail.com>
2020-10-01 18:54:00 +0300

    fix version

--- core/factory/version.php
+++ core/factory/version.php
@@ -3,5 +3,5 @@ return [
     'version' => '3.0.0', // Current version number
     'release_date' => 'Oct 1, 2020', // Date of release
     'branch' => 'Evolution CMS', // Codebase name
-    'full_appname' => 'Evolution CMS 3.0.0 (Oct 1, 2020)'
+    'full_appname' => 'Evolution CMS 3.0.0RC (Oct 1, 2020)'
 ];
