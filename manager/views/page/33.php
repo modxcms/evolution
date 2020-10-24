bf1b9266207385595aee334b204f64a53190e8d1
aleksandrpaskevic <serious2008@gmail.com>
2020-10-24 23:43:17 +0300

    core (clear) delete not used pages

--- manager/views/page/33.php
+++
@@ -1,4 +0,0 @@
-<?php
-// get the delete user page
-extract(evolutionCMS()->get('ManagerTheme')->getViewAttributes(), EXTR_OVERWRITE);
-include_once evolutionCMS()->get('ManagerTheme')->getFileProcessor("processors/delete_user.processor.php");
