a6f05c7fa73f4ba2409e8cba7641e3d62f418597
Dmi3yy <dmi3yy@gmail.com>
2020-11-30 14:31:23 +0200

    update composer

--- core/vendor/autoload.php
+++ core/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit5cc5ed78365f94fc936c8e6a57657f58::getLoader();
+return ComposerAutoloaderInit54490a7f60bf043431313d0e500ccfe6::getLoader();
