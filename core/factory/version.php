91bd5dd6cd2ebc459dedb7c9b9c2c71b37f00232
Dmi3yy <dmi3yy@gmail.com>
2021-02-18 14:52:57 +0300

    Evolution CMS 3.1.1

--- core/factory/version.php
+++ core/factory/version.php
@@ -1,7 +1,7 @@
 <?php
 return [
-    'version' => '3.1.0', // Current version number
-    'release_date' => 'Feb 17, 2021', // Date of release
+    'version' => '3.1.1', // Current version number
+    'release_date' => 'Feb 18, 2021', // Date of release
     'branch' => 'Evolution CMS', // Codebase name
-    'full_appname' => 'Evolution CMS 3.1.0 (Feb 17, 2021)'
+    'full_appname' => 'Evolution CMS 3.1.1 (Feb 18, 2021)'
 ];
