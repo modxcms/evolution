8dae9606257652844d2a109be60bf63509c9fe6d
mnoskov <nm@perm-tools.ru>
2021-06-15 00:18:09 +0500

    default pagination lang

---
+++ core/lang/en/pagination.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Pagination Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the paginator library to build
+    | the simple pagination links.
+    |
+    */
+
+    'previous' => '&laquo; Previous',
+    'next' => 'Next &raquo;',
+];
