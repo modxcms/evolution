939f452a98f943c89cf04ea0d6e72c2f29de46ba
dmi3yy <dmi3yy@gmail.com>
2022-02-17 06:44:08 +0200

    update tracy. clear vendor

--- core/vendor/rosell-dk/exec-with-fallback/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'vendor/autoload.php';
-//include 'src/ExecWithFallback.php';
-use ExecWithFallback\ExecWithFallback;
-use ExecWithFallback\Tests\ExecWithFallbackTest;
-
-ExecWithFallback::exec('echo hello');
-
-ExecWithFallbackTest::testExec();
