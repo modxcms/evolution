2e6b9f3761166e52a42cf0718544b0645f13ad0b
Serhii Kornelyuk <seiger@meta.ua>
2022-06-18 00:37:05 +0300

    [FIX] Friendly url suffix in route.

--- core/functions/laravel.php
+++ core/functions/laravel.php
@@ -322,7 +322,7 @@ if (! function_exists('route')) {
      */
     function route($name, $parameters = [], $absolute = true)
     {
-        return app('url')->route($name, $parameters, $absolute);
+        return app('url')->route($name, $parameters, $absolute).evo()->getConfig('friendly_url_suffix', '');
     }
 }
 
