9621666f08aa99555f4cbb03b24b2e3124c26389
Pathologic <m@xim.name>
2022-06-23 21:36:35 +0300

    fix warning

--- core/functions/actions/mutate_content.php
+++ core/functions/actions/mutate_content.php
@@ -370,7 +370,7 @@ if (! function_exists('getTVDisplayFormat')) {
                 $o = '';
                 $countValue = count($value);
                 for ($i = 0; $i < $countValue; $i++) {
-                    list($name, $url) = is_array($value[$i]) ? $value[$i] : explode("==", $value[$i]);
+                    list($name, $url) = is_array($value[$i]) ? $value[$i] : array_merge(explode("==", $value[$i]), ['']);
                     if (!$url) {
                         $url = $name;
                     }
